3e1c75f0168dcb7bb42f42ed23de4a7f9a8d2fb7
Sayli Karmarkar <skarmark@redhat.com>
2012-11-07 11:00:19 -0800

    adding removal of old iso distributors associated with rpm repos to the migration script

--- pulp_rpm/src/pulp_rpm/migrations/0001_rpm_add_export_distributor.py
+++ pulp_rpm/src/pulp_rpm/migrations/0001_rpm_add_export_distributor.py
@@ -23,6 +23,11 @@ EXPORT_DISTRIBUTOR_CONFIG = {"http" : False, "https" : True}
 _LOG = logging.getLogger('pulp')
 
 def _migrate_rpm_repositories():
+    '''
+    This migration takes care of adding export_distributor to all the old rpm repos 
+    with no export_distributor already associated to them. Since we have renamed iso_distributor
+    to export_distributor, it also removes iso_distributor associated with an rpm repo.  
+    '''
     collection = RepoDistributor.get_collection()
     for repo_distributor in collection.find():
 
@@ -32,14 +37,18 @@ def _migrate_rpm_repositories():
             # Check if an export_distributor exists for the same repo
             if collection.find_one({'repo_id': repo_distributor['repo_id'], 'distributor_type_id': ids.TYPE_ID_DISTRIBUTOR_EXPORT}) is None:
 
-                # If not, create a new one with default config and same auto_publish flag as corresponding yum distributor
+                # If not, create a new one with default config
                 export_distributor = RepoDistributor(repo_id = repo_distributor['repo_id'], 
                                                   id = ids.EXPORT_DISTRIBUTOR_ID,
                                                   distributor_type_id = ids.TYPE_ID_DISTRIBUTOR_EXPORT, 
                                                   config = EXPORT_DISTRIBUTOR_CONFIG, 
                                                   auto_publish = False)
                 collection.save(export_distributor, safe=True)
-
+            
+            # Remove iso_distributor associated with the repo 
+            iso_distributor = collection.find_one({'repo_id': repo_distributor['repo_id'], 'distributor_type_id': 'iso_distributor'})
+            if iso_distributor is not None:
+                collection.remove(iso_distributor, safe=True)
 
 def migrate(*args, **kwargs):
     _LOG.info("Export distributor migration for rpm repositories started")
