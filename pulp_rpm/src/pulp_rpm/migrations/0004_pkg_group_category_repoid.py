95097af5c052063696291f0cb072198fc8ffe862
Pradeep Kilambi <pkilambi@redhat.com>
2013-01-15 16:23:30 -0500

    implemented suggestions from the PR; > updated jdob's unit test to handle groups and categories

--- pulp_rpm/src/pulp_rpm/migrations/0004_pkg_group_category_repoid.py
+++ pulp_rpm/src/pulp_rpm/migrations/0004_pkg_group_category_repoid.py
@@ -17,25 +17,23 @@ import logging
 from pymongo.errors import DuplicateKeyError
 
 from pulp.server.managers import factory
-from pulp.server.managers.repo.unit_association_query import RepoUnitAssociationQueryManager,UnitAssociationCriteria
-from pulp.server.managers.repo.unit_association import RepoUnitAssociationManager
-from pulp.server.managers.content.cud import ContentManager
-from pulp.plugins.loader import api
+from pulp.server.managers.repo.unit_association_query import UnitAssociationCriteria
+from pulp.server.managers.repo.cud import RepoContentUnit
 from pulp_rpm.common.ids import TYPE_ID_PKG_GROUP, TYPE_ID_PKG_CATEGORY
 
 _log = logging.getLogger('pulp')
 
 # Initialize plugin loader api and other managers
-api.initialize()
-ass_query_mgr =  RepoUnitAssociationQueryManager()
-ass_mgr = RepoUnitAssociationManager()
-content_mgr = ContentManager()
+factory.initialize()
+ass_query_mgr =  factory.repo_unit_association_query_manager()
+ass_mgr = factory.repo_unit_association_manager()
+content_mgr = factory.content_manager()
 
 def _get_repos():
     """
      Lookups all the yum based repos in pulp.
+     @return a list of repoids
     """
-    factory.initialize()
     repos = factory.repo_query_manager().find_with_importer_type("yum_importer")
     if not repos:
         _log.debug("No repos found to perform db migrate")
@@ -49,40 +47,34 @@ def _fix_pkg_group_category_repoid(repoid, typeid):
     the unit is associated with. If they dont match,
      * take a deep copy of the pkg group or category unit
      * create(save) new unit with fixed repoid
-     * remove association between old unit and repo
-     * associate new unit with the repo
+     * re-associate new unit with the repo
     """
     units = ass_query_mgr.get_units(repo_id=repoid, criteria=UnitAssociationCriteria(type_ids=typeid))
-    _log.debug("Found %s units in repo %s" % (len(units), repoid))
     for unit in units:
         if unit['metadata']['repo_id'] != repoid:
             _log.debug("Found unit %s to migrate" % unit['id'])
             # take a copy of the unit and fix the repoid
-            new_unit = _safe_copy_unit(unit)
-            new_unit['metadata']['repo_id'] = repoid
-            try:
-                content_mgr.add_content_unit(content_type=typeid, unit_id=new_unit['id'], unit_metadata=new_unit['metadata'])
-            except DuplicateKeyError:
-                # duplicate key, if unit already exists; just continue and associate to the repo; nothing new to create
-                _log.debug("A pkg group with id %s and repoid %s already exists, nothing new to create" % (unit['id'], repoid))
-                pass
-            # unassociate the old unit from the repo
-            ass_mgr.unassociate_unit_by_id(repo_id=repoid, unit_type_id=typeid, unit_id=unit['id'], owner_type="user", owner_id="admin")
-            # associate the new unit back to the repo
-            ass_mgr.associate_unit_by_id(repo_id=repoid, unit_type_id=typeid, unit_id=new_unit['id'], owner_type="user", owner_id="admin")
+            new_unit_metadata = _safe_copy_unit(unit['metadata'])
+            new_unit_metadata['repo_id'] = repoid
+            new_unit_id = content_mgr.add_content_unit(content_type=typeid, unit_id=None, unit_metadata=new_unit_metadata)
+            # Grab the association doc itself from the DB directly
+            association = RepoContentUnit.get_collection().find_one({'_id' : unit['_id']})
+            # Update to point to the new unit
+            association['unit_id'] = new_unit_id
+            # Save it back to the DB
+            RepoContentUnit.get_collection().save(association, safe=True)
 
 def _safe_copy_unit(unit):
     """
     Creates a deep copy of the unit and cleans out the _ fields
-    @param unit: pulp.plugins.data.Unit object to clone
-    @return: cloned unit pulp.plugins.data.Unit
+    @param unit: unit metadata dict
+    @return: cloned unit metadata dict
     """
     u = copy.deepcopy(unit)
-    u.id = None
     # remove all the _ fields so save_unit defaults them
-    for key in u['metadata'].keys():
+    for key in u.keys():
         if key.startswith('_') :
-            del u['metadata'][key]
+            del u[key]
     return u
 
 def _migrate_units():
