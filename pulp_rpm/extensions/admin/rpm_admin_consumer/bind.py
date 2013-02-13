4a297d604bb0bbfa77ef34fdee5acfebe93eda49
Jason L Connor <jason.connor@gmail.com>
2013-02-13 10:29:27 -0700

    new derived bind classes

--- pulp_rpm/extensions/admin/rpm_admin_consumer/bind.py
+++ pulp_rpm/extensions/admin/rpm_admin_consumer/bind.py
@@ -11,64 +11,31 @@
 # have received a copy of GPLv2 along with this software; if not, see
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
-from gettext import gettext as _
+from pulp.client.commands.consumer.bind import (
+    ConsumerBindCommand, ConsumerUnbindCommand, OPTION_DISTRIBUTOR_ID)
 
-from pulp.bindings.exceptions import NotFoundException
-from pulp.client.extensions.extensions import PulpCliCommand
 
 YUM_DISTRIBUTOR_ID = 'yum_distributor'
 
-class BindCommand(PulpCliCommand):
-    def __init__(self, context, name, description):
-        super(BindCommand, self).__init__(name, description, self.bind)
-        self.create_option('--consumer-id', _('identifies the consumer'), required=True)
-        self.create_option('--repo-id', _('repository to bind'), required=True)
-        self.context = context
+
+class YumConsumerBindCommand(ConsumerBindCommand):
+    def __init__(self, context):
+        super(self.__class__, self).__init__(context)
+        # don't need this as an option because we'll hard code its passing
+        self.options.remove(OPTION_DISTRIBUTOR_ID)
 
     def bind(self, **kwargs):
-        id = kwargs['consumer-id']
-        repo_id = kwargs['repo-id']
+        kwargs[OPTION_DISTRIBUTOR_ID.keyword] = YUM_DISTRIBUTOR_ID
+        super(self.__class__, self).bind(**kwargs)
 
-        try:
-            response = self.context.server.bind.bind(id, repo_id, YUM_DISTRIBUTOR_ID)
-            msg = _('Bind tasks successfully created:')
-            self.context.prompt.render_success_message(msg)
-            tasks = [dict(task_id=str(t.task_id)) for t in response.response_body]
-            self.context.prompt.render_document_list(tasks)
-        except NotFoundException, e:
-            resources = e.extra_data['resources']
-            if 'consumer' in resources:
-                r_type = _('Consumer')
-                r_id = id
-            else:
-                r_type = _('Repository')
-                r_id = repo_id
-            msg = _('%(t)s [%(id)s] does not exist on the server')
-            self.context.prompt.write(msg % {'t':r_type, 'id':r_id}, tag='not-found')
 
-class UnbindCommand(PulpCliCommand):
-    def __init__(self, context, name, description):
-        super(UnbindCommand, self).__init__(name, description, self.unbind)
-        self.create_option('--consumer-id', _('identifies the consumer'), required=True)
-        self.create_option('--repo-id', _('repository to unbind'), required=True)
-        self.create_flag('--force', _('delete the binding immediately and discontinue tracking consumer actions'))
-        self.context = context
+class YumConsumerUnbindCommand(ConsumerUnbindCommand):
+    def __init__(self, context):
+        super(self.__class__, self).__init__(context)
+        # don't need this as an option because we'll hard code its passing
+        self.options.remove(OPTION_DISTRIBUTOR_ID)
 
     def unbind(self, **kwargs):
-        id = kwargs['consumer-id']
-        repo_id = kwargs['repo-id']
-        force = kwargs['force']
-        try:
-            response = self.context.server.bind.unbind(id, repo_id, YUM_DISTRIBUTOR_ID, force)
-            msg = _('Unbind tasks successfully created:')
-            self.context.prompt.render_success_message(msg)
-            tasks = [dict(task_id=str(t.task_id)) for t in response.response_body]
-            self.context.prompt.render_document_list(tasks)
-        except NotFoundException, e:
-            bind_id = e.extra_data['resources']['bind_id']
-            m = _('Binding [consumer: %(c)s, repository: %(r)s] does not exist on the server')
-            d = {
-                'c' : bind_id['consumer_id'],
-                'r' : bind_id['repo_id'],
-            }
-            self.context.prompt.write(m % d, tag='not-found')
+        kwargs[OPTION_DISTRIBUTOR_ID.keyword] = YUM_DISTRIBUTOR_ID
+        super(self.__class__, self).unbind(**kwargs)
+
