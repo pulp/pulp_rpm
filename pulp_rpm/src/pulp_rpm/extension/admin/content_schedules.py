6dac09800997f3cae08df19bb0755099ccf71418
Jason L Connor <jason.connor@gmail.com>
2013-02-13 23:07:33 -0700

    changed schedules to derive from new base content schedule classes

--- pulp_rpm/src/pulp_rpm/extension/admin/content_schedules.py
+++ pulp_rpm/src/pulp_rpm/extension/admin/content_schedules.py
@@ -11,32 +11,25 @@
 
 from gettext import gettext as _
 
-from pulp.client.commands.schedule import (
-    DeleteScheduleCommand, ListScheduleCommand, CreateScheduleCommand,
-    UpdateScheduleCommand, NextRunCommand, ScheduleStrategy)
+from pulp.client.commands.consumer.content import (
+    ConsumerContentCreateScheduleCommand, ConsumerContentScheduleStrategy,
+    OPTION_CONTENT_TYPE_ID)
 from pulp.client.commands.options import OPTION_CONSUMER_ID
 from pulp.client.extensions.extensions import PulpCliOption
 from pulp_rpm.common.ids import TYPE_ID_RPM, TYPE_ID_ERRATA, TYPE_ID_PKG_GROUP
 
 # -- commands -----------------------------------------------------------------
 
-class ContentListScheduleCommand(ListScheduleCommand):
-    def __init__(self, context, action):
-        strategy = ConsumerContentScheduleStrategy(context, action)
-        DESC_LIST = _('list scheduled %s operations' % action)
-        super(ContentListScheduleCommand, self).__init__(context, strategy,
-                                                     description=DESC_LIST)
-        self.add_option(OPTION_CONSUMER_ID)
-
-
-class ContentCreateScheduleCommand(CreateScheduleCommand):
-    def __init__(self, context, action, content_type):
-        strategy = ConsumerContentScheduleStrategy(context, action, content_type)
-        DESC_CREATE = _('adds a new scheduled %s operation' % action)
-        super(ContentCreateScheduleCommand, self).__init__(context, strategy,
-                                                       description=DESC_CREATE)
-        self.add_option(OPTION_CONSUMER_ID)
+class YumConsumerContentCreateScheduleCommand(ConsumerContentCreateScheduleCommand):
+    def __init__(self, context, action, content_type, strategy=None):
         assert(content_type in (TYPE_ID_RPM, TYPE_ID_ERRATA, TYPE_ID_PKG_GROUP))
+
+        strategy = strategy or YumConsumerContentScheduleStrategy(context, action, content_type)
+        super(self.__class__, self).__init__(context, action, strategy)
+
+        # This will be substituted by the options below
+        self.options.remove(OPTION_CONTENT_TYPE_ID)
+
         if content_type == TYPE_ID_RPM:
             self.add_option(PulpCliOption('--name', _('package name; may be repeated for multiple packages'),
                                           required=True, allow_multiple=True, aliases=['-n']))
@@ -48,50 +41,22 @@ class ContentCreateScheduleCommand(CreateScheduleCommand):
                                           required=True, allow_multiple=True, aliases=['-n']))
 
 
-class ContentDeleteScheduleCommand(DeleteScheduleCommand):
-    def __init__(self, context, action):
-        strategy = ConsumerContentScheduleStrategy(context, action)
-        DESC_DELETE = _('deletes a %s schedule' % action)
-        super(ContentDeleteScheduleCommand, self).__init__(context, strategy,
-                                                       description=DESC_DELETE)
-        self.add_option(OPTION_CONSUMER_ID)
-
-
-class ContentUpdateScheduleCommand(UpdateScheduleCommand):
-    def __init__(self, context, action):
-        strategy = ConsumerContentScheduleStrategy(context, action)
-        DESC_UPDATE = _('updates an existing %s schedule' % action)
-        super(ContentUpdateScheduleCommand, self).__init__(context, strategy,
-                                                       description=DESC_UPDATE)
-        self.add_option(OPTION_CONSUMER_ID)
-
-
-class ContentNextRunCommand(NextRunCommand):
-    def __init__(self, context, action):
-        strategy = ConsumerContentScheduleStrategy(context, action)
-        DESC_NEXT_RUN = _('displays the next scheduled %s for a consumer' % action)
-        super(ContentNextRunCommand, self).__init__(context, strategy,
-                                                description=DESC_NEXT_RUN)
-        self.add_option(OPTION_CONSUMER_ID)
-
-
 # -- framework classes --------------------------------------------------------
 
-class ConsumerContentScheduleStrategy(ScheduleStrategy):
+class YumConsumerContentScheduleStrategy(ConsumerContentScheduleStrategy):
 
     # See super class for method documentation
 
-    def __init__(self, context, action, content_type=None):
-        super(ConsumerContentScheduleStrategy, self).__init__()
-        self.context = context
-        self.action = action
+    def __init__(self, context, action, content_type):
+        assert content_type in (TYPE_ID_RPM, TYPE_ID_ERRATA, TYPE_ID_PKG_GROUP)
+
+        super(self.__class__, self).__init__(context, action)
+
         self.content_type = content_type
-        self.api = context.server.consumer_content_schedules
 
     def create_schedule(self, schedule, failure_threshold, enabled, kwargs):
         consumer_id = kwargs[OPTION_CONSUMER_ID.keyword]
         units = []
-        assert(self.content_type in (TYPE_ID_RPM, TYPE_ID_ERRATA, TYPE_ID_PKG_GROUP))
         if self.content_type in (TYPE_ID_RPM, TYPE_ID_PKG_GROUP):
             for name in kwargs['name']:
                 unit_key = dict(name=name)
@@ -108,14 +73,3 @@ class ConsumerContentScheduleStrategy(ScheduleStrategy):
         options = {}
         return self.api.add_schedule(self.action, consumer_id, schedule, units, failure_threshold, enabled, options)
 
-    def delete_schedule(self, schedule_id, kwargs):
-        consumer_id = kwargs[OPTION_CONSUMER_ID.keyword]
-        return self.api.delete_schedule(self.action, consumer_id, schedule_id)
-
-    def retrieve_schedules(self, kwargs):
-        consumer_id = kwargs[OPTION_CONSUMER_ID.keyword]
-        return self.api.list_schedules(self.action, consumer_id)
-
-    def update_schedule(self, schedule_id, **kwargs):
-        consumer_id = kwargs.pop(OPTION_CONSUMER_ID.keyword)
-        return self.api.update_schedule(self.action, consumer_id, schedule_id, **kwargs)
