1068dedf3e281da5eb6676049028f25877d85b62
Randy Barlow <rbarlow@redhat.com>
2013-02-18 17:59:51 -0500

    Hardened the ISO importer configuration validation.

--- pulp_rpm/src/pulp_rpm/plugins/importers/iso_importer/configuration.py
+++ pulp_rpm/src/pulp_rpm/plugins/importers/iso_importer/configuration.py
@@ -32,6 +32,14 @@ def validate(config):
     validators = (
         _validate_feed_url,
         _validate_max_speed,
+        _validate_num_threads,
+        _validate_proxy_password,
+        _validate_proxy_port,
+        _validate_proxy_url,
+        _validate_proxy_username,
+        _validate_ssl_ca_cert,
+        _validate_ssl_client_cert,
+        _validate_ssl_client_key,
     )
 
     for v in validators:
@@ -40,6 +48,28 @@ def validate(config):
             return valid, error_message
 
     return True, None
+
+
+def _cast_to_int_without_allowing_floats(value):
+    """
+    Attempt to return an int of the value, without allowing any floating point values. This is useful to
+    ensure that you get an int type out of value, while allowing a string representation of the value. If
+    there are any non numerical characters in value, this will raise ValueError. 
+    
+    :param value: The value you want to validate
+    :type  value: int or basestring
+    :return:      The integer representation of value
+    :rtype:       int
+    """
+    if isinstance(value, basestring):
+        # We don't want to allow floating point values
+        if not value.isdigit():
+            raise ValueError()
+        # Interpret num_threads as an integer
+        value = int(value)
+    if not isinstance(value, int):
+        raise ValueError()
+    return value
 
 
 def _validate_feed_url(config):
@@ -75,4 +105,214 @@ def _validate_max_speed(config):
         return False, _('The configuration parameter <%(max_speed_name)s> must be set to a positive '
                         'numerical value, but is currently set to <%(max_speed_value)s>.')%{
                             'max_speed_name': constants.CONFIG_MAX_SPEED, 'max_speed_value': max_speed}
+    return True, None
+
+
+def _validate_num_threads(config):
+    """
+    Make sure the num_threads value is a positive integer.
+    
+    :param config: The configuration object that we are validating.
+    :type  config: pulp.plugins.config.PluginCallConfiguration
+    :return:       Tuple of valid, error_message indicating success or failure as a boolean in the first, and
+                   an error message in the second when the first is False
+    :rtype:        tuple
+    """
+    num_threads = config.get(constants.CONFIG_NUM_THREADS)
+    if num_threads is None:
+        # We don't require num_threads to be set
+        return True, None
+    try:
+        num_threads = _cast_to_int_without_allowing_floats(num_threads)
+        if num_threads < 1:
+            raise ValueError()
+    except ValueError:
+        error_message = _('The configuration parameter <%(num_threads_name)s> must be set to a positive '
+                          'integer, but is currently set to <%(num_threads)s>.')%{
+                            'num_threads_name': constants.CONFIG_NUM_THREADS, 'num_threads': num_threads}
+        return False, error_message
+    # No errors, so return success
+    return True, None
+
+
+def _validate_proxy_password(config):
+    """
+    Make sure the proxy_password is set if the proxy_user is set, and that it is a string.
+    
+    :param config: The configuration object that we are validating.
+    :type  config: pulp.plugins.config.PluginCallConfiguration
+    :return:       Tuple of valid, error_message indicating success or failure as a boolean in the first, and
+                   an error message in the second when the first is False
+    :rtype:        tuple
+    """
+    proxy_password = config.get(constants.CONFIG_PROXY_PASSWORD)
+    if proxy_password is None:
+        # Proxy password is not required
+        return True, None
+    if not isinstance(proxy_password, basestring):
+        return False, _('The configuration parameter <%(proxy_password_name)s> should be a string, but it '
+                        'was %(type)s.')%{'proxy_password_name': constants.CONFIG_PROXY_PASSWORD,
+                                          'type': type(proxy_password)}
+    # If proxy_password is set, proxy_username must also be set
+    if not config.get(constants.CONFIG_PROXY_USER):
+        return False, _('The configuration parameter <%(password_name)s> requires the <%(username_name)s> '
+                        'parameter to also be set.')%{'password_name': constants.CONFIG_PROXY_PASSWORD,
+                                                      'username_name': constants.CONFIG_PROXY_USER}
+    # We also require the URL to be set if the password is set
+    if not config.get(constants.CONFIG_PROXY_URL):
+        return False, _('The configuration parameter <%(password_name)s> requires the <%(url_name)s> '
+                        'parameter to also be set.')%{'password_name': constants.CONFIG_PROXY_PASSWORD,
+                                                      'url_name': constants.CONFIG_PROXY_URL}
+    return True, None
+
+
+def _validate_proxy_port(config):
+    """
+    Make sure the proxy_url is set if the proxy_port is set. Also, if it is set, make sure it is a positive
+    integer.
+    
+    :param config: The configuration object that we are validating.
+    :type  config: pulp.plugins.config.PluginCallConfiguration
+    :return:       Tuple of valid, error_message indicating success or failure as a boolean in the first, and
+                   an error message in the second when the first is False
+    :rtype:        tuple
+    """
+    proxy_port = config.get(constants.CONFIG_PROXY_PORT)
+    if proxy_port is None:
+        # Proxy port is not required
+        return True, None
+    if not config.get(constants.CONFIG_PROXY_URL):
+        return False, _('The configuration parameter <%(name)s> requires the <%(url_name)s> '
+                        'parameter to also be set.')%{'name': constants.CONFIG_PROXY_PORT,
+                                                      'url_name': constants.CONFIG_PROXY_URL}
+    try:
+        proxy_port = _cast_to_int_without_allowing_floats(proxy_port)
+        if proxy_port < 1:
+            raise ValueError()
+    except ValueError:
+        error_message = _('The configuration parameter <%(name)s> must be set to a positive '
+                          'integer, but is currently set to <%(value)s>.')%{
+                            'name': constants.CONFIG_PROXY_PORT, 'value': proxy_port}
+        return False, error_message
+    # No errors, so return success
+    return True, None
+
+
+def _validate_proxy_url(config):
+    """
+    Make sure the proxy_url is a string, if it is set
+    
+    :param config: The configuration object that we are validating.
+    :type  config: pulp.plugins.config.PluginCallConfiguration
+    :return:       Tuple of valid, error_message indicating success or failure as a boolean in the first, and
+                   an error message in the second when the first is False
+    :rtype:        tuple
+    """
+    proxy_url = config.get(constants.CONFIG_PROXY_URL)
+    if proxy_url is None:
+        # Proxy url is not required
+        return True, None
+    if not isinstance(proxy_url, basestring):
+        return False, _('The configuration parameter <%(name)s> should be a string, but it '
+                        'was %(type)s.')%{'name': constants.CONFIG_PROXY_URL,
+                                          'type': type(proxy_url)}
+    return True, None
+
+
+def _validate_proxy_username(config):
+    """
+    Make sure the proxy_password is set if the proxy_user is set, and that the username is a string.
+    
+    :param config: The configuration object that we are validating.
+    :type  config: pulp.plugins.config.PluginCallConfiguration
+    :return:       Tuple of valid, error_message indicating success or failure as a boolean in the first, and
+                   an error message in the second when the first is False
+    :rtype:        tuple
+    """
+    proxy_username = config.get(constants.CONFIG_PROXY_USER)
+    if proxy_username is None:
+        # Proxy username is not required
+        return True, None
+    if not isinstance(proxy_username, basestring):
+        return False, _('The configuration parameter <%(name)s> should be a string, but it '
+                        'was %(type)s.')%{'name': constants.CONFIG_PROXY_USER,
+                                          'type': type(proxy_username)}
+    # If proxy_password is set, proxy_username must also be set
+    if not config.get(constants.CONFIG_PROXY_PASSWORD):
+        return False, _('The configuration parameter <%(username_name)s> requires the <%(password_name)s> '
+                        'parameter to also be set.')%{'password_name': constants.CONFIG_PROXY_PASSWORD,
+                                                      'username_name': constants.CONFIG_PROXY_USER}
+    # We also require the URL to be set if the password is set
+    if not config.get(constants.CONFIG_PROXY_URL):
+        return False, _('The configuration parameter <%(username_name)s> requires the <%(url_name)s> '
+                        'parameter to also be set.')%{'username_name': constants.CONFIG_PROXY_USER,
+                                                      'url_name': constants.CONFIG_PROXY_URL}
+    return True, None
+
+
+def _validate_ssl_ca_cert(config):
+    """
+    Make sure the ssl_ca_cert is a string, if it is set
+    
+    :param config: The configuration object that we are validating.
+    :type  config: pulp.plugins.config.PluginCallConfiguration
+    :return:       Tuple of valid, error_message indicating success or failure as a boolean in the first, and
+                   an error message in the second when the first is False
+    :rtype:        tuple
+    """
+    ssl_ca_cert = config.get(constants.CONFIG_SSL_CA_CERT)
+    if ssl_ca_cert is None:
+        # ssl_ca_cert is not required
+        return True, None
+    if not isinstance(ssl_ca_cert, basestring):
+        return False, _('The configuration parameter <%(name)s> should be a string, but it '
+                        'was %(type)s.')%{'name': constants.CONFIG_SSL_CA_CERT,
+                                          'type': type(ssl_ca_cert)}
+    return True, None
+
+
+def _validate_ssl_client_cert(config):
+    """
+    Make sure the ssl_client_cert is a string, if it is set
+    
+    :param config: The configuration object that we are validating.
+    :type  config: pulp.plugins.config.PluginCallConfiguration
+    :return:       Tuple of valid, error_message indicating success or failure as a boolean in the first, and
+                   an error message in the second when the first is False
+    :rtype:        tuple
+    """
+    ssl_client_cert = config.get(constants.CONFIG_SSL_CLIENT_CERT)
+    if ssl_client_cert is None:
+        # ssl_client_cert is not required
+        return True, None
+    if not isinstance(ssl_client_cert, basestring):
+        return False, _('The configuration parameter <%(name)s> should be a string, but it '
+                        'was %(type)s.')%{'name': constants.CONFIG_SSL_CLIENT_CERT,
+                                          'type': type(ssl_client_cert)}
+    return True, None
+
+
+def _validate_ssl_client_key(config):
+    """
+    Make sure the ssl_client_key is a string and that the cert is also provided, if the key is set.
+    
+    :param config: The configuration object that we are validating.
+    :type  config: pulp.plugins.config.PluginCallConfiguration
+    :return:       Tuple of valid, error_message indicating success or failure as a boolean in the first, and
+                   an error message in the second when the first is False
+    :rtype:        tuple
+    """
+    ssl_client_key = config.get(constants.CONFIG_SSL_CLIENT_KEY)
+    if ssl_client_key is None:
+        # ssl_client_key is not required
+        return True, None
+    if not isinstance(ssl_client_key, basestring):
+        return False, _('The configuration parameter <%(name)s> should be a string, but it '
+                        'was %(type)s.')%{'name': constants.CONFIG_SSL_CLIENT_KEY,
+                                          'type': type(ssl_client_key)}
+    # If the key is set, we should also have a cert
+    if not config.get(constants.CONFIG_SSL_CLIENT_CERT):
+        return False, _('The configuration parameter <%(key_name)s> requires the <%(cert_name)s> '
+                        'parameter to also be set.')%{'key_name': constants.CONFIG_SSL_CLIENT_KEY,
+                                                      'cert_name': constants.CONFIG_SSL_CLIENT_CERT}
     return True, None
--- pulp_rpm/test/unit/server/test_iso_importer_configuration.py
+++ pulp_rpm/test/unit/server/test_iso_importer_configuration.py
@@ -10,29 +10,36 @@
 # NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
 # have received a copy of GPLv2 along with this software; if not, see
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+import math
+
 from pulp_rpm.common import constants
 from pulp_rpm.plugins.importers.iso_importer import configuration
 from rpm_support_base import PulpRPMTests
 import importer_mocks
 
+CONFIG_SSL_CLIENT_CERT = 'ssl_client_cert'
+CONFIG_SSL_CLIENT_KEY  = 'ssl_client_key'
+
 class TestValidate(PulpRPMTests):
     """
     Test the validate() method.
     """
-    def test_validate(self):
-        config = importer_mocks.get_basic_config(
-            **{constants.CONFIG_FEED_URL: "http://test.com/feed", constants.CONFIG_MAX_SPEED: 56.6})
-        status, error_message = configuration.validate(config)
-        self.assertTrue(status)
-        self.assertEqual(error_message, None)
-        
     def test_invalid_config(self):
         config = importer_mocks.get_basic_config(**{constants.CONFIG_FEED_URL: "http://test.com/feed",
-                                                    constants.CONFIG_MAX_SPEED: "A Thousand"})
+                                                    constants.CONFIG_MAX_SPEED: "A Thousand",
+                                                    constants.CONFIG_NUM_THREADS: 7})
         status, error_message = configuration.validate(config)
         self.assertFalse(status)
         self.assertEqual(error_message, 'The configuration parameter <max_speed> must be set to a positive '
                                         'numerical value, but is currently set to <A Thousand>.')
+
+    def test_validate(self):
+        config = importer_mocks.get_basic_config(
+            **{constants.CONFIG_FEED_URL: "http://test.com/feed", constants.CONFIG_MAX_SPEED: 56.6,
+               constants.CONFIG_NUM_THREADS: 3})
+        status, error_message = configuration.validate(config)
+        self.assertTrue(status)
+        self.assertEqual(error_message, None)
 
 
 class TestValidateFeedUrl(PulpRPMTests):
@@ -41,7 +48,7 @@ class TestValidateFeedUrl(PulpRPMTests):
         status, error_message = configuration.validate(config)
         self.assertTrue(status)
         self.assertEqual(error_message, None)
-        
+
     def test_invalid_config(self):
         config = importer_mocks.get_basic_config(**{constants.CONFIG_FEED_URL: 42})
         status, error_message = configuration.validate(config)
@@ -55,10 +62,224 @@ class TestValidateMaxSpeed(PulpRPMTests):
         status, error_message = configuration.validate(config)
         self.assertTrue(status)
         self.assertEqual(error_message, None)
-        
+
     def test_invalid_config(self):
         config = importer_mocks.get_basic_config(**{constants.CONFIG_MAX_SPEED: -1.0})
         status, error_message = configuration.validate(config)
         self.assertFalse(status)
         self.assertEqual(error_message, 'The configuration parameter <max_speed> must be set to a positive '
                                         'numerical value, but is currently set to <-1.0>.')
+
+    def test_invalid_str(self):
+        config = importer_mocks.get_basic_config(**{constants.CONFIG_MAX_SPEED: '-42.0'})
+        status, error_message = configuration.validate(config)
+        self.assertFalse(status)
+        self.assertEqual(error_message, 'The configuration parameter <max_speed> must be set to a positive '
+                                        'numerical value, but is currently set to <-42.0>.')
+
+    def test_str(self):
+        config = importer_mocks.get_basic_config(**{constants.CONFIG_MAX_SPEED: '512.0'})
+        status, error_message = configuration.validate(config)
+        self.assertTrue(status)
+        self.assertEqual(error_message, None)
+
+
+class TestValidateNumThreads(PulpRPMTests):
+    def test_validate(self):
+        config = importer_mocks.get_basic_config(**{constants.CONFIG_NUM_THREADS: 11})
+        status, error_message = configuration.validate(config)
+        self.assertTrue(status)
+        self.assertEqual(error_message, None)
+
+    def test_float(self):
+        config = importer_mocks.get_basic_config(**{constants.CONFIG_NUM_THREADS: math.pi})
+        status, error_message = configuration.validate(config)
+        self.assertFalse(status)
+        self.assertEqual(error_message, 'The configuration parameter <num_threads> must be set to a positive '
+                                        'integer, but is currently set to <%s>.'%math.pi)
+
+    def test_float_str(self):
+        config = importer_mocks.get_basic_config(**{constants.CONFIG_NUM_THREADS: '%s'%math.e})
+        status, error_message = configuration.validate(config)
+        self.assertFalse(status)
+        self.assertEqual(error_message, 'The configuration parameter <num_threads> must be set to a positive '
+                                        'integer, but is currently set to <%s>.'%math.e)
+
+    def test_validate_str(self):
+        config = importer_mocks.get_basic_config(**{constants.CONFIG_NUM_THREADS: '2'})
+        status, error_message = configuration.validate(config)
+        self.assertTrue(status)
+        self.assertEqual(error_message, None)
+
+    def test_zero(self):
+        config = importer_mocks.get_basic_config(**{constants.CONFIG_NUM_THREADS: 0})
+        status, error_message = configuration.validate(config)
+        self.assertFalse(status)
+        self.assertEqual(error_message, 'The configuration parameter <num_threads> must be set to a positive '
+                                        'integer, but is currently set to <0>.')
+
+
+class TestValidateProxyPassword(PulpRPMTests):
+    def test_password_is_non_string(self):
+        config = importer_mocks.get_basic_config(**{constants.CONFIG_PROXY_PASSWORD: 7,
+                                                    constants.CONFIG_PROXY_USER: "the_dude"})
+        status, error_message = configuration.validate(config)
+        self.assertFalse(status)
+        self.assertEqual(error_message, "The configuration parameter <proxy_password> should be a string, "
+                                        "but it was <type 'int'>.")
+
+    def test_password_requires_url(self):
+        config = importer_mocks.get_basic_config(**{constants.CONFIG_PROXY_PASSWORD: 'duderino',
+                                                    constants.CONFIG_PROXY_USER: 'the_dude'})
+        status, error_message = configuration.validate(config)
+        self.assertFalse(status)
+        self.assertEqual(error_message, 'The configuration parameter <proxy_password> requires the '
+                                        '<proxy_url> parameter to also be set.')
+
+    def test_password_requires_username(self):
+        config = importer_mocks.get_basic_config(**{constants.CONFIG_PROXY_PASSWORD: 'duderino'})
+        status, error_message = configuration.validate(config)
+        self.assertFalse(status)
+        self.assertEqual(error_message, 'The configuration parameter <proxy_password> requires the '
+                                        '<proxy_user> parameter to also be set.')
+
+    def test_validate(self):
+        config = importer_mocks.get_basic_config(**{constants.CONFIG_PROXY_PASSWORD: 'duderino',
+                                                    constants.CONFIG_PROXY_USER: 'the_dude',
+                                                    constants.CONFIG_PROXY_URL: 'http://fake.com/'})
+        status, error_message = configuration.validate(config)
+        self.assertTrue(status)
+        self.assertEqual(error_message, None)
+
+
+class TestValidateProxyPort(PulpRPMTests):
+    def test_validate(self):
+        config = importer_mocks.get_basic_config(**{constants.CONFIG_PROXY_PORT: 8088,
+                                                    constants.CONFIG_PROXY_URL: 'http://proxy.com'})
+        status, error_message = configuration.validate(config)
+        self.assertTrue(status)
+        self.assertEqual(error_message, None)
+
+    def test_float(self):
+        config = importer_mocks.get_basic_config(**{constants.CONFIG_PROXY_PORT: math.pi,
+                                                    constants.CONFIG_PROXY_URL: 'http://test.com'})
+        status, error_message = configuration.validate(config)
+        self.assertFalse(status)
+        self.assertEqual(error_message, 'The configuration parameter <proxy_port> must be set to a positive '
+                                        'integer, but is currently set to <%s>.'%math.pi)
+
+    def test_float_str(self):
+        config = importer_mocks.get_basic_config(**{constants.CONFIG_PROXY_PORT: '%s'%math.e,
+                                                    constants.CONFIG_PROXY_URL: 'http://proxy.com'})
+        status, error_message = configuration.validate(config)
+        self.assertFalse(status)
+        self.assertEqual(error_message, 'The configuration parameter <proxy_port> must be set to a positive '
+                                        'integer, but is currently set to <%s>.'%math.e)
+
+    def test_port_requires_url(self):
+        config = importer_mocks.get_basic_config(**{constants.CONFIG_PROXY_PORT: 3128})
+        status, error_message = configuration.validate(config)
+        self.assertFalse(status)
+        self.assertEqual(error_message, 'The configuration parameter <proxy_port> requires the '
+                                        '<proxy_url> parameter to also be set.')
+
+    def test_validate_str(self):
+        config = importer_mocks.get_basic_config(**{constants.CONFIG_PROXY_PORT: '3128',
+                                                    constants.CONFIG_PROXY_URL: 'http://test.com'})
+        status, error_message = configuration.validate(config)
+        self.assertTrue(status)
+        self.assertEqual(error_message, None)
+
+    def test_zero(self):
+        config = importer_mocks.get_basic_config(**{constants.CONFIG_PROXY_PORT: 0,
+                                                    constants.CONFIG_PROXY_URL: 'http://test.com'})
+        status, error_message = configuration.validate(config)
+        self.assertFalse(status)
+        self.assertEqual(error_message, 'The configuration parameter <proxy_port> must be set to a positive '
+                                        'integer, but is currently set to <0>.')
+
+
+class TestValidateProxyURL(PulpRPMTests):
+    def test_url_is_non_string(self):
+        config = importer_mocks.get_basic_config(**{constants.CONFIG_PROXY_URL: 7})
+        status, error_message = configuration.validate(config)
+        self.assertFalse(status)
+        self.assertEqual(error_message, "The configuration parameter <proxy_url> should be a string, "
+                                        "but it was <type 'int'>.")
+
+    def test_validate(self):
+        config = importer_mocks.get_basic_config(**{constants.CONFIG_PROXY_URL: 'http://fake.com/'})
+        status, error_message = configuration.validate(config)
+        self.assertTrue(status)
+        self.assertEqual(error_message, None)
+
+
+class TestValidateProxyUsername(PulpRPMTests):
+    def test_username_is_non_string(self):
+        config = importer_mocks.get_basic_config(**{constants.CONFIG_PROXY_PASSWORD: 'bowling',
+                                                    constants.CONFIG_PROXY_USER: 185,
+                                                    constants.CONFIG_PROXY_URL: 'http://test.com'})
+        status, error_message = configuration.validate(config)
+        self.assertFalse(status)
+        self.assertEqual(error_message, "The configuration parameter <proxy_user> should be a string, "
+                                        "but it was <type 'int'>.")
+
+    def test_username_requires_url(self):
+        config = importer_mocks.get_basic_config(**{constants.CONFIG_PROXY_PASSWORD: 'duderino',
+                                                    constants.CONFIG_PROXY_USER: 'the_dude'})
+        status, error_message = configuration._validate_proxy_username(config)
+        self.assertFalse(status)
+        self.assertEqual(error_message, 'The configuration parameter <proxy_user> requires the '
+                                        '<proxy_url> parameter to also be set.')
+
+    def test_username_requires_password(self):
+        config = importer_mocks.get_basic_config(**{constants.CONFIG_PROXY_USER: 'the_dude'})
+        status, error_message = configuration._validate_proxy_username(config)
+        self.assertFalse(status)
+        self.assertEqual(error_message, 'The configuration parameter <proxy_user> requires the '
+                                        '<proxy_password> parameter to also be set.')
+
+    def test_validate(self):
+        config = importer_mocks.get_basic_config(**{constants.CONFIG_PROXY_PASSWORD: 'duderino',
+                                                    constants.CONFIG_PROXY_USER: 'the_dude',
+                                                    constants.CONFIG_PROXY_URL: 'http://fake.com/'})
+        status, error_message = configuration.validate(config)
+        self.assertTrue(status)
+        self.assertEqual(error_message, None)
+
+
+class TestValidateSSLOptions(PulpRPMTests):
+    def test_ca_cert_is_non_string(self):
+        config = importer_mocks.get_basic_config(**{constants.CONFIG_SSL_CA_CERT: 7})
+        status, error_message = configuration.validate(config)
+        self.assertFalse(status)
+        self.assertEqual(error_message, "The configuration parameter <ssl_ca_cert> should be a string, "
+                                        "but it was <type 'int'>.")
+
+    def test_client_cert_is_non_string(self):
+        config = importer_mocks.get_basic_config(**{constants.CONFIG_SSL_CLIENT_CERT: 8})
+        status, error_message = configuration.validate(config)
+        self.assertFalse(status)
+        self.assertEqual(error_message, "The configuration parameter <ssl_client_cert> should be a string, "
+                                        "but it was <type 'int'>.")
+
+    def test_client_key_is_non_string(self):
+        config = importer_mocks.get_basic_config(**{constants.CONFIG_SSL_CLIENT_KEY: 9})
+        status, error_message = configuration.validate(config)
+        self.assertFalse(status)
+        self.assertEqual(error_message, "The configuration parameter <ssl_client_key> should be a string, "
+                                        "but it was <type 'int'>.")
+
+    def test_client_key_requires_client_cert(self):
+        config = importer_mocks.get_basic_config(**{constants.CONFIG_SSL_CLIENT_KEY: 'Client Key!'})
+        status, error_message = configuration.validate(config)
+        self.assertFalse(status)
+        self.assertEqual(error_message, 'The configuration parameter <ssl_client_key> requires the '
+                                        '<ssl_client_cert> parameter to also be set.')
+
+    def test_validate(self):
+        config = importer_mocks.get_basic_config(**{constants.CONFIG_SSL_CA_CERT: 'CA Certificate!',
+                                                    constants.CONFIG_SSL_CLIENT_CERT: 'Client Certificate!'})
+        status, error_message = configuration.validate(config)
+        self.assertTrue(status)
+        self.assertEqual(error_message, None)
