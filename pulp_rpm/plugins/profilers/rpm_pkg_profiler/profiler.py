bd5f6e701ce3183a5a3cf9153b63131fe1b7d7a0
Sayli Karmarkar <skarmark@redhat.com>
2013-05-21 23:40:54 -0700

    removing unwanted additional step

--- pulp_rpm/plugins/profilers/rpm_pkg_profiler/profiler.py
+++ pulp_rpm/plugins/profilers/rpm_pkg_profiler/profiler.py
@@ -103,12 +103,11 @@ class RPMPkgProfiler(Profiler):
         if not consumer_profile_and_repo_ids:
             return reports
 
-        if criteria:
-            units = conduit.search_all_unit_ids(unit_type_id, criteria)
+        units = conduit.search_all_unit_ids(unit_type_id, criteria)
 
         # Collect applicability reports for each unit
         for unit in units:
-            applicable_consumers, rpm = self.find_applicable(unit, consumer_profile_and_repo_ids, conduit)
+            applicable_consumers, rpm = self.find_applicable(unit['_id'], consumer_profile_and_repo_ids, conduit)
             if applicable_consumers:
                 details = {}
                 summary = {'unit_key' : rpm.unit_key}
