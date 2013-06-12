03ecae5b9f02f1f5967c28360545c077b31665b2
Randy Barlow <rbarlow@redhat.com>
2013-06-12 14:10:24 -0400

    Thoroughly test pulp_rpm.extension.admin.iso.structure.
    This commit achieves 100% test coverage on the structure module.

--- pulp_rpm/test/unit/test_extension_admin_iso_structure.py
+++ pulp_rpm/test/unit/test_extension_admin_iso_structure.py
@@ -12,11 +12,14 @@
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
 from pulp.client.commands import unit
-from pulp.client.commands.repo import cudl, sync_publish
+from pulp.client.commands.repo import cudl, sync_publish, upload as pulp_upload
+from pulp.client.extensions.extensions import PulpCliSection
+from pulp.client.upload.manager import UploadManager
 import mock
 
 from pulp_rpm.common import ids
-from pulp_rpm.extension.admin.iso import contents, create_update, repo_list, structure
+from pulp_rpm.extension.admin.iso import (contents, create_update, repo_list, structure,
+                                          sync_schedules, upload)
 import rpm_support_base
 
 
@@ -79,13 +82,15 @@ class TestAddRepoSection(rpm_support_base.PulpClientTests):
         self.assertEqual(repo_section.name, structure.SECTION_REPO)
         self.assertEqual(repo_section.description, structure.DESC_REPO)
 
-        # The sync and publish sections should have been added as well
+        # The sync, publish, and uploads sections should have been added as well
         sync_section = repo_section.subsections[structure.SECTION_SYNC]
-        self.assertTrue(sync_section is not None)
+        self.assertTrue(isinstance(sync_section, PulpCliSection))
         publish_section = repo_section.subsections[structure.SECTION_PUBLISH]
-        self.assertTrue(publish_section is not None)
+        self.assertTrue(isinstance(publish_section, PulpCliSection))
+        uploads_section = repo_section.subsections[structure.SECTION_UPLOADS]
+        self.assertTrue(isinstance(uploads_section, PulpCliSection))
 
-        # There should be five commands
+        # There should be seven commands
         self.assertEqual(len(repo_section.commands), 7)
 
         # The create command should have been added
@@ -126,11 +131,50 @@ class TestAddRepoSection(rpm_support_base.PulpClientTests):
         self.assertEqual(content_command.context, self.context)
 
 
+class TestAddSchedulesSection(rpm_support_base.PulpClientTests):
+    """
+    Test the add_schedules_section() function.
+    """
+    def test_add_schedules_section(self):
+        parent_section = self.cli.create_section('parent', 'Test parent section.')
+
+        structure.add_schedules_section(self.context, parent_section)
+
+        schedules_section = parent_section.subsections['schedules']
+        self.assertEqual(schedules_section.name, structure.SECTION_SCHEDULES)
+        self.assertEqual(schedules_section.description, structure.DESC_SCHEDULES)
+
+        # Assert that the correct schedules commands are present
+        self.assertEqual(len(schedules_section.commands), 5)
+
+        create_command = schedules_section.commands['create']
+        self.assertTrue(isinstance(create_command, sync_schedules.ISOCreateScheduleCommand))
+        self.assertEqual(create_command.context, self.context)
+
+        delete_command = schedules_section.commands['delete']
+        self.assertTrue(isinstance(delete_command, sync_schedules.ISODeleteScheduleCommand))
+        self.assertEqual(delete_command.context, self.context)
+
+        list_command = schedules_section.commands['list']
+        self.assertTrue(isinstance(list_command, sync_schedules.ISOListScheduleCommand))
+        self.assertEqual(list_command.context, self.context)
+
+        next_command = schedules_section.commands['next']
+        self.assertTrue(isinstance(next_command, sync_schedules.ISONextRunCommand))
+        self.assertEqual(next_command.context, self.context)
+
+        update_command = schedules_section.commands['update']
+        self.assertTrue(isinstance(update_command, sync_schedules.ISOUpdateScheduleCommand))
+        self.assertEqual(update_command.context, self.context)
+
+
 class TestAddSyncSection(rpm_support_base.PulpClientTests):
     """
     Test the add_sync_section() function.
     """
-    def test_add_sync_section(self):
+    @mock.patch('pulp_rpm.extension.admin.iso.structure.add_schedules_section',
+                side_effect=structure.add_schedules_section, autospec=True)
+    def test_add_sync_section(self, add_schedules_section):
         parent_section = self.cli.create_section('parent', 'Test parent section.')
 
         structure.add_sync_section(self.context, parent_section)
@@ -143,3 +187,64 @@ class TestAddSyncSection(rpm_support_base.PulpClientTests):
         # The run command should have been added to the sync_section
         run_command = sync_section.commands['run']
         self.assertTrue(isinstance(run_command, sync_publish.RunSyncRepositoryCommand))
+
+        # The schedules subsection should have been added as well
+        add_schedules_section.assert_called_once_with(self.context, sync_section)
+
+
+class TestAddUploadsSection(rpm_support_base.PulpClientTests):
+    """
+    Test the add_uploads_section() function.
+    """
+    @mock.patch('pulp_rpm.extension.admin.iso.structure._get_upload_manager')
+    def test_add_uploads_section(self, _get_upload_manager):
+        parent_section = self.cli.create_section('parent', 'Test parent section.')
+        fake_upload_manager = mock.MagicMock()
+        _get_upload_manager.return_value = fake_upload_manager
+
+        structure.add_uploads_section(self.context, parent_section)
+
+        uploads_section = parent_section.subsections['uploads']
+        self.assertEqual(uploads_section.name, structure.SECTION_UPLOADS)
+        self.assertEqual(uploads_section.description, structure.DESC_UPLOADS)
+
+        # Check out the upload commands
+        self.assertEqual(len(uploads_section.commands), 4)
+
+        upload_command = uploads_section.commands['upload']
+        self.assertTrue(isinstance(upload_command, upload.UploadISOCommand))
+        self.assertEqual(upload_command.context, self.context)
+        self.assertEqual(upload_command.upload_manager, fake_upload_manager)
+
+        resume_command = uploads_section.commands['resume']
+        self.assertTrue(isinstance(resume_command, pulp_upload.ResumeCommand))
+        self.assertEqual(resume_command.context, self.context)
+        self.assertEqual(resume_command.upload_manager, fake_upload_manager)
+
+        cancel_command = uploads_section.commands['cancel']
+        self.assertTrue(isinstance(cancel_command, pulp_upload.CancelCommand))
+        self.assertEqual(cancel_command.context, self.context)
+        self.assertEqual(cancel_command.upload_manager, fake_upload_manager)
+
+        list_command = uploads_section.commands['list']
+        self.assertTrue(isinstance(list_command, pulp_upload.ListCommand))
+        self.assertEqual(list_command.context, self.context)
+        self.assertEqual(list_command.upload_manager, fake_upload_manager)
+
+
+class TestGetUploadManager(rpm_support_base.PulpClientTests):
+    """
+    Test the _get_upload_manager() function.
+    """
+    @mock.patch('pulp.client.upload.manager.UploadManager.initialize', autospec=True)
+    def test__get_upload_manager(self, initialize):
+        self.context.config['filesystem'] = {'upload_working_dir': '/path/to/nowhere'}
+        self.context.config['server'] = {'upload_chunk_size': 42}
+
+        upload_manager = structure._get_upload_manager(self.context)
+
+        initialize.assert_called_once_with(upload_manager)
+        self.assertTrue(isinstance(upload_manager, UploadManager))
+        self.assertEqual(upload_manager.upload_working_dir, '/path/to/nowhere')
+        self.assertEqual(upload_manager.bindings, self.context.server)
+        self.assertEqual(upload_manager.chunk_size, 42)
