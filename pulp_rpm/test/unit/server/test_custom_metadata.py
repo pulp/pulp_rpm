20e931f11c39dcabcffbaf7029e97b20ace02d1d
Jason L Connor <jason.connor@gmail.com>
2013-06-12 23:40:17 -0600

    converting unit tests from remote to local feeds

--- pulp_rpm/test/unit/server/test_custom_metadata.py
+++ pulp_rpm/test/unit/server/test_custom_metadata.py
@@ -15,7 +15,6 @@ import os
 import shutil
 import sys
 import tempfile
-from pprint import pprint
 
 import mock
 
@@ -27,11 +26,29 @@ sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)) + "/../../../plugi
 
 from yum_importer.importer import YumImporter
 
+import data_dir
+import http_static_test_server
 import mock_conduits
 import rpm_support_base
 
 
+TEST_DRPM_REPO_FEED = 'http://localhost:8088/%s/test_drpm_repo/published/test_drpm_repo/' % \
+    data_dir.relative_path_to_data_dir()
+
+
 class CustomMetadataTests(rpm_support_base.PulpRPMTests):
+
+    @classmethod
+    def setUpClass(cls):
+        super(CustomMetadataTests, cls).setUpClass()
+        cls.server = http_static_test_server.HTTPStaticTestServer()
+        cls.server.start()
+
+    @classmethod
+    def tearDownClass(cls):
+        super(CustomMetadataTests, cls).tearDownClass()
+        cls.server.stop()
+        cls.server = None
 
     def setUp(self):
         super(CustomMetadataTests, self).setUp()
@@ -46,7 +63,7 @@ class CustomMetadataTests(rpm_support_base.PulpRPMTests):
     def _mock_repo(self, repo_id):
         repo = mock.Mock(spec=Repository)
         repo.id = repo_id
-        repo.working_dir = os.path.join(self.root_dir, repo_id, 'working')
+        repo.working_dir = os.path.join(self.root_dir, 'working')
         os.makedirs(repo.working_dir)
         return repo
 
@@ -59,8 +76,8 @@ class CustomMetadataTests(rpm_support_base.PulpRPMTests):
         importer = YumImporter()
         repo = self._mock_repo('test-presto-delta-metadata')
         sync_conduit = mock_conduits.repo_sync_conduit(self.content_dir)
-        feed_url = "http://repos.fedorapeople.org/repos/pulp/pulp/demo_repos/test_drpm_repo/"
-        config = self._plugin_config(feed_url)
+        #feed_url = 'http://repos.fedorapeople.org/repos/pulp/pulp/demo_repos/test_drpm_repo/'
+        config = self._plugin_config(TEST_DRPM_REPO_FEED)
 
         importer._sync_repo(repo, sync_conduit, config)
 
@@ -73,4 +90,5 @@ class CustomMetadataTests(rpm_support_base.PulpRPMTests):
 
         self.assertEqual(unit.type_id, TYPE_ID_YUM_REPO_METADATA_FILE)
         self.assertEqual(unit.unit_key['data_type'], 'prestodelta')
+
 
