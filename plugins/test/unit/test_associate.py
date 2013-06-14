eb2aec2eb6e772baab27a954dcb2f1ba94b4c919
Michael Hrivnak <mhrivnak@redhat.com>
2013-06-14 17:19:10 -0400

    adding unit tests for the "purge" module

--- plugins/test/unit/test_associate.py
+++ plugins/test/unit/test_associate.py
@@ -11,7 +11,6 @@
 # have received a copy of GPLv2 along with this software; if not, see
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
-from itertools import count
 import unittest
 
 import mock
@@ -19,31 +18,10 @@ from pulp.plugins.conduits.unit_import import ImportUnitConduit
 from pulp.plugins.model import Unit, Repository
 import pulp.server.managers.factory as manager_factory
 
-from pulp_rpm.common import models
+import model_factory
 from pulp_rpm.plugins.importers.yum import associate
 
 manager_factory.initialize()
-
-rpm_counter = count()
-
-def rpm_model_factory():
-    count = rpm_counter.next()
-    return models.RPM(
-        'name-%d' % count,
-        0,
-        '2.1.%d' % count,
-        '1-1',
-        'x86_64',
-        'sha256',
-        'somehash-%d' % count,
-        {}
-    )
-
-yum_md_file_counter = count()
-
-def yum_md_file_factory():
-    return models.YumMetadataFile(models.YumMetadataFile.TYPE,
-                                  'repo-%d' % yum_md_file_counter.next(), {})
 
 
 class TestAssociateUnit(unittest.TestCase):
@@ -52,7 +30,7 @@ class TestAssociateUnit(unittest.TestCase):
 
     @mock.patch('shutil.copyfile')
     def test_rpm(self, mock_copyfile):
-        model = rpm_model_factory()
+        model = model_factory.rpm_models(1)[0]
         unit = Unit(model.TYPE, model.unit_key, model.metadata, '/')
 
         # passing "None" ensures that the importer isn't being called
@@ -64,7 +42,7 @@ class TestAssociateUnit(unittest.TestCase):
     @mock.patch('shutil.copyfile')
     def test_yum_md_file(self, mock_copyfile):
         mock_conduit = mock.MagicMock(spec_set=ImportUnitConduit('', '', '', '', '', ''))
-        model = yum_md_file_factory()
+        model = model_factory.yum_md_file()
         unit = Unit(model.TYPE, model.unit_key, model.metadata, '/foo/bar')
 
         ret = associate._associate_unit(self.repo, mock_conduit, unit)
