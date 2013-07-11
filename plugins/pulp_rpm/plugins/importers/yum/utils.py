4afa6eaf16e957d86d7617c2b851d716defe0bf5
Michael Hrivnak <mhrivnak@redhat.com>
2013-07-11 14:51:10 -0400

    983323 - fixed an XML parsing incompatibility with python 2.6 where the default XML namespace was being mishandled.

--- plugins/pulp_rpm/plugins/importers/yum/utils.py
+++ plugins/pulp_rpm/plugins/importers/yum/utils.py
@@ -28,7 +28,6 @@ else:
 DEFAULT_PAGE_SIZE = 1000
 
 # required for converting an element to raw XML
-STRIP_XMLNS_RE = re.compile('(<package )(?:xmlns.*? )+')
 STRIP_NS_RE = re.compile('{.*?}')
 Namespace = namedtuple('Namespace', ['name', 'uri'])
 
@@ -59,7 +58,7 @@ def paginate(iterable, page_size=DEFAULT_PAGE_SIZE):
         yield page
 
 
-def element_to_raw_xml(element, namespaces=None):
+def element_to_raw_xml(element, namespaces_to_register=None, default_namespace_uri=None):
     """
     Convert an Element to a raw XML block. Namespaces are preserved on element
     tags, but any namespace declaration on the root element will be removed.
@@ -67,28 +66,41 @@ def element_to_raw_xml(element, namespaces=None):
     snippets will be concatenated into a valid XML document later. This function
     will not necessarily return a valid XML document.
 
-    :param element:     XML element that should be output as an XML string
-    :type  element:     xml.etree.ElementTree.Element
-    :param namespaces:  collection of Namespace instances that should be registered
-                        while converting to a string.
-    :type  namespaces:  list or tuple
+    :param element:                 XML element that should be output as an XML string
+    :type  element:                 xml.etree.ElementTree.Element
+    :param namespaces_to_register:  collection of Namespace instances that should
+                                    be registered while converting to a string.
+                                    Any "xmlns" declarations for a prefix that
+                                    appears in this collection will be removed.
+    :type  namespaces_to_register:  list or tuple
+    :param default_namespace_uri:   URI of the default namespace, if any. This
+                                    will be stripped from the tag of the given
+                                    element and any of its descendants.
 
     :return:    XML as a string
     :rtype:     str
     """
-    namespaces = namespaces or tuple()
-    for namespace in namespaces:
+    namespaces_to_register = namespaces_to_register or tuple()
+    for namespace in namespaces_to_register:
         if not isinstance(namespace, Namespace):
             raise TypeError('"namespaces" must be an iterable of Namespace instances')
         register_namespace(namespace.name, namespace.uri)
 
+    if default_namespace_uri:
+        strip_ns(element, default_namespace_uri)
+
     tree = ET.ElementTree(element)
     io = StringIO()
     tree.write(io)
-    # clean up, since this is global
-    for namespace in namespaces:
+    ret = io.getvalue()
+
+    for namespace in namespaces_to_register:
+        # clean up, since this is global
         register_namespace(namespace.name, '')
-    return re.sub(STRIP_XMLNS_RE, r'\1', io.getvalue())
+        # in python 2.7, these show up only on the root element. in 2.6, these
+        # show up on each element that uses the prefix.
+        ret = re.sub(' *xmlns:%s="%s" *' % (namespace.name, namespace.uri), ' ', ret)
+    return ret
 
 
 @check_builtin(ET)
@@ -107,16 +119,22 @@ def register_namespace(prefix, uri):
     ET._namespace_map[uri] = prefix
 
 
-def strip_ns(element):
+def strip_ns(element, uri=None):
     """
     Given an Element object, recursively strip the namespace info from its tag
-    and all of its children.
+    and all of its children. If a URI is specified, only that URI will be removed
+    from the tag.
 
+    :param element: the element whose tag namespace should be modified
     :type  element: xml.etree.ElementTree.Element
-
-    :return:    same element object that was passed in
-    :rtype:     xml.etree.ElementTree.Element
+    :param uri:     The URI of a namespace that should be stripped. If specified,
+                    only this URI will be removed, and all others will be left
+                    alone.
+    :type  uri:     basestring
     """
-    element.tag = re.sub(STRIP_NS_RE, '', element.tag)
+    if uri is None:
+        element.tag = re.sub(STRIP_NS_RE, '', element.tag)
+    else:
+        element.tag = element.tag.replace('{%s}' % uri, '')
     for child in list(element):
-        strip_ns(child)
+        strip_ns(child, uri)
