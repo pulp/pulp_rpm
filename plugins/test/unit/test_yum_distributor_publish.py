4f8791267f225da4afcd66e3218c04976e403897
Jason L Connor <jason.connor@gmail.com>
2013-11-12 09:59:05 -0700

    fixed up listing file unit tests

--- plugins/test/unit/test_yum_distributor_publish.py
+++ plugins/test/unit/test_yum_distributor_publish.py
@@ -302,10 +302,11 @@ class YumDistributorPublishTests(unittest.TestCase):
             path = os.path.join(self.published_dir, 'http', self.publisher.repo.id, 'content', u.unit_key['name'])
             self.assertTrue(os.path.exists(path))
 
-        listing_paths= [os.path.join(self.published_dir, 'listing'),
-                        os.path.join(self.published_dir, 'http', 'listing')]
-        for p in listing_paths:
-            self.assertTrue(os.path.exists(p))
+        listing_path = os.path.join(self.published_dir, 'http', 'listing')
+        self.assertTrue(os.path.exists(listing_path))
+
+        listing_content = open(listing_path, 'r').read()
+        self.assertEqual(listing_content, self.publisher.repo.id)
 
         self.assertEqual(self.publisher.progress_report[reporting.PUBLISH_OVER_HTTP_STEP][reporting.PROCESSED], 1)
         self.assertEqual(self.publisher.progress_report[reporting.PUBLISH_OVER_HTTP_STEP][reporting.FAILURES], 0)
@@ -333,10 +334,11 @@ class YumDistributorPublishTests(unittest.TestCase):
             path = os.path.join(self.published_dir, 'https', self.publisher.repo.id, 'content', u.unit_key['name'])
             self.assertTrue(os.path.exists(path))
 
-        listing_paths= [os.path.join(self.published_dir, 'listing'),
-                        os.path.join(self.published_dir, 'https', 'listing')]
-        for p in listing_paths:
-            self.assertTrue(os.path.exists(p))
+        listing_path= os.path.join(self.published_dir, 'https', 'listing')
+        self.assertTrue(os.path.exists(listing_path))
+
+        listing_content = open(listing_path, 'r').read()
+        self.assertEqual(listing_content, self.publisher.repo.id)
 
         self.assertEqual(self.publisher.progress_report[reporting.PUBLISH_OVER_HTTPS_STEP][reporting.PROCESSED], 1)
         self.assertEqual(self.publisher.progress_report[reporting.PUBLISH_OVER_HTTPS_STEP][reporting.FAILURES], 0)
