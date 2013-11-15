8bc9c3c096bd0e9bf55a3b06d156a2736c0a9570
Jeff Ortel <jortel@redhat.com>
2013-11-15 16:14:55 -0600

    Initial add of yum cataloger.

---
+++ plugins/pulp_rpm/plugins/catalogers/yum.py
@@ -0,0 +1,92 @@
+# Copyright (c) 2013 Red Hat, Inc.
+#
+# This software is licensed to you under the GNU General Public
+# License as published by the Free Software Foundation; either version
+# 2 of the License (GPLv2) or (at your option) any later version.
+# There is NO WARRANTY for this software, express or implied,
+# including the implied warranties of MERCHANTABILITY,
+# NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
+# have received a copy of GPLv2 along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+
+import shutil
+
+from tempfile import mkdtemp
+from urlparse import urljoin
+
+from pulp.plugins.cataloger import Cataloger
+from pulp.plugins.util.nectar_config import importer_config_to_nectar_config
+
+from pulp_rpm.common import models
+from pulp_rpm.plugins.importers.yum.repomd.metadata import MetadataFiles
+from pulp_rpm.plugins.importers.yum.repomd import primary
+from pulp_rpm.plugins.importers.yum.repomd import packages
+
+
+
+TYPE_ID = 'yum'
+URL = 'url'
+
+
+def entry_point():
+    """
+    The Pulp platform uses this method to load the profiler.
+    :return: YumCataloger class and an (empty) config
+    :rtype:  tuple
+    """
+    return YumCataloger, {}
+
+
+class YumCataloger(Cataloger):
+
+    @classmethod
+    def metadata(cls):
+        return {
+            'id': TYPE_ID,
+            'display_name': "Yum Cataloger",
+            'types': [models.RPM.TYPE]
+        }
+
+    @staticmethod
+    def _add_packages(base_url, md_files):
+        with md_files.get_metadata_file_handle(primary.METADATA_FILE_NAME) as fp:
+            _packages = packages.package_list_generator(
+                fp, primary.PACKAGE_TAG, primary.process_package_element)
+            for model in _packages:
+                unit_key = model.unit_key
+                url = urljoin(base_url, model.download_path)
+                conduit.add_entry(source_id, models.RPM.TYPE, unit_key, url)
+
+    def refresh(self, source_id, conduit, config):
+        conduit.purge(source_id)
+        url = config[URL]
+        dst_dir = mkdtemp()
+        try:
+            nectar_config = importer_config_to_nectar_config(config)
+            md_files = MetadataFiles(url, dst_dir, nectar_config)
+            md_files.download_repomd()
+            md_files.parse_repomd()
+            md_files.download_metadata_files()
+            self._add_packages(url, md_files)
+        finally:
+            shutil.rmtree(dst_dir)
+
+
+class Conduit(object):
+    def add_entry(self, *args):
+        pass
+    def purge(self, *args):
+        pass
+
+
+if __name__ == '__main__':
+    from logging import basicConfig
+    from pulp.plugins.conduits.cataloger import CatalogerConduit
+    basicConfig()
+    config = {
+        URL: 'http://repos.fedorapeople.org/repos/pulp/pulp/beta/2.3/5Server/x86_64/'
+    }
+    source_id = 'test'
+    conduit = Conduit()
+    cataloger = YumCataloger()
+    cataloger.refresh(source_id, conduit, config)
