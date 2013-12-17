673e043f1e8897403166b9f07bcb8f94c270db57
Jason L Connor <jason.connor@gmail.com>
2013-12-17 11:13:14 -0700

    added clearing of orphaned parent publish directories; added docstrings

--- pulp_rpm/src/pulp_rpm/migrations/0015_new_yum_distributor.py
+++ pulp_rpm/src/pulp_rpm/migrations/0015_new_yum_distributor.py
@@ -11,6 +11,15 @@
 # You should have received a copy of GPLv2 along with this software;
 # if not, see http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+"""
+The new distributor now stores the master copy of a published repository outside
+of the distributor's working directory. It also stores the published repositories
+in a new location.
+
+This script cleans up all of the aforementioned directories and re-publishes the
+corresponding repositories.
+"""
+
 import os
 import shutil
 
@@ -28,6 +37,10 @@ OLD_HTTPS_PUBLISH_DIR = os.path.join(OLD_ROOT_PUBLISH_DIR, 'https', 'repos')
 
 
 def migrate(*args, **kwargs):
+    """
+    For each repository with a yum distributor, clean up the old yum distributor's
+    mess and re-publish the repository with the new distributor.
+    """
 
     distributor_collection = RepoDistributor.get_collection()
     yum_distributors = list(distributor_collection.find({'distributor_type_id': YUM_DISTRIBUTOR_ID}))
@@ -49,6 +62,9 @@ def migrate(*args, **kwargs):
 
 
 def _clear_working_dir(repo):
+    """
+    Clear out the repository's distributor's working directory
+    """
 
     working_dir = REPO_WORKING_DIRECTORY % repo['id']
 
@@ -62,14 +78,16 @@ def _clear_working_dir(repo):
                 shutil.rmtree(p, ignore_errors=True)
 
             else:
-                try:
-                    os.unlink(p)
-                except:
-                    # this is a best-effort kinda thing
-                    pass
+                try: os.unlink(p)
+                # this is a best-effort kinda thing
+                except: pass
 
 
 def _clear_old_publish_dirs(repo, config):
+    """
+    Remove the repository's old http and https publish directories (if they exist),
+    including any parent directories that may fall empty as a result.
+    """
 
     relative_publish_dir = config.get('relative_url', repo.id) or repo.id
 
@@ -81,15 +99,57 @@ def _clear_old_publish_dirs(repo, config):
 
     if os.path.exists(repo_http_publish_dir):
         shutil.rmtree(repo_http_publish_dir, ignore_errors=True)
+        _clear_orphaned_publish_dirs(http_publish_dir, os.path.dirname(repo_http_publish_dir))
 
     https_publish_dir = config.get('https_publish_dir', OLD_HTTPS_PUBLISH_DIR)
     repo_https_publish_dir = os.path.join(https_publish_dir, relative_publish_dir)
 
     if os.path.exists(repo_https_publish_dir):
         shutil.rmtree(repo_https_publish_dir, ignore_errors=True)
+        _clear_orphaned_publish_dirs(https_publish_dir, os.path.dirname(repo_https_publish_dir))
+
+
+def _clear_orphaned_publish_dirs(root_dir, publish_dir):
+    """
+    Clear out empty parent directories underneath a root publish directory.
+    """
+
+    if not publish_dir.startswith(root_dir):
+        return
+
+    if root_dir.endswith('/'):
+        root_dir = root_dir[:-1]
+
+    if publish_dir.endswith('/'):
+        publish_dir = publish_dir[:-1]
+
+    if publish_dir == root_dir:
+        return
+
+    if not os.path.exists(publish_dir):
+        return
+
+    contents = os.listdir(publish_dir)
+
+    if contents == ['listing'] or not contents:
+
+        listing_path = os.path.join(publish_dir, 'listing')
+
+        if os.path.exists(listing_path):
+            try: os.unlink(listing_path)
+            except: pass
+
+        os.rmdir(publish_dir)
+
+    _clear_orphaned_publish_dirs(root_dir, os.path.dirname(publish_dir))
 
 
 def _re_publish_repository(repo, distributor):
+    """
+    Re-publish the repository using the new yum distributor.
+
+    NOTE: this may be a bit time-consuming.
+    """
 
     manager = RepoPublishManager()
     manager.publish(repo['id'], distributor['id'])
