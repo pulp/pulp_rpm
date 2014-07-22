e0c0df73117d9d18f93b4c3b81c595b5f9df57ce
Barnaby Court <bcourt@redhat.com>
2014-07-22 12:41:42 -0400

    Update for platform changes to FastForwardXml metadata generator

--- plugins/test/unit/plugins/distributors/yum/metadata/test_filelists.py
+++ plugins/test/unit/plugins/distributors/yum/metadata/test_filelists.py
@@ -1,7 +1,6 @@
 import shutil
 import tempfile
 import unittest
-from xml.dom import pulldom
 
 import mock
 
@@ -20,30 +19,6 @@ class FilelistsXMLFileContextTests(unittest.TestCase):
         context = FilelistsXMLFileContext(self.working_dir, 3)
         self.assertEquals(context.fast_forward, False)
         self.assertEquals(context.num_packages, 3)
-
-    def test_initialize_no_fast_forward(self):
-        context = FilelistsXMLFileContext(self.working_dir, 3)
-        # test to make sure we don't fail on initializing without fast forwarding
-        context.initialize()
-
-    def test_initialize_fast_forward(self):
-        context = FilelistsXMLFileContext(self.working_dir, 3)
-        context._open_metadata_file_handle = mock.Mock()
-        context._write_file_header = mock.Mock()
-        context.fast_forward = True
-        node = mock.Mock(nodeName='filelists', attributes={'packages': mock.Mock(value='2')})
-        end_node = mock.Mock(nodeName='filelists')
-        node2 = mock.Mock(nodeName='filelists', attributes={'packages': mock.Mock(value='2')})
-
-        context.xml_generator = [(pulldom.START_ELEMENT, node),
-                                 (pulldom.END_ELEMENT, end_node),
-                                 (pulldom.START_ELEMENT, node2)]
-
-        context.initialize()
-
-        self.assertEquals('5', node.attributes['packages'].value)
-        # Check that we stopped processing at the end tag
-        self.assertEquals('2', node2.attributes['packages'].value)
 
     def test_add_unit_metadata(self):
         context = FilelistsXMLFileContext(self.working_dir, 3)
--- plugins/test/unit/plugins/distributors/yum/metadata/test_other.py
+++ plugins/test/unit/plugins/distributors/yum/metadata/test_other.py
@@ -1,7 +1,6 @@
 import shutil
 import tempfile
 import unittest
-from xml.dom import pulldom
 
 import mock
 
@@ -20,28 +19,6 @@ class PrimaryXMLFileContextTests(unittest.TestCase):
     def test_init(self):
         self.assertEquals(self.context.fast_forward, False)
         self.assertEquals(self.context.num_packages, 3)
-
-    def test_initialize_no_fast_forward(self):
-        # test to make sure we don't fail on initializing without fast forwarding
-        self.context.initialize()
-
-    def test_initialize_fast_forward(self):
-        self.context._open_metadata_file_handle = mock.Mock()
-        self.context._write_file_header = mock.Mock()
-        self.context.fast_forward = True
-        node = mock.Mock(nodeName='otherdata', attributes={'packages': mock.Mock(value='2')})
-        end_node = mock.Mock(nodeName='otherdata')
-        node2 = mock.Mock(nodeName='otherdata', attributes={'packages': mock.Mock(value='2')})
-
-        self.context.xml_generator = [(pulldom.START_ELEMENT, node),
-                                      (pulldom.END_ELEMENT, end_node),
-                                      (pulldom.START_ELEMENT, node2)]
-
-        self.context.initialize()
-
-        self.assertEquals('5', node.attributes['packages'].value)
-        # Check that we stopped processing at the end tag
-        self.assertEquals('2', node2.attributes['packages'].value)
 
     def test_add_unit_metadata(self):
         self.context.metadata_file_handle = mock.Mock()
