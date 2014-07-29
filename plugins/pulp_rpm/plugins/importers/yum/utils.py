4df5c7a6f80026a2c71912b4a40d61c35dea3f2b
Michael Hrivnak <mhrivnak@redhat.com>
2014-07-29 09:49:49 -0400

    moving "paginate" utility to platform

--- plugins/pulp_rpm/plugins/importers/yum/utils.py
+++ plugins/pulp_rpm/plugins/importers/yum/utils.py
@@ -13,37 +13,9 @@ else:
     from xml.etree import cElementTree as ET
 
 
-DEFAULT_PAGE_SIZE = 1000
-
 # required for converting an element to raw XML
 STRIP_NS_RE = re.compile('{.*?}')
 Namespace = namedtuple('Namespace', ['name', 'uri'])
-
-
-# TODO: probably should move this to pulp.common
-def paginate(iterable, page_size=DEFAULT_PAGE_SIZE):
-    """
-    Takes any kind of iterable and chops it up into tuples of size "page_size".
-    A generator is returned, so this can be an efficient way to chunk items from
-    some other generator.
-
-    :param iterable:    any iterable such as a list, tuple, or generator
-    :type  iterable:    iterable
-    :param page_size:   how many items should be in each returned tuple
-    :type  page_size:   int
-
-    :return:    generator of tuples, each including "page_size" number of items
-                from "iterable", except the last tuple, which may contain
-                fewer.
-    :rtype:     generator
-    """
-    # this won't work properly if we give islice something that isn't a generator
-    generator = (x for x in iterable)
-    while True:
-        page = tuple(itertools.islice(generator, 0, page_size))
-        if not page:
-            return
-        yield page
 
 
 def element_to_raw_xml(element, namespaces_to_register=None, default_namespace_uri=None):
--- plugins/test/unit/test_util.py
+++ plugins/test/unit/test_util.py
@@ -1,4 +1,3 @@
-import inspect
 import re
 import unittest
 from xml.etree import cElementTree as ET
@@ -120,77 +119,6 @@ class TestStripNS(unittest.TestCase):
         self.assertFalse(element.tag.startswith('{'))
         for child in element:
             self._check_all_elements(child)
-
-
-class TestPaginate(unittest.TestCase):
-    def test_list(self):
-        iterable = list(range(10))
-        ret = utils.paginate(iterable, 3)
-
-        self.assertTrue(inspect.isgenerator(ret))
-
-        pieces = list(ret)
-
-        self.assertEqual(pieces, [(0,1,2), (3,4,5), (6,7,8), (9,)])
-
-    def test_list_one_page(self):
-        iterable = list(range(10))
-        ret = utils.paginate(iterable, 100)
-
-        self.assertTrue(inspect.isgenerator(ret))
-
-        pieces = list(ret)
-
-        self.assertEqual(pieces, [tuple(range(10))])
-
-    def test_empty_list(self):
-        ret = utils.paginate([], 3)
-
-        self.assertTrue(inspect.isgenerator(ret))
-
-        pieces = list(ret)
-
-        self.assertEqual(pieces, [])
-
-    def test_tuple(self):
-        iterable = tuple(range(10))
-        ret = utils.paginate(iterable, 3)
-
-        self.assertTrue(inspect.isgenerator(ret))
-
-        pieces = list(ret)
-
-        self.assertEqual(pieces, [(0,1,2), (3,4,5), (6,7,8), (9,)])
-
-    def test_tuple_one_page(self):
-        iterable = tuple(range(10))
-        ret = utils.paginate(iterable, 100)
-
-        self.assertTrue(inspect.isgenerator(ret))
-
-        pieces = list(ret)
-
-        self.assertEqual(pieces, [tuple(range(10))])
-
-    def test_generator(self):
-        iterable = (x for x in range(10))
-        ret = utils.paginate(iterable, 3)
-
-        self.assertTrue(inspect.isgenerator(ret))
-
-        pieces = list(ret)
-
-        self.assertEqual(pieces, [(0,1,2), (3,4,5), (6,7,8), (9,)])
-
-    def test_generator_one_page(self):
-        iterable = (x for x in range(10))
-        ret = utils.paginate(iterable, 100)
-
-        self.assertTrue(inspect.isgenerator(ret))
-
-        pieces = list(ret)
-
-        self.assertEqual(pieces, [tuple(range(10))])
 
 
 PRIMARY_XML = """<?xml version="1.0" encoding="UTF-8"?>
