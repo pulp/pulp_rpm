da2b179b11ebbf630d840fffe21132b875f9d769
Dennis Kliban <dkliban@redhat.com>
2015-01-14 15:39:11 -0500

    Fixed doc strings and other PR comments

--- plugins/pulp_rpm/plugins/migrations/0003_custom_metadata_on_scratchpad.py
+++ plugins/pulp_rpm/plugins/migrations/0003_custom_metadata_on_scratchpad.py
@@ -21,11 +21,13 @@ def repository_working_dir(repo_id, mkdir=True):
     the directory will be created as part of this call.
     See the module-level docstrings for more information on the directory
     structure.
-    @param mkdir: if true, this call will create the directory; otherwise the
+    :param repo_id: repository id
+    :type  repo_id: str
+    :param mkdir: if true, this call will create the directory; otherwise the
                   full path will just be generated
-    @type  mkdir: bool
-    @return: full path on disk
-    @rtype:  str
+    :type  mkdir: bool
+    :return: full path on disk
+    :rtype:  str
     """
     working_dir = os.path.join(_repo_working_dir(), repo_id)
 
@@ -42,12 +44,16 @@ def importer_working_dir(importer_type_id, repo_id, mkdir=True):
     part of this call.
     See the module-level docstrings for more information on the directory
     structure.
-    @param mkdir: if true, this call will create the directory; otherwise the
+    :param importer_type_id: id of importer type
+    :type  importer_type_id: str
+    :param repo_id: id of repository
+    :type  repo_id: str
+    :param mkdir: if true, this call will create the directory; otherwise the
                   full path will just be generated
-    @type  mkdir: bool
-    @return: full path on disk to the directory the importer can use for the
+    :type  mkdir: bool
+    :return: full path on disk to the directory the importer can use for the
              given repository
-    @rtype:  str
+    :rtype:  str
     """
     repo_working_dir = repository_working_dir(repo_id, mkdir)
     working_dir = os.path.join(repo_working_dir, 'importers', importer_type_id)
@@ -59,14 +65,21 @@ def importer_working_dir(importer_type_id, repo_id, mkdir=True):
 
 
 def _working_dir_root():
+    """
+    Returns the path 'working' directory inside of the 'storage_dir' as defined in the [server]
+    section of the config. The default value is /var/lib/pulp/working.
+    """
     storage_dir = pulp_config.config.get('server', 'storage_dir')
     dir_root = os.path.join(storage_dir, 'working')
     return dir_root
 
 
 def _repo_working_dir():
-    dir = os.path.join(_working_dir_root(), 'repos')
-    return dir
+    """
+    Returns the path inside the working directory dedicated to repositories. The default value is
+    /var/lib/pulp/working/repos.
+    """
+    return os.path.join(_working_dir_root(), 'repos')
 
 
 def preserve_custom_metadata_on_repo_scratchpad():
