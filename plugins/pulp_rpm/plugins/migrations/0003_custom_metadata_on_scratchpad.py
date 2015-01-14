780f1e58b0c1f0f8d16ea3ec87aa4178f8186dd2
Dennis Kliban <dkliban@redhat.com>
2015-01-14 14:24:33 -0500

    Moved some helper functions from pulp server codebase to pulp_rpm
    This is part of refactoring that was needed to enable pulp workers to have local
    working directories. These functions no longer get used anywhere other than
    plugins/pulp_rpm/plugins/migrations/0003_custom_metadata_on_scratchpad.py
    
    https://pulp.plan.io/issues/106

--- plugins/pulp_rpm/plugins/migrations/0003_custom_metadata_on_scratchpad.py
+++ plugins/pulp_rpm/plugins/migrations/0003_custom_metadata_on_scratchpad.py
@@ -5,13 +5,68 @@ import os
 import gzip
 import logging
 
+from pulp.server import config as pulp_config
 from pulp.server.managers import factory
-from pulp.server.managers.repo._common import importer_working_dir
 
 from pulp_rpm.yum_plugin import util
 
 
 _log = logging.getLogger('pulp')
+
+
+def repository_working_dir(repo_id, mkdir=True):
+    """
+    Determines the repository's working directory. Individual plugin working
+    directories will be placed under this. If the mkdir argument is set to true,
+    the directory will be created as part of this call.
+    See the module-level docstrings for more information on the directory
+    structure.
+    @param mkdir: if true, this call will create the directory; otherwise the
+                  full path will just be generated
+    @type  mkdir: bool
+    @return: full path on disk
+    @rtype:  str
+    """
+    working_dir = os.path.join(_repo_working_dir(), repo_id)
+
+    if mkdir and not os.path.exists(working_dir):
+        os.makedirs(working_dir)
+
+    return working_dir
+
+
+def importer_working_dir(importer_type_id, repo_id, mkdir=True):
+    """
+    Determines the working directory for an importer to use for a repository.
+    If the mkdir argument is set to true, the directory will be created as
+    part of this call.
+    See the module-level docstrings for more information on the directory
+    structure.
+    @param mkdir: if true, this call will create the directory; otherwise the
+                  full path will just be generated
+    @type  mkdir: bool
+    @return: full path on disk to the directory the importer can use for the
+             given repository
+    @rtype:  str
+    """
+    repo_working_dir = repository_working_dir(repo_id, mkdir)
+    working_dir = os.path.join(repo_working_dir, 'importers', importer_type_id)
+
+    if mkdir and not os.path.exists(working_dir):
+        os.makedirs(working_dir)
+
+    return working_dir
+
+
+def _working_dir_root():
+    storage_dir = pulp_config.config.get('server', 'storage_dir')
+    dir_root = os.path.join(storage_dir, 'working')
+    return dir_root
+
+
+def _repo_working_dir():
+    dir = os.path.join(_working_dir_root(), 'repos')
+    return dir
 
 
 def preserve_custom_metadata_on_repo_scratchpad():
