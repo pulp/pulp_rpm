6638bee2074b53e15591e752b88ebacc401e4c9b
Barnaby Court <bcourt@redhat.com>
2015-04-17 11:14:16 -0400

    Bump release to 0.2.alpha

--- plugins/setup.py
+++ plugins/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_rpm_plugins',
-    version='2.7.0a1',
+    version='2.7.0a2',
     license='GPLv2+',
     packages=find_packages(),
     author='Pulp Team',
