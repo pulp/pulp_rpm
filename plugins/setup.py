7bbc3d621d884149f0594bbf5316fc0160056c18
Dennis Kliban <dkliban@redhat.com>
2015-10-20 16:10:45 -0400

    Bumping version to 2.7.0-0.7.beta

--- plugins/setup.py
+++ plugins/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_rpm_plugins',
-    version='2.7.1a1',
+    version='2.7.0b7',
     license='GPLv2+',
     packages=find_packages(),
     author='Pulp Team',
