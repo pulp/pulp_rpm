d2286409bf89053dd12a4e7c465476414dc6ba92
Sean Myers <sean.myers@redhat.com>
2015-12-11 14:45:18 -0500

    Block attempts to load server.conf when running unittests

--- plugins/test/unit/__init__.py
+++ plugins/test/unit/__init__.py
@@ -5,6 +5,9 @@ from pulp.devel.unit.server import base as devel_base
 
 __path__ = extend_path(__path__, __name__)
 
+# prevent attempts to load the server conf during testing
+devel_base.block_load_conf()
+
 
 def setup():
     """
