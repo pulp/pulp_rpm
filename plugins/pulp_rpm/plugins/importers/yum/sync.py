4eefacfe71ec6ad57dfe0a55b6aec0fa0eba2eb9
Jeremy Cline <jeremy@jcline.org>
2015-12-23 13:46:11 -0500

    Fix catalog entries to use the relativepath attribute.
    RPMs are not necessarily located at baseurl/filename. The primary.xml
    file may contain a relative path (from the baseurl) to the RPM itself.

--- plugins/pulp_rpm/plugins/importers/yum/sync.py
+++ plugins/pulp_rpm/plugins/importers/yum/sync.py
@@ -584,7 +584,7 @@ class RepoSync(object):
             entry.importer_id = str(self.conduit.importer_object_id)
             entry.unit_id = unit.id
             entry.unit_type_id = unit.type_id
-            entry.url = urljoin(base_url, unit.filename)
+            entry.url = urljoin(base_url, unit.relativepath)
             entry.save_revision()
             yield unit
 
