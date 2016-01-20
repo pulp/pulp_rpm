261c228e83b6f96425c42b1d90ec3c441625c9d6
Dennis Kliban <dkliban@redhat.com>
2016-01-20 11:14:33 -0500

    Bumping version to 2.8.0-0.4.beta

--- plugins/setup.py
+++ plugins/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_rpm_plugins',
-    version='2.8.0b3',
+    version='2.8.0b4',
     license='GPLv2+',
     packages=find_packages(),
     author='Pulp Team',
