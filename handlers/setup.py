5ae2bb14bde8f60e838685d3e374616ab05d09d9
Sean Myers <sean.myers@redhat.com>
2016-09-13 17:00:44 -0400

    Bumping version to 2.10.0-0.6.rc

--- handlers/setup.py
+++ handlers/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_rpm_handlers',
-    version='2.10c5',
+    version='2.10c6',
     license='GPLv2+',
     packages=find_packages(exclude=['test', 'test.*']),
     author='Pulp Team',
--- plugins/setup.py
+++ plugins/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_rpm_plugins',
-    version='2.10c5',
+    version='2.10c6',
     license='GPLv2+',
     packages=find_packages(exclude=['test', 'test.*']),
     author='Pulp Team',
