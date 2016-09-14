8745e7ba330013adbe37d60cbca69701ea55a442
Michael Hrivnak <mhrivnak@redhat.com>
2016-09-14 11:35:30 -0400

    Adds a required django.setup() call to unit tests.
    Some unit tests failed under django 1.9 without this setup call.
    
    https://pulp.plan.io/issues/2257
    fixes #2257

--- plugins/test/unit/__init__.py
+++ plugins/test/unit/__init__.py
@@ -1,5 +1,6 @@
 from pkgutil import extend_path
 
+import django
 from pulp.devel.unit.server import base as devel_base
 
 
@@ -7,6 +8,18 @@ __path__ = extend_path(__path__, __name__)
 
 # prevent attempts to load the server conf during testing
 devel_base.block_load_conf()
+
+
+# The call to setup() happens automatically when a django app is started as a
+# WSGI process. It also gets called automatically by celery during its
+# initialization. Thus it only needs to be called by pulp when running unit
+# tests. Some change in django 1.9 caused it to break when certain
+# functionality was used before calling setup(), thus these two lines were
+# added.
+#
+# https://pulp.plan.io/issues/2257
+if django.VERSION >= (1, 9):
+    django.setup()
 
 
 def setup():
