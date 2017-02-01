819deb2a573adc828bb9ef3744ab0594030392cf
Sean Myers <sean.myers@redhat.com>
2017-02-01 13:24:49 -0500

    Bumping version to 2.12.0-1

--- common/setup.py
+++ common/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_rpm_common',
-    version='2.12c2',
+    version='2.12',
     license='GPLv2+',
     packages=find_packages(exclude=['test', 'test.*']),
     author='Pulp Team',
