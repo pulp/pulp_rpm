00191a8acea2912a949d26030c9c78d8aaa4af94
Daniel Alley <dalley@redhat.com>
2018-09-14 17:15:54 -0400

    Compile and install createrepo_c so that we can run smash tests

--- .travis/install.sh
+++ .travis/install.sh
@@ -8,7 +8,7 @@ pip install -r test_requirements.txt
 
 cd .. && git clone https://github.com/pulp/pulp.git
 
-if [ -n $PULP_PR_NUMBER ]; then
+if [ -n "$PULP_PR_NUMBER" ]; then
   pushd pulp
   git fetch origin +refs/pull/$PULP_PR_NUMBER/merge
   git checkout FETCH_HEAD
@@ -18,6 +18,15 @@ fi
 pushd pulp/common/ && pip install -e . && popd
 pushd pulp/pulpcore/ && pip install -e . && popd
 pushd pulp/plugin/ && pip install -e .  && popd
+
+
+### Install createrepo_c build deps ###
+sudo apt-get update -y
+sudo apt-get install -y gcc libbz2-dev cmake libexpat1-dev libmagic-dev libglib2.0-dev libcurl4-openssl-dev libxml2-dev libpython3-dev librpm-dev libssl-dev libsqlite3-dev liblzma-dev zlib1g-dev
+
+pip install scikit-build
+pip install https://repos.fedorapeople.org/pulp/pulp/python-packages/createrepo_c-0.11.1.tar.gz
+#######################################
 
 cd pulp_rpm
 pip install -e .
