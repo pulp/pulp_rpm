d2286d19470938e5dd4d16ac0ce3c2c34d8ffa2e
Tatiana Tereshchenko <ttereshc@redhat.com>
2018-12-17 18:43:42 +0100

    Make pulp_rpm compatible with the pulpcore-plugin 0.1.0b14
    [noissue]

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 requirements = [
     'createrepo_c',
-    'pulpcore-plugin==0.1.0b13',
+    'pulpcore-plugin>=0.1.0b14',
 ]
 
 with open('README.rst') as f:
