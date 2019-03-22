60cbb8ef69f79eea96aebe15d7aa291225aadf38
Dennis Kliban <dkliban@redhat.com>
2019-03-22 16:56:24 -0400

    Problem: YumMetadataFile is a symlink
    Solution: Copy YumMetadataFile to the publish directory
    
    YumCloneDistributor and RPMRsyncDistributor both copy the content of repodata directory.
    This results in broken symlinks for all YumMetadataFile content.
    
    fixes: #4550
    https://pulp.plan.io/issues/4550
    
    fixes: #4560
    https://pulp.plan.io/issues/4560
    (cherry picked from commit 713fedaa16b0c65bb2fbd78db47fd250c9fb343e)

--- plugins/pulp_rpm/plugins/distributors/yum/publish.py
+++ plugins/pulp_rpm/plugins/distributors/yum/publish.py
@@ -2,6 +2,7 @@ import copy
 import datetime
 import itertools
 import os
+import shutil
 import subprocess
 
 from gettext import gettext as _
@@ -522,12 +523,12 @@ class PublishMetadataStep(platform_steps.UnitModelPluginStep):
                                                       REPO_DATA_DIR_NAME)
 
         metadata_file_name = os.path.basename(unit._storage_path)
-        link_path = os.path.join(publish_location_relative_path, metadata_file_name)
-        plugin_misc.create_symlink(unit._storage_path, link_path)
+        file_path = os.path.join(publish_location_relative_path, metadata_file_name)
+        shutil.copyfile(unit._storage_path, file_path)
 
         # Add the proper relative reference to the metadata file to repomd
         self.parent.repomd_file_context.\
-            add_metadata_file_metadata(unit.data_type, link_path)
+            add_metadata_file_metadata(unit.data_type, file_path)
 
 
 class PublishDrpmStep(platform_steps.UnitModelPluginStep):
