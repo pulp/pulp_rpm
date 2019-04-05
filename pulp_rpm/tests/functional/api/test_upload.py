2d6f12672911cca21de1890bfda3118f0117c884
koliveir <koliveir@redhat.com>
2019-04-05 14:14:44 -0400

    Add test for duplicate files single request upload
    Add test to single request upload for file already present in Pulp.
    
    https://pulp.plan.io/issues/4536
    closes #4536

--- pulp_rpm/tests/functional/api/test_upload.py
+++ pulp_rpm/tests/functional/api/test_upload.py
@@ -2,10 +2,10 @@
 """Tests that verify upload of content to Pulp."""
 import hashlib
 import unittest
-from urllib.parse import urljoin
 
 from pulp_smash import api, config, utils
-from pulp_smash.pulp3.constants import ARTIFACTS_PATH, BASE_PATH, REPO_PATH
+from pulp_smash.exceptions import TaskReportError
+from pulp_smash.pulp3.constants import ARTIFACTS_PATH, REPO_PATH
 from pulp_smash.pulp3.utils import (
     delete_orphans,
     gen_repo,
@@ -13,6 +13,7 @@ from pulp_smash.pulp3.utils import (
 
 from pulp_rpm.tests.functional.constants import (
     RPM_CONTENT_PATH,
+    RPM_SINGLE_REQUEST_UPLOAD,
     RPM_UNSIGNED_URL,
 )
 from pulp_rpm.tests.functional.utils import set_up_module as setUpModule  # noqa:F401
@@ -36,10 +37,9 @@ class SingleRequestUploadTestCase(unittest.TestCase):
         file = {'file': utils.http_get(RPM_UNSIGNED_URL)}
         client = api.Client(cfg, api.page_handler)
         repo = client.post(REPO_PATH, gen_repo())
-
         self.addCleanup(client.delete, repo['_href'])
         client.post(
-            urljoin(BASE_PATH, 'rpm/upload/'),
+            RPM_SINGLE_REQUEST_UPLOAD,
             files=file,
             data={'repository': repo['_href']}
         )
@@ -60,3 +60,42 @@ class SingleRequestUploadTestCase(unittest.TestCase):
         # Assertion about content unit.
         content = client.get(RPM_CONTENT_PATH)
         self.assertEqual(len(content), 1, content)
+
+
+class SingleRequestUploadDuplicateTestCase(unittest.TestCase):
+    """Attempt to use single request upload for unit already in Pulp.
+
+    This test targets the following issue:
+
+    * `Pulp #4536 <https://pulp.plan.io/issues/4536>`_
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """Create class-wide variables."""
+        cls.cfg = config.get_config()
+        delete_orphans(cls.cfg)
+        cls.client = api.Client(cls.cfg, api.page_handler)
+        cls.file = {'file': utils.http_get(RPM_UNSIGNED_URL)}
+
+    def test_duplicate_unit(self):
+        """Test single request upload for unit already present in Pulp."""
+        repo = self.client.post(REPO_PATH, gen_repo())
+        self.addCleanup(self.client.delete, repo['_href'])
+        self.single_request_upload(repo)
+        with self.assertRaises(TaskReportError) as ctx:
+            self.single_request_upload(repo)
+        for key in ('already', 'exists'):
+            self.assertIn(
+                key,
+                ctx.exception.task['error']['description'].lower(),
+                ctx.exception.task['error']
+            )
+
+    def single_request_upload(self, repo):
+        """Create single request upload."""
+        self.client.post(
+            RPM_SINGLE_REQUEST_UPLOAD,
+            files=self.file,
+            data={'repository': repo['_href']}
+        )
