5779d04f522d1208398c77bd92ed07aef5a9f75e
ragabala <ragavendranb92@gmail.com>
2019-04-10 23:29:37 -0400

    Check whether the checksum type is not present in the remote repo
    According to issue #4109 and #4033, the updateinfo.xml after
    distributing a publication, had extraneous 'unknown checksum' in the
    response. The sum tag is now removed from the updateinfo after
    publishing. This test checks that.
    
    https://pulp.plan.io/issues/4109
    closes #4109

--- pulp_rpm/tests/functional/api/test_publish.py
+++ pulp_rpm/tests/functional/api/test_publish.py
@@ -2,12 +2,15 @@
 """Tests that publish rpm plugin repositories."""
 import unittest
 from random import choice
+from xml.etree import ElementTree
 
 from requests.exceptions import HTTPError
 
 from pulp_smash import api, config
-from pulp_smash.pulp3.constants import REPO_PATH
+from pulp_smash.pulp3.constants import DISTRIBUTION_PATH, REPO_PATH
 from pulp_smash.pulp3.utils import (
+    download_content_unit,
+    gen_distribution,
     gen_repo,
     get_content,
     get_content_summary,
@@ -22,6 +25,7 @@ from pulp_rpm.tests.functional.constants import (
     RPM_ALT_LAYOUT_FIXTURE_URL,
     RPM_FIXTURE_SUMMARY,
     RPM_LONG_UPDATEINFO_FIXTURE_URL,
+    RPM_NAMESPACES,
     RPM_PACKAGE_CONTENT_NAME,
     RPM_PUBLICATION_PATH,
     RPM_REFERENCES_UPDATEINFO_URL,
@@ -191,3 +195,86 @@ class SyncPublishTestCase(unittest.TestCase):
 
         publication = publish(self.cfg, repo)
         self.addCleanup(self.client.delete, publication['_href'])
+
+
+class ValidateNoChecksumTagTestCase(unittest.TestCase):
+    """Publish repository and validate the updateinfo.
+
+    This Test does the following:
+
+    1. Create a rpm repo and a remote.
+    2. Sync the repo with the remote.
+    3. Publish and distribute the repo.
+    4. Check whether CheckSum tag ``sum`` not present in ``updateinfo.xml``.
+
+    This test targets the following issue:
+
+    * `Pulp #4109 <https://pulp.plan.io/issues/4109>`_
+    * `Pulp #4033 <https://pulp.plan.io/issues/4033>`_
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """Create class-wide variables."""
+        cls.cfg = config.get_config()
+        cls.client = api.Client(cls.cfg, api.page_handler)
+
+    def test_all(self):
+        """Sync and publish an RPM repository and verify the checksum."""
+        # Step 1
+        repo = self.client.post(REPO_PATH, gen_repo())
+        self.addCleanup(self.client.delete, repo['_href'])
+
+        remote = self.client.post(RPM_REMOTE_PATH, gen_rpm_remote())
+        self.addCleanup(self.client.delete, remote['_href'])
+
+        # Step 2
+        sync(self.cfg, remote, repo)
+        repo = self.client.get(repo['_href'])
+
+        self.assertIsNotNone(repo['_latest_version_href'])
+
+        # Step 3
+        publication = publish(self.cfg, repo)
+        self.addCleanup(self.client.delete, publication['_href'])
+        body = gen_distribution()
+        body['publication'] = publication['_href']
+        distribution = self.client.using_handler(api.task_handler).post(
+            DISTRIBUTION_PATH, body
+        )
+        self.addCleanup(self.client.delete, distribution['_href'])
+        # Step 4
+        repo_md = ElementTree.fromstring(
+            download_content_unit(self.cfg, distribution, 'repodata/repomd.xml')
+        )
+        update_info_content = ElementTree.fromstring(
+            download_content_unit(
+                self.cfg,
+                distribution,
+                self._get_updateinfo_xml_path(repo_md)
+            )
+        )
+        tags = {elem.tag for elem in update_info_content.iter()}
+        self.assertNotIn('sum', tags, update_info_content)
+
+    @staticmethod
+    def _get_updateinfo_xml_path(root_elem):
+        """Return the path to ``updateinfo.xml.gz``, relative to repository root.
+
+        Given a repomd.xml, this method parses the xml and returns the
+        location of updateinfo.xml.gz.
+        """
+        # <ns0:repomd xmlns:ns0="http://linux.duke.edu/metadata/repo">
+        #     <ns0:data type="primary">
+        #         <ns0:checksum type="sha256">[…]</ns0:checksum>
+        #         <ns0:location href="repodata/[…]-primary.xml.gz" />
+        #         …
+        #     </ns0:data>
+        #     …
+        xpath = '{{{}}}data'.format(RPM_NAMESPACES['metadata/repo'])
+        data_elems = [
+            elem for elem in root_elem.findall(xpath)
+            if elem.get('type') == 'updateinfo'
+        ]
+        xpath = '{{{}}}location'.format(RPM_NAMESPACES['metadata/repo'])
+        return data_elems[0].find(xpath).get('href')
