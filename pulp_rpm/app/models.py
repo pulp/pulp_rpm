f1f757f157e6d27eb1f94da4bcfcbc54c3f4274c
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2019-09-11 15:17:39 -0300

    Adding RepoMetadataFile model.
    closes #5430
    https://pulp.plan.io/issues/5430

--- pulp_rpm/app/models.py
+++ pulp_rpm/app/models.py
@@ -1249,3 +1249,28 @@ class Variant(Model):
             "packages",
             "distribution_tree",
         )
+
+
+class RepoMetadataFile(Content):
+    """
+    Model for custom/unknown repository metadata.
+
+    Fields:
+        data_type (Text):
+            Metadata type
+        checksum_type (Text):
+            Checksum type for the file
+        checksum (Text):
+            Checksum value for the file
+
+    """
+
+    TYPE = 'repo_metadata_file'
+
+    data_type = models.CharField(max_length=20)
+    checksum_type = models.CharField(max_length=6)
+    checksum = models.CharField(max_length=128)
+
+    class Meta:
+        default_related_name = "%(app_label)s_%(model_name)s"
+        unique_together = ("data_type", "checksum")
