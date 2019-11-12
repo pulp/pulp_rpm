d1b766bfa4f534a879dde6e9038826ae28c76782
Dana Walker <dawalker@redhat.com>
2019-11-11 21:59:43 -0500

    Switch from models.py to directory with multiple files
    Refactor models.py to a directory for better organization,
    ease of finding related models.
    
    closes #5325
    http://pulp.plan.io/issues/5325
    
    (cherry picked from commit 08dfe9e2f24e1ab960971e6892ffd2975719ae71)

---
+++ pulp_rpm/app/models/__init__.py
@@ -0,0 +1,7 @@
+from .advisory import UpdateCollection, UpdateCollectionPackage, UpdateRecord, UpdateReference  # noqa
+from .comps import PackageCategory, PackageEnvironment, PackageGroup, PackageLangpacks  # noqa
+from .custom_metadata import RepoMetadataFile  # noqa
+from .distribution import Addon, Checksum, DistributionTree, Image, Variant  # noqa
+from .modulemd import Modulemd, ModulemdDefaults  # noqa
+from .package import Package  # noqa
+from .repository import RpmDistribution, RpmPublication, RpmRemote, RpmRepository  # noqa
