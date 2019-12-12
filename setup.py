33ef4841bd65736a82787a593e7531dae7ff758c
Tatiana Tereshchenko <ttereshc@redhat.com>
2019-12-12 21:20:19 +0100

    Version bump to 3.0.1.dev
    [noissue]

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('README.rst') as f:
 
 setup(
     name='pulp-rpm',
-    version='3.0.0',
+    version='3.0.1.dev',
     description='RPM plugin for the Pulp Project',
     long_description=long_description,
     license='GPLv2+',
