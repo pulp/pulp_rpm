3490865fed124b32e2d05fc017ebef0a40a92059
David Davis <daviddavis@redhat.com>
2020-01-22 17:06:20 -0500

    Allow content to be filtered during copy
    fixes #6009

--- pulp_rpm/app/tasks/copy.py
+++ pulp_rpm/app/tasks/copy.py
@@ -1,9 +1,31 @@
 from django.db.models import Q
 
-from pulpcore.plugin.models import Repository, RepositoryVersion
+from pulpcore.plugin.models import RepositoryVersion
 
 from pulp_rpm.app.depsolving import Solver
 from pulp_rpm.app.models import RpmRepository
+
+
+def _filter_content(content, criteria):
+    """
+    Filter content in the source repository version by criteria
+
+    Args:
+        content: a queryset of content to filter
+        criteria: a validated dict that maps content type to a list of filter criteria
+    """
+    if not criteria:
+        return content
+
+    content_pks = []
+    for content_type in RpmRepository.CONTENT_TYPES:
+        if criteria.get(content_type.TYPE):
+            filters = Q()
+            for filter in criteria[content_type.TYPE]:
+                filters |= Q(**filter)
+            content_pks += content_type.objects.filter(filters).values_list("pk", flat=True)
+
+    return content.filter(pk__in=content_pks)
 
 
 def copy_content(source_repo_version_pk, dest_repo_pk, criteria, dependency_solving):
@@ -13,7 +35,8 @@ def copy_content(source_repo_version_pk, dest_repo_pk, criteria, dependency_solv
     Args:
         source_repo_version_pk: repository version primary key to copy units from
         dest_repo_pk: repository primary key to copy units into
-        types: a tuple of strings representing the '_type' values of types to include in the copy
+        criteria: a dict that maps type to a list of criteria to filter content by. Note that this
+            criteria MUST be validated before being passed to this task.
     """
     source_repo_version = RepositoryVersion.objects.get(pk=source_repo_version_pk)
     source_repo = RpmRepository.objects.get(pk=source_repo_version.repository)
@@ -24,7 +47,7 @@ def copy_content(source_repo_version_pk, dest_repo_pk, criteria, dependency_solv
     content_types = source_repo.CONTENT_TYPES
     list(content_types)
 
-    content_to_copy = source_repo_version.content
+    content_to_copy = _filter_content(source_repo_version.content, criteria)
 
     if dependency_solving:
         # TODO: add lookaside repos here
