966a8f8fb628c08b6aa15e0256533c2ccfb149f4
David Davis <daviddavis@redhat.com>
2020-02-19 15:04:17 -0500

    Allow content to be copied by href
    fixes #6019
    https://pulp.plan.io/issues/6019

---
+++ CHANGES/6019.feature
@@ -0,0 +1,1 @@
+Add a content parameter to the copy api that accepts a list of hrefs to be copied.
--- pulp_rpm/tests/functional/api/test_copy.py
+++ pulp_rpm/tests/functional/api/test_copy.py
@@ -18,6 +18,7 @@ from pulp_rpm.tests.functional.constants import (
     RPM_FIXTURE_SUMMARY,
     RPM_REMOTE_PATH,
     RPM_REPO_PATH,
+    UPDATERECORD_CONTENT_PATH,
 )
 from pulp_rpm.tests.functional.utils import gen_rpm_remote, rpm_copy
 from pulp_rpm.tests.functional.utils import set_up_module as setUpModule  # noqa:F401
@@ -34,8 +35,8 @@ class BasicCopyTestCase(unittest.TestCase):
 
         delete_orphans(cls.cfg)
 
-    def _do_test(self, criteria, expected_results):
-        """Test copying content units with the RPM plugin.
+    def _setup_repos(self):
+        """Prepare for a copy test by creating two repos and syncing.
 
         Do the following:
 
@@ -43,8 +44,6 @@ class BasicCopyTestCase(unittest.TestCase):
         2. Sync the remote.
         3. Assert that repository version is not None.
         4. Assert that the correct number of units were added and are present in the repo.
-        5. Use the RPM copy API to units from the repo to the empty repo.
-        7. Assert that the correct number of units were added and are present in the dest repo.
         """
         source_repo = self.client.post(RPM_REPO_PATH, gen_repo())
         self.addCleanup(self.client.delete, source_repo['pulp_href'])
@@ -69,6 +68,18 @@ class BasicCopyTestCase(unittest.TestCase):
             get_added_content_summary(source_repo), RPM_FIXTURE_SUMMARY
         )
 
+        return source_repo, dest_repo
+
+    def _do_test(self, criteria, expected_results):
+        """Test copying content units with the RPM plugin.
+
+        Calls _setup_repos() which creates two repos and syncs to the source. Then:
+
+        1. Use the RPM copy API to units from the repo to the empty repo.
+        2. Assert that the correct number of units were added and are present in the dest repo.
+        """
+        source_repo, dest_repo = self._setup_repos()
+
         rpm_copy(self.cfg, source_repo, dest_repo, criteria)
         dest_repo = self.client.get(dest_repo['pulp_href'])
 
@@ -78,21 +89,10 @@ class BasicCopyTestCase(unittest.TestCase):
             get_added_content_summary(dest_repo), expected_results,
         )
 
-    def test_copy_none(self):
-        """Test copying none of the content."""
-        criteria = {}
-        results = {}
-        self._do_test(criteria, results)
-
     def test_copy_all(self):
         """Test copying all the content from one repo to another."""
         results = RPM_FIXTURE_SUMMARY
         self._do_test(None, results)
-
-    # def test_copy_by_href(self):
-    #     criteria = {}
-    #     results = {}
-    #     self._do_test(criteria, results)
 
     def test_copy_by_advisory_id(self):
         """Test copying an advisory by its id."""
@@ -117,6 +117,34 @@ class BasicCopyTestCase(unittest.TestCase):
                 'advisory': [{'bad': 'field'}]
             }
             self._do_test(criteria, {})
+
+    def test_both_criteria_and_content(self):
+        """Test that specifying 'content' and 'criteria' is invalid."""
+        criteria = {
+            'advisory': [{'bad': 'field'}]
+        }
+        content = ["/pulp/api/v3/content/rpm/packages/6d0e1e29-ae58-4c70-8bcb-ae957250fc8f/"]
+        self._setup_repos()
+        source_repo, dest_repo = self._setup_repos()
+
+        with self.assertRaises(HTTPError):
+            rpm_copy(self.cfg, source_repo, dest_repo, criteria, content)
+
+    def test_content(self):
+        """Test the content parameter."""
+        source_repo, dest_repo = self._setup_repos()
+        latest_href = source_repo["latest_version_href"]
+
+        content = self.client.get(f"{UPDATERECORD_CONTENT_PATH}?repository_version={latest_href}")
+        content_to_copy = (content["results"][0]["pulp_href"], content["results"][1]["pulp_href"])
+
+        rpm_copy(self.cfg, source_repo, dest_repo, content=content_to_copy)
+
+        latest_href = dest_repo["pulp_href"] + "versions/1/"
+        dc = self.client.get(f"{UPDATERECORD_CONTENT_PATH}?repository_version={latest_href}")
+        dest_content = [c["pulp_href"] for c in dc["results"]]
+
+        self.assertEqual(sorted(content_to_copy), sorted(dest_content))
 
 
 class DependencySolvingTestCase(unittest.TestCase):
