dd62b81cf2468a6427fe4943c9712489af552050
Daniel Alley <dalley@redhat.com>
2020-02-26 16:41:11 -0500

    Add support for incremental update (errata copy w/ referenced rpms)
    closes: #4768
    https://pulp.plan.io/issues/4768

--- pulp_rpm/app/tasks/copy.py
+++ pulp_rpm/app/tasks/copy.py
@@ -1,9 +1,10 @@
+from django.core.exceptions import MultipleObjectsReturned
 from django.db.models import Q
 
 from pulpcore.plugin.models import Content, RepositoryVersion
 
 from pulp_rpm.app.depsolving import Solver
-from pulp_rpm.app.models import RpmRepository
+from pulp_rpm.app.models import UpdateRecord, Package, RpmRepository, Modulemd
 
 
 def _filter_content(content, criteria, content_pks):
@@ -32,6 +33,64 @@ def _filter_content(content, criteria, content_pks):
     return content.filter(pk__in=content_pks)
 
 
+def find_children_of_content(content, repository_version):
+    """Finds the content referenced directly by other content and returns it all together.
+
+    Finds RPMs referenced by Advisory/Errata content.
+
+    Args:
+        content (iterable): Content for which to resolve children
+        repository_version (pulpcore.models.RepositoryVersion): Source repo version
+
+    Returns: Queryset of Content objects that are children of the intial set of content
+    """
+    # Advisories that were selected to be copied
+    advisory_ids = content.filter(pulp_type=UpdateRecord.get_pulp_type()).only('pk')
+    # All packages in the source repository version
+    package_ids = repository_version.content.filter(
+        pulp_type=Package.get_pulp_type()).only('pk')
+    # All modules in the source repository version
+    module_ids = repository_version.content.filter(
+        pulp_type=Modulemd.get_pulp_type()).only('pk')
+
+    advisories = UpdateRecord.objects.filter(pk__in=advisory_ids)
+    packages = Package.objects.filter(pk__in=package_ids)
+    modules = Modulemd.objects.filter(pk__in=module_ids)
+
+    children = set()
+
+    for advisory in advisories:
+        # Find rpms referenced by Advisories/Errata
+        package_nevras = advisory.get_pkglist()
+        for nevra in package_nevras:
+            (name, epoch, version, release, arch) = nevra
+            try:
+                package = packages.get(
+                    name=name, epoch=epoch, version=version, release=release, arch=arch)
+                children.add(package.pk)
+            except Package.DoesNotExist:
+                raise
+            except MultipleObjectsReturned:
+                raise
+
+        module_nsvcas = advisory.get_module_list()
+        for nsvca in module_nsvcas:
+            (name, stream, version, context, arch) = nsvca
+            try:
+                module = modules.get(
+                    name=name, stream=stream, version=version, context=context, arch=arch)
+                children.add(module.pk)
+            except Modulemd.DoesNotExist:
+                raise
+            except MultipleObjectsReturned:
+                raise
+
+    # TODO: Find rpms referenced by PackageGroups,
+    # PackageGroups referenced by PackageCategories, etc.
+
+    return Content.objects.filter(pk__in=children)
+
+
 def copy_content(source_repo_version_pk, dest_repo_pk, criteria, content_pks, dependency_solving):
     """
     Copy content from one repo to another.
@@ -51,6 +110,7 @@ def copy_content(source_repo_version_pk, dest_repo_pk, criteria, content_pks, de
 
     if not dependency_solving:
         content_to_copy = _filter_content(source_repo_version.content, criteria, content_pks)
+        content_to_copy |= find_children_of_content(content_to_copy, source_repo_version)
 
         with dest_repo.new_version() as new_version:
             new_version.add_content(content_to_copy)
@@ -70,7 +130,10 @@ def copy_content(source_repo_version_pk, dest_repo_pk, criteria, content_pks, de
     for src in repo_mapping.keys():
         repo_name = solver.load_source_repo(src)
         libsolv_repo_names[repo_name] = src
-        content_to_copy[repo_name] = _filter_content(src.content, criteria, content_pks)
+
+        content = _filter_content(src.content, criteria, content_pks)
+        children = find_children_of_content(content, src)
+        content_to_copy[repo_name] = content | children
 
     for tgt in repo_mapping.values():
         solver.load_target_repo(tgt)
