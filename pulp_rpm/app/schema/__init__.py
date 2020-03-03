539cd7809d26f55f81d2a11ea76e7763824069b7
David Davis <daviddavis@redhat.com>
2020-03-03 12:46:39 -0500

    Allow copying of multiple sources to destinations in one call
    fixes #6268
    https://pulp.plan.io/issues/6268
    
    fixes #6018
    https://pulp.plan.io/issues/6018

--- pulp_rpm/app/schema/__init__.py
+++ pulp_rpm/app/schema/__init__.py
@@ -3,5 +3,5 @@ import os
 
 location = os.path.dirname(os.path.realpath(__file__))
 
-with open(os.path.join(location, "copy_criteria.json")) as copy_json:
-    COPY_CRITERIA_SCHEMA = json.load(copy_json)
+with open(os.path.join(location, "copy_config.json")) as copy_config_json:
+    COPY_CONFIG_SCHEMA = json.load(copy_config_json)
