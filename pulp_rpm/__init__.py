c33af0737efe2dc0de1b8d0744eb0c1b9cdf3b71
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2020-12-08 10:44:54 -0300

    Adds PulpPluginAppConfig.version
    [noissue]

--- pulp_rpm/__init__.py
+++ pulp_rpm/__init__.py
@@ -1,4 +1,1 @@
-__version__ = "3.9.0.dev"
-
-
 default_app_config = "pulp_rpm.app.PulpRpmPluginAppConfig"
--- pulp_rpm/app/__init__.py
+++ pulp_rpm/app/__init__.py
@@ -8,3 +8,4 @@ class PulpRpmPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_rpm.app"
     label = "rpm"
+    version = "3.9.0.dev"
