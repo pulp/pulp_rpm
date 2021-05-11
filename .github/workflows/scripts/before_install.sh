1ef1e7043d60757b87db8a5122dbd6062dfa9c51
Grant Gainey <ggainey@redhat.com>
2021-05-11 13:46:03 -0400

    Marking us as requiring core-3.12 until deprecations can be addressed.
    Also pin django to 2.2.14<=20 while we wait for a django-release.
    
    [noissue]
    [nocoverage]

--- .github/workflows/scripts/before_install.sh
+++ .github/workflows/scripts/before_install.sh
@@ -98,7 +98,7 @@ pulp config create --base-url http://pulp --location tests/settings.toml
 cd ..
 
 
-git clone --depth=1 https://github.com/pulp/pulpcore.git --branch master
+git clone --depth=1 https://github.com/pulp/pulpcore.git --branch 3.12
 
 cd pulpcore
 if [ -n "$PULPCORE_PR_NUMBER" ]; then
