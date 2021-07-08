860e9683db8ad502910a2320c1300bcbc40791e6
David Davis <daviddavis@redhat.com>
2021-07-08 13:03:52 -0400

    Drop support for Python 3.6 and 3.7
    fixes #9033

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     author="Pulp Project Developers",
     author_email="pulp-list@redhat.com",
     url="http://www.pulpproject.org",
-    python_requires=">=3.6",
+    python_requires=">=3.8",
     install_requires=requirements,
     include_package_data=True,
     packages=find_packages(exclude=["test"]),
@@ -28,8 +28,8 @@ setup(
         "Framework :: Django",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ),
     entry_points={
         "pulpcore.plugin": [
