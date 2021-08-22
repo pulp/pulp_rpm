90285e03b1830aff84d2fae503bf0e189d8bb8f6
Tatiana Tereshchenko <ttereshc@redhat.com>
2021-08-22 22:55:47 +0200

    Adapted to some (more) SUSE repo edge-cases.
    * Some metadata-files can have '-' in the filename
    * Some advisory datetimes are just timestamps.
    
    backports #8275
    backports #9096
    
    fixes #9113
    
    (cherry picked from commit 47584d3c562e17ab8d2cfc873cae19b4a8855cb1)

--- pulp_rpm/app/tasks/publishing.py
+++ pulp_rpm/app/tasks/publishing.py
@@ -91,7 +91,9 @@ class PublicationData:
             current_file = content_artifact.artifact.file.file
             path = content_artifact.relative_path.split("/")[-1]
             if repo_metadata_file.checksum in path:
-                path = path.split("-")[-1]
+                # filenames can be checksum-xxxx.yyy - but can also be checksum-mmm-nnn-ooo.yyy
+                # split off the checksum, keep the rest
+                path = "-".join(path.split("-")[1:])
             if folder:
                 path = os.path.join(folder, path)
             with open(path, "wb") as new_file:
@@ -537,6 +539,7 @@ def create_repomd_xml(
     repomdrecords.extend(extra_repomdrecords)
 
     sqlite_files = ("primary_db", "filelists_db", "other_db")
+
     for name, path, db_to_update in repomdrecords:
         record = cr.RepomdRecord(name, path)
         checksum_type = get_checksum_type(name, checksum_types)
