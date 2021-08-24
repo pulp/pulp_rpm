39bf08641e0f7de67c155a6680934b2a8ccd08e2
Tatiana Tereshchenko <ttereshc@redhat.com>
2021-08-24 08:59:08 +0200

    Revert "Update GHW sign"
    To make this branch compatible with pulpcore 3.7.
    
    This reverts commit 15444172a8b84f693a3fe75ff40942ca84b5499c.
    [noissue]

--- .github/workflows/scripts/post_before_script.sh
+++ .github/workflows/scripts/post_before_script.sh
@@ -3,18 +3,17 @@
 set -euv
 
 cmd_stdin_prefix bash -c "cat > /root/sign-metadata.sh" < "$GITHUB_WORKSPACE"/pulp_rpm/tests/functional/sign-metadata.sh
-cmd_stdin_prefix bash -c "cat > /root/sign.py" < "$PWD"/.github/workflows/scripts/sign.py
 
 cmd_prefix bash -c "curl -L https://github.com/pulp/pulp-fixtures/raw/master/common/GPG-PRIVATE-KEY-pulp-qe | gpg --import"
-cmd_prefix bash -c "curl -O https://github.com/pulp/pulp-fixtures/raw/master/common/GPG-KEY-pulp-qe"
-cmd_prefix chmod a+x /root/sign-metadata.sh /root/sign.py
+cmd_prefix chmod a+x /root/sign-metadata.sh
 
-# If KEY_FINGERPRINT changes, change it in sign.py as well.
 KEY_FINGERPRINT="6EDF301256480B9B801EBA3D05A5E6DA269D9D98"
 TRUST_LEVEL="6"
 echo "$KEY_FINGERPRINT:$TRUST_LEVEL:" | cmd_stdin_prefix gpg --import-ownertrust
 
-cmd_prefix bash -c "django-admin shell < /root/sign.py"
+CREATE_SIGNING_SERVICE="from pulpcore.app.models.content import AsciiArmoredDetachedSigningService; AsciiArmoredDetachedSigningService.objects.create(name='sign-metadata', script='/root/sign-metadata.sh')"
+cmd_prefix bash -c "django-admin shell -c \"${CREATE_SIGNING_SERVICE}\""
+
 
 echo "machine pulp
 login admin
