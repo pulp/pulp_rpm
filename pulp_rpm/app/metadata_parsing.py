fcbb66600e4650928a663d00d0dc3a057be7a904
Daniel Alley <dalley@redhat.com>
2021-08-28 00:20:09 -0400

    Fix a parsing error that occurs when mirror=False and SRPMs are skipped
    backports #9308
    
    Also refactor repository metadata parsing to facilitate adding the new
    createrepo_c API in the future
    
    fixes: #9312

--- pulp_rpm/app/metadata_parsing.py
+++ pulp_rpm/app/metadata_parsing.py
@@ -1,9 +1,12 @@
 import bz2
+import collections
 import gzip
 import logging
 import lzma
 import os
 import re
+
+import createrepo_c as cr
 from xml.etree.cElementTree import iterparse
 
 log = logging.getLogger(__name__)
@@ -123,3 +126,163 @@ def process_other_package_element(element):
             changelogs.append((author, date, text))
 
     return pkgid, changelogs
+
+
+def warningcb(warning_type, message):
+    """Optional callback for warnings about wierd stuff and formatting in XML.
+
+    Args:
+        warning_type (int): One of the XML_WARNING_* constants.
+        message (str): Message.
+    """
+    log.warn("PARSER WARNING: %s" % message)
+    return True  # continue parsing
+
+
+def parse_repodata(primary_xml_path, filelists_xml_path, other_xml_path, only_primary=False):
+    """
+    Parse repodata to extract package info.
+
+    Args:
+        primary_xml_path (str): a path to a downloaded primary.xml
+        filelists_xml_path (str): a path to a downloaded filelists.xml
+        other_xml_path (str): a path to a downloaded other.xml
+
+    Kwargs:
+        only_primary (bool): If true, only the metadata in primary.xml will be parsed.
+
+    Returns:
+        dict: createrepo_c package objects with the pkgId as a key
+
+    """
+
+    def pkgcb(pkg):
+        """
+        A callback which is used when a whole package entry in xml is parsed.
+
+        Args:
+            pkg(preaterepo_c.Package): a parsed metadata for a package
+
+        """
+        packages[pkg.pkgId] = pkg
+
+    def newpkgcb(pkgId, name, arch):
+        """
+        A callback which is used when a new package entry is encountered.
+
+        Only opening <package> element is parsed at that moment.
+        This function has to return a package which parsed data will be added to
+        or None if a package should be skipped.
+
+        pkgId, name and arch of a package can be used to skip further parsing. Available
+        only for filelists.xml and other.xml.
+
+        Args:
+            pkgId(str): pkgId of a package
+            name(str): name of a package
+            arch(str): arch of a package
+
+        Returns:
+            createrepo_c.Package: a package which parsed data should be added to.
+
+            If None is returned, further parsing of a package will be skipped.
+
+        """
+        return packages.get(pkgId, None)
+
+    packages = collections.OrderedDict()
+
+    cr.xml_parse_primary(primary_xml_path, pkgcb=pkgcb, warningcb=warningcb, do_files=False)
+    if not only_primary:
+        cr.xml_parse_filelists(filelists_xml_path, newpkgcb=newpkgcb, warningcb=warningcb)
+        cr.xml_parse_other(other_xml_path, newpkgcb=newpkgcb, warningcb=warningcb)
+    return packages
+
+
+class MetadataParser:
+    """Parser for RPM metadata."""
+
+    def __init__(self):
+        """Initialize empty (use one of the alternate constructors)."""
+        self.primary_xml_path = None
+        self.filelists_xml_path = None
+        self.other_xml_path = None
+
+    @staticmethod
+    def from_metadata_files(primary_xml_path, filelists_xml_path, other_xml_path):
+        """Construct a parser from the three main metadata files."""
+        parser = MetadataParser()
+        parser.primary_xml_path = primary_xml_path
+        parser.filelists_xml_path = filelists_xml_path
+        parser.other_xml_path = other_xml_path
+        return parser
+
+    def count_packages(self):
+        """Count the total number of packages."""
+        # It would be much faster to just read the number in the header of the metadata.
+        # But there's no way to do that, and also we can't necessarily rely on that number because
+        # of duplicates.
+        len(
+            parse_repodata(
+                self.primary_xml_path,
+                self.filelists_xml_path,
+                self.other_xml_path,
+                only_primary=True,
+            )
+        )
+
+    def parse_packages_iterative(self, file_extension, skip_srpms=False):
+        """Parse packages iteratively using the hybrid parser."""
+        extra_repodata_parser = iterative_files_changelog_parser(
+            file_extension, self.filelists_xml_path, self.other_xml_path
+        )
+        seen_pkgids = set()
+        # We *do not* want to skip srpms when parsing primary because otherwise we run into
+        # trouble when we encounter them again on the iterative side of the parser. Just skip
+        # them at the end.
+        for pkg in self.parse_packages(only_primary=True):
+            pkgid = pkg.pkgId
+            while True:
+                pkgid_extra, files, changelogs = next(extra_repodata_parser)
+                if pkgid_extra in seen_pkgids:
+                    # This is a dirty hack to handle cases that "shouldn't" happen.
+                    # Sometimes repositories have packages listed twice under the same
+                    # pkgid. This is a problem because the primary.xml parsing
+                    # deduplicates the entries by placing them into a dict keyed by pkgid.
+                    # So if the iterative parser(s) run into a package we've seen before,
+                    # we should skip it and move on.
+                    continue
+                else:
+                    seen_pkgids.add(pkgid)
+                    break
+
+            assert pkgid == pkgid_extra, (
+                "Package id from primary metadata ({}), does not match package id "
+                "from filelists, other metadata ({})"
+            ).format(pkgid, pkgid_extra)
+
+            if skip_srpms and pkg.arch == "src":
+                continue
+
+            pkg.files = files
+            pkg.changelogs = changelogs
+            yield pkg
+
+    def parse_packages(self, only_primary=False, skip_srpms=False):
+        """Parse packages using the traditional createrepo_c parser."""
+        packages = parse_repodata(
+            self.primary_xml_path,
+            self.filelists_xml_path,
+            self.other_xml_path,
+            only_primary=only_primary,
+        )
+        while True:
+            try:
+                (pkgid, pkg) = packages.popitem(last=False)
+            except KeyError:
+                break
+
+            if skip_srpms and pkg.arch == "src":
+                continue
+
+            yield pkg
