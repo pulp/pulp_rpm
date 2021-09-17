934dc96ad66eab37a8fecae6720be64ec03ea980
Pavel Picka <ppicka@redhat.com>
2021-09-17 15:14:24 +0200

    ACS support
    Adding model/serializer and refresh endpoint. Updating documentation.
    
    [noissue]

--- pulp_rpm/app/viewsets.py
+++ pulp_rpm/app/viewsets.py
@@ -1,17 +1,23 @@
+import os
 from django_filters import CharFilter
 from gettext import gettext as _
 
 from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from rest_framework.generics import get_object_or_404
 from rest_framework.serializers import ValidationError as DRFValidationError
 
 from pulpcore.plugin.models import PulpTemporaryFile
 from pulpcore.plugin.actions import ModifyRepositoryActionMixin
-from pulpcore.plugin.models import RepositoryVersion
+from pulpcore.plugin.models import AlternateContentSourcePath, RepositoryVersion, TaskGroup
 from pulpcore.plugin.tasking import dispatch
-from pulpcore.plugin.serializers import AsyncOperationResponseSerializer
+from pulpcore.plugin.serializers import (
+    AsyncOperationResponseSerializer,
+    TaskGroupOperationResponseSerializer,
+)
 from pulpcore.plugin.viewsets import (
+    AlternateContentSourceViewSet,
     DistributionViewSet,
     ContentFilter,
     NoArtifactContentUploadViewSet,
@@ -23,11 +29,13 @@ from pulpcore.plugin.viewsets import (
     RepositoryViewSet,
     RepositoryVersionViewSet,
     SingleArtifactContentUploadViewSet,
+    TaskGroupOperationResponse,
 )
 
 from pulp_rpm.app import tasks
 from pulp_rpm.app.constants import SYNC_POLICIES
 from pulp_rpm.app.models import (
+    RpmAlternateContentSource,
     DistributionTree,
     Modulemd,
     ModulemdDefaults,
@@ -45,6 +53,7 @@ from pulp_rpm.app.models import (
     UpdateRecord,
 )
 from pulp_rpm.app.serializers import (
+    RpmAlternateContentSourceSerializer,
     CopySerializer,
     CompsXmlSerializer,
     DistributionTreeSerializer,
@@ -527,3 +536,66 @@ class CompsXmlViewSet(viewsets.ViewSet):
             kwargs={},
         )
         return OperationPostponedResponse(task, request)
+
+
+class RpmAlternateContentSourceViewSet(AlternateContentSourceViewSet):
+    """
+    ViewSet for ACS.
+    """
+
+    endpoint_name = "rpm"
+    queryset = RpmAlternateContentSource.objects.all()
+    serializer_class = RpmAlternateContentSourceSerializer
+
+    @extend_schema(
+        description="Trigger an asynchronous task to create Alternate Content Source content.",
+        responses={202: TaskGroupOperationResponseSerializer},
+    )
+    @action(methods=["post"], detail=True)
+    def refresh(self, request, pk):
+        """
+        Refresh ACS metadata.
+        """
+        acs = get_object_or_404(RpmAlternateContentSource, pk=pk)
+        acs_paths = AlternateContentSourcePath.objects.filter(alternate_content_source=pk)
+        task_group = TaskGroup.objects.create(
+            description=f"Refreshing {acs_paths.count()} alternate content source path(s)."
+        )
+
+        # Get required defaults for sync operation
+        optimize = RpmRepositorySyncURLSerializer().data["optimize"]
+        skip_types = RpmRepositorySyncURLSerializer().data["skip_types"]
+
+        for acs_path in acs_paths:
+            # Create or get repository for the path
+            repo_data = {
+                "name": f"{acs.name}--{acs_path.pk}--repository",
+                "retain_repo_versions": 1,
+                "user_hidden": True,
+            }
+            repo, created = RpmRepository.objects.get_or_create(**repo_data)
+            if created:
+                acs_path.repository = repo
+                acs_path.save()
+            acs_url = (
+                os.path.join(acs.remote.url, acs_path.path) if acs_path.path else acs.remote.url
+            )
+
+            # Dispatching ACS path to own task and assign it to common TaskGroup
+            dispatch(
+                tasks.synchronize,
+                shared_resources=[acs.remote, acs],
+                task_group=task_group,
+                kwargs={
+                    "remote_pk": str(acs.remote.pk),
+                    "repository_pk": str(acs_path.repository.pk),
+                    "sync_policy": SYNC_POLICIES.MIRROR_CONTENT_ONLY,
+                    "skip_types": skip_types,
+                    "optimize": optimize,
+                    "url": acs_url,
+                },
+            )
+
+        # Update TaskGroup that all child task are dispatched
+        task_group.finish()
+        return TaskGroupOperationResponse(task_group, request)
