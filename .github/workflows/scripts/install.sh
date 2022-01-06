81273706b8baad5d48d2c02b72c25590663f4dd3
pulpbot <pulp-infra@redhat.com>
2022-01-06 16:42:15 +0000

    Update CI files
    [noissue]

--- .github/workflows/scripts/install.sh
+++ .github/workflows/scripts/install.sh
@@ -16,7 +16,6 @@ set -euv
 source .github/workflows/scripts/utils.sh
 
 if [[ "$TEST" = "docs" || "$TEST" = "publish" ]]; then
-  pip install "mistune<2.0.0"
   pip install -r ../pulpcore/doc_requirements.txt
   pip install -r doc_requirements.txt
 fi
