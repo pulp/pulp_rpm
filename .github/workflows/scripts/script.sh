6fda888f3f2e49926c282e7ed9df1bba00ed3a4c
pulpbot <pulp-infra@redhat.com>
2022-02-13 02:46:40 +0000

    Update CI files
    [noissue]

--- .github/workflows/scripts/script.sh
+++ .github/workflows/scripts/script.sh
@@ -163,7 +163,7 @@ if [ -f $FUNC_TEST_SCRIPT ]; then
   source $FUNC_TEST_SCRIPT
 else
 
-    if [[ "$GITHUB_EVENT_NAME" == "schedule" ]]; then
+    if [[ "$GITHUB_WORKFLOW" == "Rpm Nightly CI/CD" ]]; then
         pytest -v -r sx --color=yes --suppress-no-test-exit-code --pyargs pulp_rpm.tests.functional -m parallel -n 8
         pytest -v -r sx --color=yes --pyargs pulp_rpm.tests.functional -m "not parallel"
     else
