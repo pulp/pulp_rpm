09c5779c832d2b737438f99689ae6aefb25d9b72
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2022-02-28 17:46:02 +0000

    Update CI files
    [noissue]

--- pulp_rpm/app/urls.py
+++ pulp_rpm/app/urls.py
@@ -1,8 +1,11 @@
+from django.conf import settings
 from django.urls import path
 
 from .viewsets import CopyViewSet, CompsXmlViewSet
 
+V3_API_ROOT = settings.V3_API_ROOT_NO_FRONT_SLASH
+
 urlpatterns = [
-    path("pulp/api/v3/rpm/copy/", CopyViewSet.as_view({"post": "create"})),
-    path("pulp/api/v3/rpm/comps/", CompsXmlViewSet.as_view({"post": "create"})),
+    path(f"{V3_API_ROOT}rpm/copy/", CopyViewSet.as_view({"post": "create"})),
+    path(f"{V3_API_ROOT}rpm/comps/", CompsXmlViewSet.as_view({"post": "create"})),
 ]
