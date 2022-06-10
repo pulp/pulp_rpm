d43984b3859cafe75e4af7953e1ae8c0de4820dc
Daniel Alley <dalley@redhat.com>
2022-06-10 16:06:07 -0400

    Assume libmodulemd 2.12+ is available (which is needed for new module support)
    closes #2276

--- pulp_rpm/app/modulemd.py
+++ pulp_rpm/app/modulemd.py
@@ -87,9 +87,7 @@ def parse_modulemd(module_names, module_index):
             modulemd[PULP_MODULE_ATTR.NAME] = stream.props.module_name
             modulemd[PULP_MODULE_ATTR.STREAM] = stream.props.stream_name
             modulemd[PULP_MODULE_ATTR.VERSION] = stream.props.version
-            modulemd[PULP_MODULE_ATTR.STATIC_CONTEXT] = getattr(
-                stream.props, "static_context", None
-            )
+            modulemd[PULP_MODULE_ATTR.STATIC_CONTEXT] = stream.props.static_context
             modulemd[PULP_MODULE_ATTR.CONTEXT] = stream.props.context
             modulemd[PULP_MODULE_ATTR.ARCH] = stream.props.arch
             modulemd[PULP_MODULE_ATTR.ARTIFACTS] = stream.get_rpm_artifacts()
