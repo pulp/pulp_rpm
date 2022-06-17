dacf20bf503a55ff815821166a801cbe4989c8a4
Dennis Kliban <dkliban@redhat.com>
2022-06-17 11:36:37 -0400

    Updates CI with latest plugin_template changes.
    Runs functional tests in container running Pulp.
    Removes ruby bindings test job.
    
    [noissue]

--- .github/workflows/scripts/install_python_client.sh
+++ .github/workflows/scripts/install_python_client.sh
@@ -7,7 +7,7 @@
 #
 # For more info visit https://github.com/pulp/plugin_template
 
-set -euv
+set -mveuo pipefail
 
 export PULP_URL="${PULP_URL:-https://pulp}"
 
@@ -30,7 +30,7 @@ export response=$(curl --write-out %{http_code} --silent --output /dev/null http
 if [ "$response" == "200" ];
 then
   echo "pulp_rpm client $VERSION has already been released. Installing from PyPI."
-  pip install pulp-rpm-client==$VERSION
+  docker exec pulp pip3 install pulp-rpm-client==$VERSION
   mkdir -p dist
   tar cvf python-client.tar ./dist
   exit
@@ -41,7 +41,7 @@ rm -rf pulp_rpm-client
 ./generate.sh pulp_rpm python $VERSION
 cd pulp_rpm-client
 python setup.py sdist bdist_wheel --python-tag py3
-find . -name "*.whl" -exec pip install {} \;
+find . -name "*.whl" -exec docker exec pulp pip3 install /root/pulp-openapi-generator/pulp_rpm-client/{} \;
 tar cvf ../../pulp_rpm/python-client.tar ./dist
 
 find ./docs/* -exec sed -i 's/Back to README/Back to HOME/g' {} \;
