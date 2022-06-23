afea95859a65628928899dd267919e5e1c7096ad
Dennis Kliban <dkliban@redhat.com>
2022-06-23 14:02:14 -0400

    Changes the CentOS mirror URL to http://mirror.centos.org
    [noissue]

--- pulp_rpm/tests/functional/constants.py
+++ pulp_rpm/tests/functional/constants.py
@@ -606,7 +606,7 @@ AMAZON_MIRROR = "http://amazonlinux.us-east-1.amazonaws.com/2/core/latest/x86_64
 CENTOS7_URL = "http://mirror.centos.org/centos-7/7/os/x86_64/"
 CENTOS8_STREAM_BASEOS_URL = "http://mirror.centos.org/centos/8-stream/BaseOS/x86_64/os/"
 CENTOS8_STREAM_APPSTREAM_URL = "http://mirror.centos.org/centos/8-stream/AppStream/x86_64/os/"
-CENTOS7_OPSTOOLS_URL = "http://ftp.cs.stanford.edu/centos/7/opstools/x86_64/"
+CENTOS7_OPSTOOLS_URL = "http://mirror.centos.org/centos/7/opstools/x86_64/"
 EPEL7_URL = "https://dl.fedoraproject.org/pub/epel/7/x86_64/"
 EPEL8_MIRRORLIST_URL = "https://mirrors.fedoraproject.org/mirrorlist?repo=epel-8&arch=x86_64"
 EPEL8_PLAYGROUND_KICKSTART_URL = "http://mirrors.sonic.net/epel/playground/8/Everything/x86_64/os/"
