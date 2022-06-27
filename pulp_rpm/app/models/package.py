pulp/pulp_rpm

commit bcb4eb86b18d2e2b1025f92d5cfbb78524a108e9

Optional relative_path
User doesn't have to specify ``relative_path``.
It is generated by NeVRA info from package if not specified.

closes: #2440
https://github.com/pulp/pulp_rpm/issues/2440

Require PR: https://github.com/pulp/pulp-cli/pull/507

pulp_rpm/app/models/package.py
@@ -247,16 +247,14 @@ class Package(Content):
         """
         Package NEVRA string (Name-Epoch-Version-Release-Architecture).
         """
-        return "{n}-{e}:{v}-{r}.{a}".format(
-            n=self.name, e=self.epoch, v=self.version, r=self.release, a=self.arch
-        )
+        return self.to_nevra(self.createrepo_to_dict(self))
 
     @property
     def nvra(self):
         """
         Package NVRA string (Name-Version-Release-Architecture).
         """
-        return "{n}-{v}-{r}.{a}".format(n=self.name, v=self.version, r=self.release, a=self.arch)
+        return self.to_nvra(self.createrepo_to_dict(self))
 
     class Meta:
         default_related_name = "%(app_label)s_%(model_name)s"

commit c2305c625b49194ad7a6853591517bfe0d45c652

Fix is_modular detection, regressed by #2537
[noissue]

pulp_rpm/app/models/package.py
@@ -340,7 +340,7 @@ class Package(Content):
 
         Returns: NEVRA as a string
         """
-        return f"{pkg['name']}-{pkg['epoch']}:{pkg['version']}-{pkg['release']}.{pkg['arch']}.rpm"
+        return f"{pkg['name']}-{pkg['epoch']}:{pkg['version']}-{pkg['release']}.{pkg['arch']}"
 
     @staticmethod
     def to_nvra(pkg):
@@ -352,7 +352,7 @@ class Package(Content):
 
         Returns: NVRA as a string
         """
-        return f"{pkg['name']}-{pkg['version']}-{pkg['release']}.{pkg['arch']}.rpm"
+        return f"{pkg['name']}-{pkg['version']}-{pkg['release']}.{pkg['arch']}"
 
     @classmethod
     def short_nevra(cls, pkg):

pulp_rpm/app/serializers/package.py
@@ -274,7 +274,7 @@ class PackageSerializer(SingleArtifactContentUploadSerializer, ContentChecksumSe
                 _("There is already a package with: {values}.").format(values=error_data)
             )
 
-        new_pkg["location_href"] = Package.short_nevra(new_pkg)
+        new_pkg["location_href"] = Package.short_nevra(new_pkg) + ".rpm"
         if not data.get("relative_path"):
             data["relative_path"] = new_pkg["location_href"]
 