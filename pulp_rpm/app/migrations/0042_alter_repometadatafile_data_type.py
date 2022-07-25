b67565afa79c17e8b4ebee6d422c7ac4460ccddf
Pavel Picka <ppicka@redhat.com>
2022-07-25 19:13:38 +0200

    Modulemd obsoletes migrations fix
    fix order of migrations to easy backport
    
    [noissue]

--- pulp_rpm/app/migrations/0042_alter_repometadatafile_data_type.py
+++ pulp_rpm/app/migrations/0042_alter_repometadatafile_data_type.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('rpm', '0040_rpmalternatecontentsource'),
+        ('rpm', '0041_modulemdobsolete'),
     ]
 
     operations = [
