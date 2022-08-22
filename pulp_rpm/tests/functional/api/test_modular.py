10037c41a3b8965f2a40ad2420c7702c6c010a72
Pavel Picka <ppicka@redhat.com>
2022-08-22 14:28:26 +0200

    [3.17] Test ``is_modular``
    Simple test to check that modular packages are marked as modular
    package. Added missing pytest config to run the test.
    
    closes: #2620
    https://github.com/pulp/pulp_rpm/issues/2620
    
    (cherry picked from commit https://github.com/pulp/pulp_rpm/commit/355f40a5bd5141de06d476fefa12c40f970fabbd)

---
+++ pulp_rpm/tests/functional/api/test_modular.py
@@ -0,0 +1,49 @@
+from pulpcore.client.pulp_rpm import RpmRepositorySyncURL
+
+from pulp_smash.pulp3.utils import gen_repo, get_content
+from pulp_smash.pulp3.bindings import monitor_task
+
+from pulp_rpm.tests.functional.utils import gen_rpm_remote
+from pulp_rpm.tests.functional.constants import (
+    RPM_SIGNED_FIXTURE_URL,
+    RPM_PACKAGE_CONTENT_NAME,
+    RPM_FIXTURE_SUMMARY,
+    RPM_MODULAR_FIXTURE_URL,
+)
+
+
+def test_is_modular_flag(
+    rpm_repository_api,
+    rpm_package_api,
+    rpm_rpmremote_api,
+    gen_object_with_cleanup,
+    delete_orphans_pre,
+):
+    """
+    Test package is marked as modular when synced from modular repository.
+    """
+
+    remote_data = gen_rpm_remote(RPM_SIGNED_FIXTURE_URL)
+    remote = gen_object_with_cleanup(rpm_rpmremote_api, remote_data)
+    repo_data = gen_repo(remote=remote.pulp_href)
+    repo = gen_object_with_cleanup(rpm_repository_api, repo_data)
+    sync_url = RpmRepositorySyncURL(remote=remote.pulp_href)
+    sync_response = rpm_repository_api.sync(repo.pulp_href, sync_url)
+    monitor_task(sync_response.task)
+
+    # assert no package is marked modular
+    assert rpm_package_api.list().count == RPM_FIXTURE_SUMMARY[RPM_PACKAGE_CONTENT_NAME]
+    for pkg in get_content(repo.to_dict())[RPM_PACKAGE_CONTENT_NAME]:
+        assert pkg["is_modular"] is False
+
+    remote_modular_data = gen_rpm_remote(RPM_MODULAR_FIXTURE_URL)
+    remote_modular = gen_object_with_cleanup(rpm_rpmremote_api, remote_modular_data)
+    repo_modular_data = gen_repo(remote=remote_modular.pulp_href)
+    repo_modular = gen_object_with_cleanup(rpm_repository_api, repo_modular_data)
+    sync_url = RpmRepositorySyncURL(remote=remote_modular.pulp_href)
+    sync_response = rpm_repository_api.sync(repo_modular.pulp_href, sync_url)
+    monitor_task(sync_response.task)
+
+    # assert all package from modular repo is marked as modular
+    for pkg in get_content(repo_modular.to_dict())[RPM_PACKAGE_CONTENT_NAME]:
+        assert pkg["is_modular"] is True
