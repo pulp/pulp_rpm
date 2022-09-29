8fa0a8abc609405c4e561ed74a8f31cc66667f8e
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2022-09-29 16:07:06 +0100

    Fix CI signing service
    [noissue]
    [nocoverage]

--- .github/workflows/scripts/post_before_script.sh
+++ .github/workflows/scripts/post_before_script.sh
@@ -6,17 +6,14 @@ if [[ "$TEST" == "upgrade" ]]; then
     exit
 fi
 
-cmd_stdin_prefix bash -c "cat > /root/sign-metadata.sh" < "$GITHUB_WORKSPACE"/pulp_rpm/tests/functional/sign-metadata.sh
+cmd_stdin_prefix bash -c "cat > /var/lib/pulp/scripts/sign-metadata.sh" < "$GITHUB_WORKSPACE"/pulp_rpm/tests/functional/sign-metadata.sh
 
-cmd_prefix bash -c "curl -L https://github.com/pulp/pulp-fixtures/raw/master/common/GPG-PRIVATE-KEY-pulp-qe | gpg --import"
-cmd_prefix bash -c "curl -O -L https://github.com/pulp/pulp-fixtures/raw/master/common/GPG-KEY-pulp-qe"
-cmd_prefix chmod a+x /root/sign-metadata.sh
+curl -L https://github.com/pulp/pulp-fixtures/raw/master/common/GPG-KEY-pulp-qe | cmd_stdin_prefix su pulp -c "cat > /tmp/GPG-KEY-pulp-qe"
+curl -L https://github.com/pulp/pulp-fixtures/raw/master/common/GPG-PRIVATE-KEY-pulp-qe | cmd_stdin_prefix su pulp -c "gpg --import"
+echo "6EDF301256480B9B801EBA3D05A5E6DA269D9D98:6:" | cmd_stdin_prefix gpg --import-ownertrust
+cmd_prefix chmod a+x /var/lib/pulp/scripts/sign-metadata.sh
 
-KEY_FINGERPRINT="6EDF301256480B9B801EBA3D05A5E6DA269D9D98"
-TRUST_LEVEL="6"
-echo "$KEY_FINGERPRINT:$TRUST_LEVEL:" | cmd_stdin_prefix gpg --import-ownertrust
-
-cmd_prefix bash -c "pulpcore-manager add-signing-service sign-metadata /root/sign-metadata.sh \"Pulp QE\""
+cmd_prefix su pulp -c "pulpcore-manager add-signing-service sign-metadata /var/lib/pulp/scripts/sign-metadata.sh \"Pulp QE\""
 
 echo "machine pulp
 login admin
