9bbb77c95e6febe4abbe9e5c313ceaa9e8a09537
pulpbot <pulp-infra@redhat.com>
2022-09-30 13:21:22 +0000

    Bump to 3.18.6.dev
    [noissue]

--- pulp_rpm/app/__init__.py
+++ pulp_rpm/app/__init__.py
@@ -8,5 +8,5 @@ class PulpRpmPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_rpm.app"
     label = "rpm"
-    version = "3.18.5"
+    version = "3.18.6.dev"
     python_package_name = "pulp-rpm"
