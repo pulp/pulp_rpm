3032958c1c69cbbcf1f51511c2a2caff336cfea3
pulpbot <pulp-infra@redhat.com>
2022-11-21 15:48:30 +0000

    Release 3.17.15 GH Issues: 2858,2892
    [noissue]

--- pulp_rpm/app/__init__.py
+++ pulp_rpm/app/__init__.py
@@ -8,5 +8,5 @@ class PulpRpmPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_rpm.app"
     label = "rpm"
-    version = "3.17.15.dev"
+    version = "3.17.15"
     python_package_name = "pulp-rpm"
