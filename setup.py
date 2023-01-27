1c3ac5c0a1d41ec1716c0f1a53ca07e991c0510a
pulpbot <pulp-infra@redhat.com>
2023-01-27 18:28:45 +0000

    Bump to 3.18.11.dev
    [noissue]

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.rst") as f:
 
 setup(
     name="pulp-rpm",
-    version="3.18.10",
+    version="3.18.11.dev",
     description="RPM plugin for the Pulp Project",
     long_description=long_description,
     license="GPLv2+",
