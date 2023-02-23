6e34de2bce0510a3b5f47dac35b9ff61e2296856
Lubos Mjachky <lmjachky@redhat.com>
2023-02-23 19:21:17 +0100

    Refactory copy tests
    closes #2874
    
    (cherry picked from commit 336cca1d9420f05b9fb3e801ebebb6b58591adcf)

--- pulp_rpm/tests/functional/conftest.py
+++ pulp_rpm/tests/functional/conftest.py
@@ -18,11 +18,11 @@ from pulpcore.client.pulp_rpm import (
     RemotesUlnApi,
     RepositoriesRpmApi,
     RepositoriesRpmVersionsApi,
+    RpmCopyApi,
     RpmRepositorySyncURL,
 )
 
-from pulp_rpm.tests.functional.constants import RPM_UNSIGNED_FIXTURE_URL
-from pulp_rpm.tests.functional.utils import gen_rpm_remote
+from pulp_rpm.tests.functional.constants import RPM_UNSIGNED_FIXTURE_URL, RPM_KICKSTART_FIXTURE_URL
 
 
 @pytest.fixture(scope="session")
@@ -114,8 +114,51 @@ def rpm_content_distribution_trees_api(rpm_client):
     return ContentDistributionTreesApi(rpm_client)
 
 
+@pytest.fixture(scope="session")
+def rpm_copy_api(rpm_client):
+    return RpmCopyApi(rpm_client)
+
+
 @pytest.fixture(scope="class")
-def init_and_sync(gen_object_with_cleanup, monitor_task, rpm_repository_api, rpm_rpmremote_api):
+def rpm_repository_factory(rpm_repository_api, gen_object_with_cleanup):
+    """A factory to generate an RPM Repository with auto-deletion after the test run."""
+
+    def _rpm_repository_factory(**kwargs):
+        data = {"name": str(uuid.uuid4())}
+        data.update(kwargs)
+        return gen_object_with_cleanup(rpm_repository_api, data)
+
+    return _rpm_repository_factory
+
+
+@pytest.fixture(scope="class")
+def rpm_rpmremote_factory(rpm_rpmremote_api, gen_object_with_cleanup):
+    """A factory to generate an RPM Remote with auto-deletion after the test run."""
+
+    def _rpm_rpmremote_factory(*, url=RPM_UNSIGNED_FIXTURE_URL, policy="immediate", **kwargs):
+        data = {"url": url, "policy": policy, "name": str(uuid.uuid4())}
+        data.update(kwargs)
+        return gen_object_with_cleanup(rpm_rpmremote_api, data)
+
+    return _rpm_rpmremote_factory
+
+
+@pytest.fixture(scope="class")
+def rpm_distribution_factory(rpm_distribution_api, gen_object_with_cleanup):
+    """A factory to generate an RPM Distribution with auto-deletion after the test run."""
+
+    def _rpm_distribution_factory(**kwargs):
+        data = {"base_path": str(uuid.uuid4()), "name": str(uuid.uuid4())}
+        data.update(kwargs)
+        return gen_object_with_cleanup(rpm_distribution_api, data)
+
+    return _rpm_distribution_factory
+
+
+@pytest.fixture(scope="class")
+def init_and_sync(rpm_repository_factory, rpm_repository_api, rpm_rpmremote_factory, monitor_task):
+    """Initialize a new repository and remote and sync the content from the passed URL."""
+
     def _init_and_sync(
         repository=None,
         remote=None,
@@ -124,13 +167,10 @@ def init_and_sync(gen_object_with_cleanup, monitor_task, rpm_repository_api, rpm
         sync_policy="additive",
         skip_types=None,
     ):
-        """Initialize a new repository and remote and sync the content from the passed URL."""
         if repository is None:
-            repository = gen_object_with_cleanup(rpm_repository_api, {"name": str(uuid.uuid4())})
+            repository = rpm_repository_factory()
         if remote is None:
-            remote = gen_object_with_cleanup(
-                rpm_rpmremote_api, gen_rpm_remote(url=url, policy=policy)
-            )
+            remote = rpm_rpmremote_factory(url=url, policy=policy)
 
         repository_sync_data = RpmRepositorySyncURL(
             remote=remote.pulp_href, sync_policy=sync_policy, skip_types=skip_types
@@ -147,10 +187,16 @@ def init_and_sync(gen_object_with_cleanup, monitor_task, rpm_repository_api, rpm
 @pytest.fixture(scope="class")
 def rpm_unsigned_repo_immediate(init_and_sync):
     repo, _ = init_and_sync()
-    yield repo
+    return repo
 
 
 @pytest.fixture(scope="class")
 def rpm_unsigned_repo_on_demand(init_and_sync):
     repo, _ = init_and_sync(policy="on_demand")
-    yield repo
+    return repo
+
+
+@pytest.fixture(scope="class")
+def rpm_kickstart_repo_immediate(init_and_sync):
+    repo, _ = init_and_sync(url=RPM_KICKSTART_FIXTURE_URL)
+    return repo
