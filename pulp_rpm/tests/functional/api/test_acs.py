4e51faad3b6ed220a632f08ba9effcfed3a67d83
Gerrod Ubben <gerrodubben@gmail.com>
2023-03-28 11:01:38 -0400

    Use pytest style for test_acs
    fixes: #2867
    (cherry picked from commit 4c699a39b212c6b69b85a7156d83fffb856efde4)

--- pulp_rpm/tests/functional/api/test_acs.py
+++ pulp_rpm/tests/functional/api/test_acs.py
@@ -1,16 +1,6 @@
-from pulp_smash import config
+import pytest
 
-from pulp_smash.pulp3.bindings import (
-    delete_orphans,
-    monitor_task,
-    monitor_task_group,
-    PulpTaskError,
-    PulpTestCase,
-)
-from pulp_smash.pulp3.utils import (
-    gen_repo,
-    get_content_summary,
-)
+from pulpcore.tests.functional.utils import PulpTaskError
 
 from pulp_rpm.tests.functional.constants import (
     PULP_FIXTURES_BASE_URL,
@@ -19,94 +9,70 @@ from pulp_rpm.tests.functional.constants import (
     RPM_KICKSTART_ONLY_META_FIXTURE_URL,
     RPM_ONLY_METADATA_REPO_URL,
 )
-from pulp_rpm.tests.functional.utils import gen_rpm_client, gen_rpm_remote
 
 from pulpcore.client.pulp_rpm import (
-    AcsRpmApi,
-    RemotesRpmApi,
-    RepositoriesRpmApi,
     RpmRepositorySyncURL,
 )
 
 
-class AlternateContentSourceSyncTestCase(PulpTestCase):
-    """
-    Test Alternate Content Source.
+@pytest.mark.parametrize(
+    "paths,remote_url,content_summary",
+    [
+        (["rpm-unsigned/"], RPM_ONLY_METADATA_REPO_URL, RPM_FIXTURE_SUMMARY),
+        (
+            ["rpm-distribution-tree/"],
+            RPM_KICKSTART_ONLY_META_FIXTURE_URL,
+            RPM_KICKSTART_FIXTURE_SUMMARY,
+        ),
+    ],
+)
+def test_acs_simple(
+    paths,
+    remote_url,
+    content_summary,
+    rpm_repository_version_api,
+    rpm_repository_api,
+    rpm_acs_api,
+    rpm_repository_factory,
+    rpm_rpmremote_factory,
+    monitor_task,
+    monitor_task_group,
+    gen_object_with_cleanup,
+    delete_orphans_pre,
+):
+    """Test to sync repo with use of ACS."""
+    # ACS is rpm-unsigned repository which has all packages needed
+    acs_remote = rpm_rpmremote_factory(url=PULP_FIXTURES_BASE_URL, policy="on_demand")
 
-    1. Create ACS
-    2. Sync from repo where only metadata is
-    """
+    acs_data = {
+        "name": "alternatecontentsource",
+        "remote": acs_remote.pulp_href,
+        "paths": paths,
+    }
+    acs = gen_object_with_cleanup(rpm_acs_api, acs_data)
 
-    @classmethod
-    def setUpClass(cls):
-        """Create class-wide variables."""
-        cls.cfg = config.get_config()
-        cls.client = gen_rpm_client()
-        cls.acs_api = AcsRpmApi(cls.client)
-        cls.repo_api = RepositoriesRpmApi(cls.client)
-        cls.remote_api = RemotesRpmApi(cls.client)
+    repo = rpm_repository_factory()
+    remote = rpm_rpmremote_factory(url=remote_url)
 
-        delete_orphans()
+    # Sync repo with metadata only, before ACS refresh it should fail
+    repository_sync_data = RpmRepositorySyncURL(remote=remote.pulp_href)
 
-    @classmethod
-    def tearDownClass(cls):
-        """Tear down."""
-        delete_orphans()
-
-    def do_test(self, acs_url, paths, remote_url):
-        """Sync with ACS test."""
-        # ACS is rpm-unsigned repository which has all packages needed
-        acs_remote = self.remote_api.create(gen_rpm_remote(url=acs_url, policy="on_demand"))
-        self.addCleanup(self.remote_api.delete, acs_remote.pulp_href)
-
-        acs_data = {
-            "name": "alternatecontentsource",
-            "remote": acs_remote.pulp_href,
-            "paths": paths,
-        }
-        acs = self.acs_api.create(acs_data)
-        self.addCleanup(self.acs_api.delete, acs.pulp_href)
-
-        repo = self.repo_api.create(gen_repo())
-        self.addCleanup(self.repo_api.delete, repo.pulp_href)
-        remote = self.remote_api.create(gen_rpm_remote(url=remote_url))
-        self.addCleanup(self.remote_api.delete, remote.pulp_href)
-
-        # Sync repo with metadata only, before ACS refresh it should fail
-        repository_sync_data = RpmRepositorySyncURL(remote=remote.pulp_href)
-
-        with self.assertRaises(PulpTaskError) as ctx:
-            sync_response = self.repo_api.sync(repo.pulp_href, repository_sync_data)
-            monitor_task(sync_response.task)
-
-        self.assertIn("404, message='Not Found'", ctx.exception.task.error["description"])
-
-        # ACS refresh
-        acs_refresh = self.acs_api.refresh(acs.pulp_href)
-        monitor_task_group(acs_refresh.task_group)
-
-        # Sync repository with metadata only
-        sync_response = self.repo_api.sync(repo.pulp_href, repository_sync_data)
+    with pytest.raises(PulpTaskError) as ctx:
+        sync_response = rpm_repository_api.sync(repo.pulp_href, repository_sync_data)
         monitor_task(sync_response.task)
 
-        return self.repo_api.read(repo.pulp_href)
+    assert "404, message='Not Found'" in ctx.value.task.error["description"]
 
-    def test_acs_simple(self):
-        """Test to sync repo with use of ACS."""
-        repo = self.do_test(
-            acs_url=PULP_FIXTURES_BASE_URL,
-            paths=["rpm-unsigned/"],
-            remote_url=RPM_ONLY_METADATA_REPO_URL,
-        )
+    # ACS refresh
+    acs_refresh = rpm_acs_api.refresh(acs.pulp_href)
+    monitor_task_group(acs_refresh.task_group)
 
-        self.assertEqual(get_content_summary(repo.to_dict()), RPM_FIXTURE_SUMMARY)
+    # Sync repository with metadata only
+    sync_response = rpm_repository_api.sync(repo.pulp_href, repository_sync_data)
+    monitor_task(sync_response.task)
 
-    def test_acs_with_dist_tree(self):
-        """Test to sync repo with distribution tree."""
-        repo = self.do_test(
-            acs_url=PULP_FIXTURES_BASE_URL,
-            paths=["rpm-distribution-tree/"],
-            remote_url=RPM_KICKSTART_ONLY_META_FIXTURE_URL,
-        )
+    repo = rpm_repository_api.read(repo.pulp_href)
+    repo_ver = rpm_repository_version_api.read(repo.latest_version_href)
 
-        self.assertEqual(get_content_summary(repo.to_dict()), RPM_KICKSTART_FIXTURE_SUMMARY)
+    present_summary = {k: v["count"] for k, v in repo_ver.content_summary.present.items()}
+    assert present_summary == content_summary
