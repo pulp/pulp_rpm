f7e0997b58c7a8f471d85706c8004eee07a77d69
Lubos Mjachky <lmjachky@redhat.com>
2023-04-04 17:42:23 +0200

    Rewrite retention policy tests
    closes #2885
    
    (cherry picked from commit 5e5936829d1f18a16a96985af49762bc1b24c1ae)

--- pulp_rpm/tests/functional/api/test_retention_policy.py
+++ pulp_rpm/tests/functional/api/test_retention_policy.py
@@ -1,20 +1,7 @@
 """Tests for the retention policy feature of repositories."""
+import pytest
 
 from collections import defaultdict
-
-from pulp_smash.pulp3.bindings import (
-    delete_orphans,
-    monitor_task,
-    PulpTestCase,
-)
-from pulp_smash.pulp3.utils import (
-    gen_repo,
-    get_added_content_summary,
-    get_content,
-    get_content_summary,
-    get_removed_content,
-    get_removed_content_summary,
-)
 
 from pulp_rpm.tests.functional.constants import (
     PULP_TYPE_PACKAGE,
@@ -24,225 +11,178 @@ from pulp_rpm.tests.functional.constants import (
     RPM_MODULES_STATIC_CONTEXT_FIXTURE_URL,
     RPM_MODULAR_STATIC_FIXTURE_SUMMARY,
 )
-from pulp_rpm.tests.functional.utils import (
-    gen_rpm_client,
-    gen_rpm_remote,
-)
-from pulp_rpm.tests.functional.utils import set_up_module as setUpModule  # noqa:F401
-
-from pulpcore.client.pulp_rpm import (
-    RepositoriesRpmApi,
-    RpmRepositorySyncURL,
-    RemotesRpmApi,
-)
 from pulpcore.client.pulp_rpm.exceptions import ApiException
 
 
-class RetentionPolicyTestCase(PulpTestCase):
-    """Verify functionality of the "retain_package_versions" setting on a Repository."""
+def test_sync_with_retention(
+    delete_orphans_pre,
+    init_and_sync,
+    rpm_repository_api,
+    rpm_repository_version_api,
+    rpm_package_api,
+    monitor_task,
+):
+    """Verify functionality with sync.
 
-    @classmethod
-    def setUpClass(cls):
-        """Create class-wide variables."""
-        cls.client = gen_rpm_client()
-        cls.repo_api = RepositoriesRpmApi(cls.client)
-        cls.remote_api = RemotesRpmApi(cls.client)
+    Do the following:
 
-    def test_sync_with_retention(self):
-        """Verify functionality with sync.
+    1. Create a repository, and a remote.
+    2. Sync the remote.
+    3. Assert that the correct number of units were added and are present
+       in the repo.
+    4. Change the "retain_package_versions" on the repository to 1 (retain the latest
+       version only).
+    5. Sync the remote one more time.
+    6. Assert that repository version is different from the previous one.
+    7. Assert the repository version we end with has only one version of each package.
+    """
+    repo, remote, task = init_and_sync(policy="on_demand", optimize=False, return_task=True)
 
-        Do the following:
+    # Test that, by default, everything is retained / nothing is tossed out.
+    version = rpm_repository_version_api.read(repo.latest_version_href)
+    present = {k: v["count"] for k, v in version.content_summary.present.items()}
+    added = {k: v["count"] for k, v in version.content_summary.added.items()}
+    assert present == RPM_FIXTURE_SUMMARY
+    assert added == RPM_FIXTURE_SUMMARY
+    # Test that the # of packages processed is correct
+    reports = get_progress_reports_by_code(task)
+    assert reports["sync.parsing.packages"].total == RPM_PACKAGE_COUNT
 
-        1. Create a repository, and a remote.
-        2. Sync the remote.
-        3. Assert that the correct number of units were added and are present
-           in the repo.
-        4. Change the "retain_package_versions" on the repository to 1 (retain the latest
-           version only).
-        5. Sync the remote one more time.
-        6. Assert that repository version is different from the previous one.
-        7. Assert the repository version we end with has only one version of each package.
-        """
-        delete_orphans()
+    # Set the retention policy to retain only 1 version of each package
+    repo_data = repo.to_dict()
+    repo_data.update({"retain_package_versions": 1})
+    monitor_task(rpm_repository_api.update(repo.pulp_href, repo_data).task)
+    repo = rpm_repository_api.read(repo.pulp_href)
 
-        repo = self.repo_api.create(gen_repo())
-        self.addCleanup(self.repo_api.delete, repo.pulp_href)
+    repo, remote, task = init_and_sync(
+        repository=repo, remote=remote, optimize=False, return_task=True
+    )
 
-        remote = self.remote_api.create(gen_rpm_remote(policy="on_demand"))
-        self.addCleanup(self.remote_api.delete, remote.pulp_href)
+    # Test that only one version of each package is present
+    content = rpm_package_api.list(repository_version=repo.latest_version_href).results
+    assert check_retention_policy(content, 1)
+    # Test that (only) 4 RPMs were removed (no advisories etc. touched)
+    version = rpm_repository_version_api.read(repo.latest_version_href)
+    removed = {k: v["count"] for k, v in version.content_summary.removed.items()}
+    assert removed == {PULP_TYPE_PACKAGE: 4}
+    # Test that the versions that were removed are the versions we expect.
+    content = rpm_package_api.list(repository_version_removed=repo.latest_version_href).results
+    versions = versions_for_packages(content)
+    assert versions == {"duck": ["0.6", "0.7"], "kangaroo": ["0.2"], "walrus": ["0.71"]}, versions
+    # Test that the number of packages processed is correct (doesn't include older ones)
+    reports = get_progress_reports_by_code(task)
+    assert reports["sync.parsing.packages"].total == RPM_PACKAGE_COUNT
+    assert reports["sync.skipped.packages"].total == 4
 
-        task = self.sync(repository=repo, remote=remote, optimize=False)
-        repo = self.repo_api.read(repo.pulp_href)
 
-        # Test that, by default, everything is retained / nothing is tossed out.
-        self.assertDictEqual(get_content_summary(repo.to_dict()), RPM_FIXTURE_SUMMARY)
-        self.assertDictEqual(get_added_content_summary(repo.to_dict()), RPM_FIXTURE_SUMMARY)
-        # Test that the # of packages processed is correct
-        reports = self.get_progress_reports_by_code(task)
-        self.assertEqual(reports["sync.parsing.packages"].total, RPM_PACKAGE_COUNT)
+def test_sync_with_retention_and_modules(
+    delete_orphans_pre,
+    init_and_sync,
+    rpm_repository_api,
+    rpm_repository_version_api,
+    monitor_task,
+):
+    """Verify functionality with sync.
 
-        # Set the retention policy to retain only 1 version of each package
-        repo_data = repo.to_dict()
-        repo_data.update({"retain_package_versions": 1})
-        self.repo_api.update(repo.pulp_href, repo_data)
-        repo = self.repo_api.read(repo.pulp_href)
+    Do the following:
 
-        task = self.sync(repository=repo, remote=remote, optimize=False)
-        repo = self.repo_api.read(repo.pulp_href)
+    1. Create a repository, and a remote.
+    2. Sync the remote.
+    3. Assert that the correct number of units were added and are present in the repo.
+    4. Change the "retain_package_versions" on the repository to 1 (retain the latest
+       version only).
+    5. Sync the remote one more time.
+    6. Assert that repository version is the same as the previous one, because the older
+       versions are part of modules, and they should be ignored by the retention policy.
+    """
 
-        # Test that only one version of each package is present
-        self.assertTrue(
-            self.check_retention_policy(get_content(repo.to_dict())[PULP_TYPE_PACKAGE], 1)
-        )
-        # Test that (only) 4 RPMs were removed (no advisories etc. touched)
-        self.assertDictEqual(get_removed_content_summary(repo.to_dict()), {PULP_TYPE_PACKAGE: 4})
-        # Test that the versions that were removed are the versions we expect.
-        versions_for_packages = self.versions_for_packages(
-            get_removed_content(repo.to_dict())[PULP_TYPE_PACKAGE]
-        )
-        self.assertDictEqual(
-            versions_for_packages,
-            {"duck": ["0.6", "0.7"], "kangaroo": ["0.2"], "walrus": ["0.71"]},
-            versions_for_packages,
-        )
-        # Test that the number of packages processed is correct (doesn't include older ones)
-        reports = self.get_progress_reports_by_code(task)
-        self.assertEqual(reports["sync.parsing.packages"].total, RPM_PACKAGE_COUNT)
-        self.assertEqual(reports["sync.skipped.packages"].total, 4)
+    repo, remote, task = init_and_sync(
+        url=RPM_MODULES_STATIC_CONTEXT_FIXTURE_URL,
+        policy="on_demand",
+        optimize=False,
+        return_task=True,
+    )
 
-    def test_sync_with_retention_and_modules(self):
-        """Verify functionality with sync.
+    # Test that, by default, everything is retained / nothing is tossed out.
+    version = rpm_repository_version_api.read(repo.latest_version_href)
+    present = {k: v["count"] for k, v in version.content_summary.present.items()}
+    added = {k: v["count"] for k, v in version.content_summary.added.items()}
+    assert present == RPM_MODULAR_STATIC_FIXTURE_SUMMARY
+    assert added == RPM_MODULAR_STATIC_FIXTURE_SUMMARY
+    # Test that the # of packages processed is correct
+    reports = get_progress_reports_by_code(task)
+    assert reports["sync.parsing.packages"].total == RPM_MODULAR_PACKAGE_COUNT
+    assert reports["sync.skipped.packages"].total == 0
 
-        Do the following:
+    # Set the retention policy to retain only 1 version of each package
+    repo_data = repo.to_dict()
+    repo_data.update({"retain_package_versions": 1})
+    monitor_task(rpm_repository_api.update(repo.pulp_href, repo_data).task)
+    repo = rpm_repository_api.read(repo.pulp_href)
 
-        1. Create a repository, and a remote.
-        2. Sync the remote.
-        3. Assert that the correct number of units were added and are present in the repo.
-        4. Change the "retain_package_versions" on the repository to 1 (retain the latest
-           version only).
-        5. Sync the remote one more time.
-        6. Assert that repository version is the same as the previous one, because the older
-           versions are part of modules, and they should be ignored by the retention policy.
-        """
-        delete_orphans()
+    repo, remote, task = init_and_sync(
+        repository=repo, remote=remote, optimize=False, return_task=True
+    )
 
-        repo = self.repo_api.create(gen_repo())
-        self.addCleanup(self.repo_api.delete, repo.pulp_href)
+    # Test that no RPMs were removed (and no advisories etc. touched)
+    # it should be the same because the older version are covered by modules
+    version = rpm_repository_version_api.read(repo.latest_version_href)
+    assert version.content_summary.removed == {}
+    # Test that the number of packages processed is correct
+    reports = get_progress_reports_by_code(task)
+    assert reports["sync.parsing.packages"].total == RPM_MODULAR_PACKAGE_COUNT
+    assert reports["sync.skipped.packages"].total == 0
 
-        remote = self.remote_api.create(
-            gen_rpm_remote(
-                url=RPM_MODULES_STATIC_CONTEXT_FIXTURE_URL,
-                policy="on_demand",
-            )
-        )
-        self.addCleanup(self.remote_api.delete, remote.pulp_href)
 
-        task = self.sync(repository=repo, remote=remote, optimize=False)
-        repo = self.repo_api.read(repo.pulp_href)
+@pytest.mark.parallel
+def test_mirror_sync_with_retention_fails(init_and_sync, rpm_repository_factory):
+    """Verify functionality with sync.
 
-        self.addCleanup(delete_orphans)  # TODO: #2587
+    Do the following:
 
-        # Test that, by default, everything is retained / nothing is tossed out.
-        self.assertDictEqual(
-            get_content_summary(repo.to_dict()), RPM_MODULAR_STATIC_FIXTURE_SUMMARY
-        )
-        self.assertDictEqual(
-            get_added_content_summary(repo.to_dict()), RPM_MODULAR_STATIC_FIXTURE_SUMMARY
-        )
-        # Test that the # of packages processed is correct
-        reports = self.get_progress_reports_by_code(task)
-        self.assertEqual(reports["sync.parsing.packages"].total, RPM_MODULAR_PACKAGE_COUNT)
-        self.assertEqual(reports["sync.skipped.packages"].total, 0)
+    1. Create a repository with 'retain_package_versions' set, and a remote.
+    2. Sync the remote in mirror mode.
+    3. Assert that the sync fails.
+    """
+    repo = rpm_repository_factory(retain_package_versions=1)
+    with pytest.raises(ApiException) as exc:
+        init_and_sync(repository=repo, optimize=False, sync_policy="mirror_complete")
 
-        # Set the retention policy to retain only 1 version of each package
-        repo_data = repo.to_dict()
-        repo_data.update({"retain_package_versions": 1})
-        self.repo_api.update(repo.pulp_href, repo_data)
-        repo = self.repo_api.read(repo.pulp_href)
+    assert exc.value.status == 400
 
-        task = self.sync(repository=repo, remote=remote, optimize=False)
-        repo = self.repo_api.read(repo.pulp_href)
 
-        # Test that no RPMs were removed (and no advisories etc. touched)
-        # it should be the same because the older version are covered by modules)
-        self.assertDictEqual(get_removed_content_summary(repo.to_dict()), {})
-        # Test that the number of packages processed is correct
-        reports = self.get_progress_reports_by_code(task)
-        self.assertEqual(reports["sync.parsing.packages"].total, RPM_MODULAR_PACKAGE_COUNT)
-        self.assertEqual(reports["sync.skipped.packages"].total, 0)
+def get_progress_reports_by_code(task):
+    """Return the progress reports in a dictionary keyed by codename."""
+    return {report.code: report for report in task.progress_reports}
 
-    def test_mirror_sync_with_retention_fails(self):
-        """Verify functionality with sync.
 
-        Do the following:
+def versions_for_packages(packages):
+    """Get a list of versions for each package present in a list of Package dicts.
 
-        1. Create a repository with 'retain_package_versions' set, and a remote.
-        2. Sync the remote in mirror mode.
-        3. Assert that the sync fails.
-        """
-        repo_data = gen_repo()
-        repo_data.update({"retain_package_versions": 1})
-        repo = self.repo_api.create(repo_data)
-        self.addCleanup(self.repo_api.delete, repo.pulp_href)
+    Args:
+        packages: List of Package info dicts
+    """
+    packages_by_version = defaultdict(list)
 
-        remote = self.remote_api.create(gen_rpm_remote())
-        self.addCleanup(self.remote_api.delete, remote.pulp_href)
+    for package in packages:
+        packages_by_version[package.name].append(package.version)
 
-        with self.assertRaises(ApiException) as exc:
-            self.sync(repository=repo, remote=remote, optimize=False, mirror=True)
-            self.assertEqual(exc.code, 400)
+    for pkg_list in packages_by_version.values():
+        pkg_list.sort()
 
-    def get_progress_reports_by_code(self, task):
-        """Return the progress reports in a dictionary keyed by codename."""
-        return {report.code: report for report in task.progress_reports}
+    return packages_by_version
 
-    def versions_for_packages(self, packages):
-        """Get a list of versions for each package present in a list of Package dicts.
 
-        Args:
-            packages: List of Package info dicts
-        """
-        packages_by_version = defaultdict(list)
+def check_retention_policy(packages, retain_package_versions):
+    """Check that the number of versions of each package <= permitted number.
 
-        for package in packages:
-            packages_by_version[package["name"]].append(package["version"])
-
-        for pkg_list in packages_by_version.values():
-            pkg_list.sort()
-
-        return packages_by_version
-
-    def check_retention_policy(self, packages, retain_package_versions):
-        """Check that the number of versions of each package <= permitted number.
-
-        Args:
-            packages: List of Package info dicts
-            retention_policy: Number of package versions permitted.
-        """
-        return all(
-            [
-                len(versions) <= retain_package_versions
-                for versions in self.versions_for_packages(packages).values()
-            ]
-        )
-
-    def sync(self, repository, remote, optimize=True, mirror=False):
-        """Sync a repository and return the task.
-
-        Args:
-            repository (pulp_rpm.app.models.repository.RpmRepository):
-                object of RPM repository
-            remote (pulp_rpm.app.models.repository.RpmRemote):
-                object of RPM Remote
-            optimize (bool):
-                whether to enable optimized sync
-            mirror (bool):
-                whether to use mirror-mode sync
-        Returns (list):
-            list of the ProgressReport objects created from this sync
-        """
-        repository_sync_data = RpmRepositorySyncURL(
-            remote=remote.pulp_href, optimize=optimize, mirror=mirror
-        )
-        sync_response = self.repo_api.sync(repository.pulp_href, repository_sync_data)
-        return monitor_task(sync_response.task)
+    Args:
+        packages: List of Package info dicts
+        retention_policy: Number of package versions permitted.
+    """
+    return all(
+        [
+            len(versions) <= retain_package_versions
+            for versions in versions_for_packages(packages).values()
+        ]
+    )
