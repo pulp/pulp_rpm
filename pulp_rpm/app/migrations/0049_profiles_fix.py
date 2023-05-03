75deb3b822fe404372e8012aada4f5681bba1f83
Daniel Alley <dalley@redhat.com>
2023-05-03 01:27:53 -0400

    Fix a regression in profiles formatting
    Make sure we use the same formatting in 3.19 as in 3.18
    
    closes #3131
    
    (cherry picked from commit f105a8b556e7dc3b5f74db5104212c60f39a0c48)

---
+++ pulp_rpm/app/migrations/0049_profiles_fix.py
@@ -0,0 +1,39 @@
+from django.db import migrations
+import yaml
+
+
+def fixup_modulemd_profiles(apps, schema_editor):
+    """ Set "profiles" for any saved post-3.19 back to the 3.18 format.
+    """
+    Modulemd = apps.get_model("rpm", "Modulemd")
+
+    modules_to_update = []
+
+    for mmd in Modulemd.objects.all().iterator():
+        modulemd = yaml.safe_load(mmd.snippet)
+        unprocessed_profiles = modulemd["data"].get("profiles", {})
+        profiles = {}
+        if unprocessed_profiles:
+            for name, data in unprocessed_profiles.items():
+                rpms = data.get("rpms")
+                if rpms:
+                    profiles[name] = rpms
+            mmd.profiles = profiles
+            modules_to_update.append(mmd)
+
+        if len(modules_to_update) >= 100:
+            Modulemd.objects.bulk_update(modules_to_update, ["profiles"])
+            modules_to_update.clear()
+
+    Modulemd.objects.bulk_update(modules_to_update, ["profiles"])
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('rpm', '0048_artifacts_dependencies_fix'),
+    ]
+
+    operations = [
+        migrations.RunPython(fixup_modulemd_profiles),
+    ]
