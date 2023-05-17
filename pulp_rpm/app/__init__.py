9fbd3227745fa99f104c2864a41c985ec3b9133e
pulpbot <pulp-infra@redhat.com>
2023-05-17 17:31:19 +0000

    Bump minor version [noissue]

--- pulp_rpm/app/__init__.py
+++ pulp_rpm/app/__init__.py
@@ -8,5 +8,5 @@ class PulpRpmPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_rpm.app"
     label = "rpm"
-    version = "3.21.0.dev"
+    version = "3.22.0.dev"
     python_package_name = "pulp-rpm"
