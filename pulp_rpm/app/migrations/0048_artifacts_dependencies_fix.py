31c75c84f9943358579a48054af6ced03131621c
Grant Gainey <ggainey@redhat.com>
2023-06-28 13:46:44 -0400

    Made migration more robust when there are "broken" modulemds.
    fixes #3177.
    
    (cherry picked from commit b5ca79ae1f3121d249739f084f206f19388049e0)

--- pulp_rpm/app/migrations/0048_artifacts_dependencies_fix.py
+++ pulp_rpm/app/migrations/0048_artifacts_dependencies_fix.py
@@ -11,12 +11,13 @@ def fixup_modulemd_artifacts_dependencies(apps, schema_editor):
 
     for mmd in Modulemd.objects.all().iterator():
         modulemd = yaml.safe_load(mmd.snippet)
-        mmd.artifacts = modulemd["data"].get("artifacts", {}).get("rpms", [])
-        mmd.dependencies = modulemd["data"].get("dependencies", [])
-        modules_to_update.append(mmd)
-        if len(modules_to_update) >= 100:
-            Modulemd.objects.bulk_update(modules_to_update, ["artifacts", "dependencies"])
-            modules_to_update.clear()
+        if modulemd:
+            mmd.artifacts = modulemd["data"].get("artifacts", {}).get("rpms", [])
+            mmd.dependencies = modulemd["data"].get("dependencies", [])
+            modules_to_update.append(mmd)
+            if len(modules_to_update) >= 100:
+                Modulemd.objects.bulk_update(modules_to_update, ["artifacts", "dependencies"])
+                modules_to_update.clear()
 
     Modulemd.objects.bulk_update(modules_to_update, ["artifacts", "dependencies"])
 
