cf2f171fc1814e83c93a7d911fdca04544f35f29
Grant Gainey <ggainey@redhat.com>
2023-07-17 15:50:04 -0400

    Made another migration more robust when there are "broken" modulemds.
    fixes #3196.
    
    (cherry picked from commit 2d7bedabede13d441cb4483a4098af59d1f6cb87)

--- pulp_rpm/app/migrations/0049_profiles_fix.py
+++ pulp_rpm/app/migrations/0049_profiles_fix.py
@@ -11,19 +11,20 @@ def fixup_modulemd_profiles(apps, schema_editor):
 
     for mmd in Modulemd.objects.all().iterator():
         modulemd = yaml.safe_load(mmd.snippet)
-        unprocessed_profiles = modulemd["data"].get("profiles", {})
-        profiles = {}
-        if unprocessed_profiles:
-            for name, data in unprocessed_profiles.items():
-                rpms = data.get("rpms")
-                if rpms:
-                    profiles[name] = rpms
-            mmd.profiles = profiles
-            modules_to_update.append(mmd)
+        if modulemd:
+            unprocessed_profiles = modulemd["data"].get("profiles", {})
+            profiles = {}
+            if unprocessed_profiles:
+                for name, data in unprocessed_profiles.items():
+                    rpms = data.get("rpms")
+                    if rpms:
+                        profiles[name] = rpms
+                mmd.profiles = profiles
+                modules_to_update.append(mmd)
 
-        if len(modules_to_update) >= 100:
-            Modulemd.objects.bulk_update(modules_to_update, ["profiles"])
-            modules_to_update.clear()
+            if len(modules_to_update) >= 100:
+                Modulemd.objects.bulk_update(modules_to_update, ["profiles"])
+                modules_to_update.clear()
 
     Modulemd.objects.bulk_update(modules_to_update, ["profiles"])
 
