6848ed376e484bc0fab724fcead4a6010272a203
pulpbot <pulp-infra@redhat.com>
2023-07-24 13:43:55 +0000

    Release 3.19.9 GH Issues: 3196
    [noissue]

--- pulp_rpm/app/__init__.py
+++ pulp_rpm/app/__init__.py
@@ -8,5 +8,5 @@ class PulpRpmPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_rpm.app"
     label = "rpm"
-    version = "3.19.9.dev"
+    version = "3.19.9"
     python_package_name = "pulp-rpm"
