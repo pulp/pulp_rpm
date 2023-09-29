b4eebfb90974f8d2ac4212f342f3d10e33bda76c
pulpbot <pulp-infra@redhat.com>
2023-09-29 16:23:30 +0000

    Release 3.18.18 GH Issues: 3225,3259,3226
    [noissue]

--- pulp_rpm/app/__init__.py
+++ pulp_rpm/app/__init__.py
@@ -8,5 +8,5 @@ class PulpRpmPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_rpm.app"
     label = "rpm"
-    version = "3.18.18.dev"
+    version = "3.18.18"
     python_package_name = "pulp-rpm"
