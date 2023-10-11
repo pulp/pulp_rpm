7617df94c673b703aef02b8476fc948457e265f9
Daniel Alley <dalley@redhat.com>
2023-10-10 21:56:13 -0400

    Export optimizations
    Django-import-export isn't smart enough to avoid the fk lookup even if
    it only sierializes the fk directly.
    
    [noissue]
    
    (cherry picked from commit 64f779921d22a68e0827a68a20652f236c78445d)

--- pulp_rpm/app/modelresource.py
+++ pulp_rpm/app/modelresource.py
@@ -73,6 +73,9 @@ class RpmContentResource(BaseContentResource):
 
         return content
 
+    def dehydrate__pulp_domain(self, content):
+        return str(content._pulp_domain_id)
+
 
 class ModulemdResource(RpmContentResource):
     """
@@ -401,9 +404,13 @@ class UpdateCollectionResource(QueryModelResource):
             UpdateCollections belonging to UpdateRecords for a specified repo-version.
 
         """
-        return UpdateCollection.objects.filter(
-            update_record__in=UpdateRecord.objects.filter(pk__in=self.repo_version.content)
-        ).order_by("pulp_id")
+        return (
+            UpdateCollection.objects.filter(
+                update_record__in=UpdateRecord.objects.filter(pk__in=self.repo_version.content)
+            )
+            .order_by("pulp_id")
+            .select_related("update_record")
+        )
 
     class Meta:
         model = UpdateCollection
@@ -430,9 +437,13 @@ class UpdateReferenceResource(QueryModelResource):
             UpdateReferences belonging to UpdateRecords for a specified repo-version.
 
         """
-        return UpdateReference.objects.filter(
-            update_record__in=UpdateRecord.objects.filter(pk__in=self.repo_version.content)
-        ).order_by("pulp_id")
+        return (
+            UpdateReference.objects.filter(
+                update_record__in=UpdateRecord.objects.filter(pk__in=self.repo_version.content)
+            )
+            .order_by("pulp_id")
+            .select_related("update_record")
+        )
 
     class Meta:
         model = UpdateReference
@@ -519,6 +530,7 @@ class UpdateCollectionPackageResource(QueryModelResource):
             )
             .distinct("name", "epoch", "version", "release", "arch")
             .order_by("name", "epoch", "version", "release", "arch")
+            .select_related("update_collection", "update_collection__update_record")
         )
 
     class Meta:
