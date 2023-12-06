baa47d697e475e77bce11c339aa38528f3f5da4b
pulpbot <pulp-infra@redhat.com>
2023-12-06 11:13:29 +0000

    Update CI files
    [noissue]

--- .github/workflows/scripts/install_python_client.sh
+++ .github/workflows/scripts/install_python_client.sh
@@ -52,6 +52,7 @@ DOCSYAML
 # Building the bindings docs
 mkdocs build
 
-tar cvf ../../pulp_rpm/rpm-python-client-docs.tar ./docs
+# Pack the built site.
+tar cvf ../../pulp_rpm/rpm-python-client-docs.tar ./site
 popd
 popd
