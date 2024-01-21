dbb3db146d82a828aef5dd388dad8611c9a61def
pulpbot <pulp-infra@redhat.com>
2024-01-21 02:38:45 +0000

    Update CI files
    [noissue]

--- .github/workflows/scripts/install_ruby_client.sh
+++ .github/workflows/scripts/install_ruby_client.sh
@@ -14,8 +14,10 @@ cd "$(dirname "$(realpath -e "$0")")"/../../..
 
 source .github/workflows/scripts/utils.sh
 
-export PULP_URL="${PULP_URL:-https://pulp}"
-
+PULP_URL="${PULP_URL:-https://pulp}"
+export PULP_URL
+PULP_API_ROOT="${PULP_API_ROOT:-/pulp/}"
+export PULP_API_ROOT
 
 REPORTED_STATUS="$(pulp status)"
 REPORTED_VERSION="$(echo "$REPORTED_STATUS" | jq --arg plugin "rpm" -r '.versions[] | select(.component == $plugin) | .version')"
@@ -23,7 +25,15 @@ VERSION="$(echo "$REPORTED_VERSION" | python -c 'from packaging.version import V
 
 pushd ../pulp-openapi-generator
 rm -rf pulp_rpm-client
-./generate.sh pulp_rpm ruby "$VERSION"
+
+if pulp debug has-plugin --name "core" --specifier ">=3.44.0.dev"
+then
+  curl --fail-with-body -k -o api.json "${PULP_URL}${PULP_API_ROOT}api/v3/docs/api.json?bindings&component=rpm"
+  USE_LOCAL_API_JSON=1 ./generate.sh pulp_rpm ruby "$VERSION"
+else
+  ./generate.sh pulp_rpm ruby "$VERSION"
+fi
+
 pushd pulp_rpm-client
 gem build pulp_rpm_client
 gem install --both "./pulp_rpm_client-$VERSION.gem"
