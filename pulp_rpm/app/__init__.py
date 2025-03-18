d935998660eae024d4ecfa986e412ef48b0b7484
pulpbot <pulp-infra@redhat.com>
2025-03-18 15:00:19 +0000

    Bump version: 3.19.13 → 3.19.14.dev

--- pulp_rpm/app/__init__.py
+++ pulp_rpm/app/__init__.py
@@ -8,5 +8,5 @@ class PulpRpmPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_rpm.app"
     label = "rpm"
-    version = "3.19.13"
+    version = "3.19.14.dev"
     python_package_name = "pulp-rpm"
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.rst") as f:
 
 setup(
     name="pulp-rpm",
-    version="3.19.13",
+    version="3.19.14.dev",
     description="RPM plugin for the Pulp Project",
     long_description=long_description,
     long_description_content_type="text/x-rst",
