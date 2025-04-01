3328ec086873b561b00e5943fd0a05682f06afe6
Jonathan Dieter <jdieter@ciq.com>
2025-04-01 15:29:03 +0100

    Handle chunked uploads when signing rpms
    Currently chunked uploads are rejected when signing rpms with the following
    message: "To sign a package on upload, a file must be provided."  This is
    because the pulp cli sends the ID for the upload rather than a file.
    
    To fix this, I've extended the serializer and viewset to handle both a file
    and an upload, and have updated the signing task to properly deal with an
    upload.
    
    closes #3927
    
    Signed-off-by: Jonathan Dieter <jdieter@ciq.com>

--- pulp_rpm/app/tasks/signing.py
+++ pulp_rpm/app/tasks/signing.py
@@ -1,10 +1,24 @@
 from tempfile import NamedTemporaryFile
 
-from pulpcore.plugin.models import Artifact, CreatedResource, PulpTemporaryFile
+from pulpcore.plugin.models import Artifact, CreatedResource, PulpTemporaryFile, Upload, UploadChunk
 from pulpcore.plugin.tasking import general_create
 from pulpcore.plugin.util import get_url
 
 from pulp_rpm.app.models.content import RpmPackageSigningService
+
+
+def _save_file(fileobj, final_package):
+    with fileobj.file.open() as fd:
+        final_package.write(fd.read())
+    final_package.flush()
+
+
+def _save_upload(uploadobj, final_package):
+    chunks = UploadChunk.objects.filter(upload=uploadobj).order_by("offset")
+    for chunk in chunks:
+        final_package.write(chunk.file.read())
+        chunk.file.close()
+    final_package.flush()
 
 
 def sign_and_create(
@@ -21,11 +35,13 @@ def sign_and_create(
 
     # Get unsigned package file and sign it
     package_signing_service = RpmPackageSigningService.objects.get(pk=signing_service_pk)
-    uploaded_package = PulpTemporaryFile.objects.get(pk=temporary_file_pk)
     with NamedTemporaryFile(mode="wb", dir=".", delete=False) as final_package:
-        with uploaded_package.file.open() as fd:
-            final_package.write(fd.read())
-            final_package.flush()
+        try:
+            uploaded_package = PulpTemporaryFile.objects.get(pk=temporary_file_pk)
+            _save_file(uploaded_package, final_package)
+        except PulpTemporaryFile.DoesNotExist:
+            uploaded_package = Upload.objects.get(pk=temporary_file_pk)
+            _save_upload(uploaded_package, final_package)
 
         package_signing_service.sign(final_package.name, pubkey_fingerprint=signing_fingerprint)
         artifact = Artifact.init_and_validate(final_package.name)
@@ -39,4 +55,8 @@ def sign_and_create(
     # The Package serializer validation method have two branches: the signing and non-signing.
     # Here, the package is already signed, so we need to update the context for a proper validation.
     context["sign_package"] = False
+    # The request data is immutable when there's an upload, so we can't delete the upload out of the
+    # request data like we do for a file.  Instead, we'll delete it here.
+    if "upload" in data:
+        del data["upload"]
     general_create(app_label, serializer_name, data=data, context=context, *args, **kwargs)
