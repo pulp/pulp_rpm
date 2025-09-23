991a8f30c0c6a98b9764ac154f3fbaa101247a99
Daniel Alley <dalley@redhat.com>
2025-09-22 21:56:37 -0400

    Change implementation of Python version sort
    (cherry picked from commit e974e04e4fe6abbb060321647e60a67381e38360)

--- pulp_rpm/tests/unit/test_rpm_version.py
+++ pulp_rpm/tests/unit/test_rpm_version.py
@@ -7,8 +7,8 @@ to ensure compatibility between different RPM version comparison implementations
 
 from pulp_rpm.app.rpm_version import (
     RpmVersion,
-    compare_rpm_versions,
-    compare_version_strings,
+    _compare_rpm_versions,
+    _compare_version_strings,
     from_evr,
 )
 
@@ -55,9 +55,9 @@ class TestRpmVersionComparison:
 
     def test_rpm_evr_compare(self):
         """Test direct comparison of rpm EVR strings."""
-        assert compare_rpm_versions("0:1.2.3-45", "1.2.3-45") == 0
-        assert compare_rpm_versions("1.2.3-45", "1:1.2.3-45") < 0
-        assert compare_rpm_versions("1.2.3-46", "1.2.3-45") > 0
+        assert _compare_rpm_versions("0:1.2.3-45", "1.2.3-45") == 0
+        assert _compare_rpm_versions("1.2.3-45", "1:1.2.3-45") < 0
+        assert _compare_rpm_versions("1.2.3-46", "1.2.3-45") > 0
 
     def test_evr_ord(self):
         """Test comparing EVRs using comparison operators."""
@@ -148,158 +148,158 @@ class TestRpmVersionComparison:
 
     def test_compare_version_string(self):
         """Test many different combinations of version string comparison behavior."""
-        assert compare_version_strings("1.0", "1.0") == 0
-        assert compare_version_strings("1.0", "2.0") < 0
-        assert compare_version_strings("2.0", "1.0") > 0
+        assert _compare_version_strings("1.0", "1.0") == 0
+        assert _compare_version_strings("1.0", "2.0") < 0
+        assert _compare_version_strings("2.0", "1.0") > 0
 
-        assert compare_version_strings("2.0.1", "2.0.1") == 0
-        assert compare_version_strings("2.0", "2.0.1") < 0
-        assert compare_version_strings("2.0.1", "2.0") > 0
+        assert _compare_version_strings("2.0.1", "2.0.1") == 0
+        assert _compare_version_strings("2.0", "2.0.1") < 0
+        assert _compare_version_strings("2.0.1", "2.0") > 0
 
-        assert compare_version_strings("5.0.1", "5.0.1a") < 0
-        assert compare_version_strings("5.0.1a", "5.0.1") > 0
+        assert _compare_version_strings("5.0.1", "5.0.1a") < 0
+        assert _compare_version_strings("5.0.1a", "5.0.1") > 0
 
-        assert compare_version_strings("5.0.a1", "5.0.a1") == 0
-        assert compare_version_strings("5.0.1a", "5.0.1a") == 0
-        assert compare_version_strings("5.0.a1", "5.0.a2") < 0
-        assert compare_version_strings("5.0.a2", "5.0.a1") > 0
+        assert _compare_version_strings("5.0.a1", "5.0.a1") == 0
+        assert _compare_version_strings("5.0.1a", "5.0.1a") == 0
+        assert _compare_version_strings("5.0.a1", "5.0.a2") < 0
+        assert _compare_version_strings("5.0.a2", "5.0.a1") > 0
 
-        assert compare_version_strings("10abc", "10.1abc") < 0
-        assert compare_version_strings("10.1abc", "10abc") > 0
+        assert _compare_version_strings("10abc", "10.1abc") < 0
+        assert _compare_version_strings("10.1abc", "10abc") > 0
 
-        assert compare_version_strings("8.0", "8.0.rc1") < 0
-        assert compare_version_strings("8.0.rc1", "8.0") > 0
+        assert _compare_version_strings("8.0", "8.0.rc1") < 0
+        assert _compare_version_strings("8.0.rc1", "8.0") > 0
 
-        assert compare_version_strings("10b2", "10a1") > 0
-        assert compare_version_strings("10a2", "10b2") < 0
+        assert _compare_version_strings("10b2", "10a1") > 0
+        assert _compare_version_strings("10a2", "10b2") < 0
 
-        assert compare_version_strings("6.6p1", "7.5p1") < 0
-        assert compare_version_strings("7.5p1", "6.6p1") > 0
+        assert _compare_version_strings("6.6p1", "7.5p1") < 0
+        assert _compare_version_strings("7.5p1", "6.6p1") > 0
 
-        assert compare_version_strings("6.5p1", "6.5p1") == 0
-        assert compare_version_strings("6.5p1", "6.5p2") < 0
-        assert compare_version_strings("6.5p2", "6.5p1") > 0
-        assert compare_version_strings("6.5p2", "6.6p1") < 0
-        assert compare_version_strings("6.6p1", "6.5p2") > 0
+        assert _compare_version_strings("6.5p1", "6.5p1") == 0
+        assert _compare_version_strings("6.5p1", "6.5p2") < 0
+        assert _compare_version_strings("6.5p2", "6.5p1") > 0
+        assert _compare_version_strings("6.5p2", "6.6p1") < 0
+        assert _compare_version_strings("6.6p1", "6.5p2") > 0
 
-        assert compare_version_strings("6.5p10", "6.5p10") == 0
-        assert compare_version_strings("6.5p1", "6.5p10") < 0
-        assert compare_version_strings("6.5p10", "6.5p1") > 0
+        assert _compare_version_strings("6.5p10", "6.5p10") == 0
+        assert _compare_version_strings("6.5p1", "6.5p10") < 0
+        assert _compare_version_strings("6.5p10", "6.5p1") > 0
 
-        assert compare_version_strings("abc10", "abc10") == 0
-        assert compare_version_strings("abc10", "abc10.1") < 0
-        assert compare_version_strings("abc10.1", "abc10") > 0
+        assert _compare_version_strings("abc10", "abc10") == 0
+        assert _compare_version_strings("abc10", "abc10.1") < 0
+        assert _compare_version_strings("abc10.1", "abc10") > 0
 
-        assert compare_version_strings("abc.4", "abc.4") == 0
-        assert compare_version_strings("abc.4", "8") < 0
-        assert compare_version_strings("8", "abc.4") > 0
-        assert compare_version_strings("abc.4", "2") < 0
-        assert compare_version_strings("2", "abc.4") > 0
+        assert _compare_version_strings("abc.4", "abc.4") == 0
+        assert _compare_version_strings("abc.4", "8") < 0
+        assert _compare_version_strings("8", "abc.4") > 0
+        assert _compare_version_strings("abc.4", "2") < 0
+        assert _compare_version_strings("2", "abc.4") > 0
 
-        assert compare_version_strings("1.0aa", "1.0aa") == 0
-        assert compare_version_strings("1.0a", "1.0aa") < 0
-        assert compare_version_strings("1.0aa", "1.0a") > 0
+        assert _compare_version_strings("1.0aa", "1.0aa") == 0
+        assert _compare_version_strings("1.0a", "1.0aa") < 0
+        assert _compare_version_strings("1.0aa", "1.0a") > 0
 
     def test_version_comparison_numeric_handling(self):
         """Test handling of numeric-like values in version strings."""
-        assert compare_version_strings("10.0001", "10.0001") == 0
+        assert _compare_version_strings("10.0001", "10.0001") == 0
         # sequences of leading zeroes are meant to be ignored - it's not *actually* treated
         # like a numeric value
-        assert compare_version_strings("10.0001", "10.1") == 0
-        assert compare_version_strings("10.1", "10.0001") == 0
-        assert compare_version_strings("10.0001", "10.0039") < 0
-        assert compare_version_strings("10.0039", "10.0001") > 0
+        assert _compare_version_strings("10.0001", "10.1") == 0
+        assert _compare_version_strings("10.1", "10.0001") == 0
+        assert _compare_version_strings("10.0001", "10.0039") < 0
+        assert _compare_version_strings("10.0039", "10.0001") > 0
         # but sequences of zeroes within a numeric segment are not ignored
-        assert compare_version_strings("10.1", "10.10001") < 0
-        assert compare_version_strings("10.1111", "10.10001") < 0
-        assert compare_version_strings("10.11111", "10.10001") > 0
+        assert _compare_version_strings("10.1", "10.10001") < 0
+        assert _compare_version_strings("10.1111", "10.10001") < 0
+        assert _compare_version_strings("10.11111", "10.10001") > 0
 
-        assert compare_version_strings("20240521", "20240521") == 0
-        assert compare_version_strings("20240521", "20240522") < 0
-        assert compare_version_strings("20240522", "20240521") > 0
-        assert compare_version_strings("20240521", "202405210") < 0
+        assert _compare_version_strings("20240521", "20240521") == 0
+        assert _compare_version_strings("20240521", "20240522") < 0
+        assert _compare_version_strings("20240522", "20240521") > 0
+        assert _compare_version_strings("20240521", "202405210") < 0
 
     def test_version_comparison_tilde_and_caret(self):
         """Test behavior of tilde and caret operators."""
-        assert compare_version_strings("1.0~rc1", "1.0~rc1") == 0
-        assert compare_version_strings("1.0~rc1", "1.0") < 0
-        assert compare_version_strings("1.0", "1.0~rc1") > 0
-        assert compare_version_strings("1.0~rc1", "1.0~rc2") < 0
-        assert compare_version_strings("1.0~rc2", "1.0~rc1") > 0
-        assert compare_version_strings("1.0~rc1~git123", "1.0~rc1~git123") == 0
-        assert compare_version_strings("1.0~rc1~git123", "1.0~rc1") < 0
-        assert compare_version_strings("1.0~rc1", "1.0~rc1~git123") > 0
+        assert _compare_version_strings("1.0~rc1", "1.0~rc1") == 0
+        assert _compare_version_strings("1.0~rc1", "1.0") < 0
+        assert _compare_version_strings("1.0", "1.0~rc1") > 0
+        assert _compare_version_strings("1.0~rc1", "1.0~rc2") < 0
+        assert _compare_version_strings("1.0~rc2", "1.0~rc1") > 0
+        assert _compare_version_strings("1.0~rc1~git123", "1.0~rc1~git123") == 0
+        assert _compare_version_strings("1.0~rc1~git123", "1.0~rc1") < 0
+        assert _compare_version_strings("1.0~rc1", "1.0~rc1~git123") > 0
 
-        assert compare_version_strings("1.0^", "1.0^") == 0
-        assert compare_version_strings("1.0", "1.0^") < 0
-        assert compare_version_strings("1.0^", "1.0") > 0
+        assert _compare_version_strings("1.0^", "1.0^") == 0
+        assert _compare_version_strings("1.0", "1.0^") < 0
+        assert _compare_version_strings("1.0^", "1.0") > 0
 
-        assert compare_version_strings("1.0", "1.0git1^") < 0
-        assert compare_version_strings("1.0^git1", "1.0^git2") < 0
-        assert compare_version_strings("1.01", "1.0^git1") > 0
-        assert compare_version_strings("1.0^20240501", "1.0^20240501") == 0
-        assert compare_version_strings("1.0^20240501", "1.0.1") < 0
-        assert compare_version_strings("1.0^20240501^git1", "1.0^20240501^git1") == 0
-        assert compare_version_strings("1.0^20240502", "1.0^20240501^git1") > 0
-        assert compare_version_strings("1.0~rc1^git1", "1.0~rc1^git1") == 0
-        assert compare_version_strings("1.0~rc1", "1.0~rc1^git1") < 0
-        assert compare_version_strings("1.0~rc1^git1", "1.0~rc1") > 0
-        assert compare_version_strings("1.0^git1~pre", "1.0^git1~pre") == 0
-        assert compare_version_strings("1.0^git1~pre", "1.0^git1") < 0
-        assert compare_version_strings("1.0^git1", "1.0^git1~pre") > 0
+        assert _compare_version_strings("1.0", "1.0git1^") < 0
+        assert _compare_version_strings("1.0^git1", "1.0^git2") < 0
+        assert _compare_version_strings("1.01", "1.0^git1") > 0
+        assert _compare_version_strings("1.0^20240501", "1.0^20240501") == 0
+        assert _compare_version_strings("1.0^20240501", "1.0.1") < 0
+        assert _compare_version_strings("1.0^20240501^git1", "1.0^20240501^git1") == 0
+        assert _compare_version_strings("1.0^20240502", "1.0^20240501^git1") > 0
+        assert _compare_version_strings("1.0~rc1^git1", "1.0~rc1^git1") == 0
+        assert _compare_version_strings("1.0~rc1", "1.0~rc1^git1") < 0
+        assert _compare_version_strings("1.0~rc1^git1", "1.0~rc1") > 0
+        assert _compare_version_strings("1.0^git1~pre", "1.0^git1~pre") == 0
+        assert _compare_version_strings("1.0^git1~pre", "1.0^git1") < 0
+        assert _compare_version_strings("1.0^git1", "1.0^git1~pre") > 0
 
     def test_non_intuitive_comparison_behavior(self):
         """Test some version comparison behavior that is a bit non-intuitive."""
         # (but needs to be maintained for compatibility)
-        assert compare_version_strings("1e.fc33", "1.fc33") < 0
-        assert compare_version_strings("1g.fc33", "1.fc33") > 0
+        assert _compare_version_strings("1e.fc33", "1.fc33") < 0
+        assert _compare_version_strings("1g.fc33", "1.fc33") > 0
 
     def test_non_alphanumeric_equivalence(self):
         """Test handling of non-alphanumeric ascii characters (excluding separators)."""
         # the existence of sequences of non-alphanumeric characters should not impact
         # the version comparison at all
-        assert compare_version_strings("b", "b") == 0
-        assert compare_version_strings("b+", "b+") == 0
-        assert compare_version_strings("b+", "b_") == 0
-        assert compare_version_strings("b_", "b+") == 0
-        assert compare_version_strings("+b", "+b") == 0
-        assert compare_version_strings("+b", "_b") == 0
-        assert compare_version_strings("_b", "+b") == 0
+        assert _compare_version_strings("b", "b") == 0
+        assert _compare_version_strings("b+", "b+") == 0
+        assert _compare_version_strings("b+", "b_") == 0
+        assert _compare_version_strings("b_", "b+") == 0
+        assert _compare_version_strings("+b", "+b") == 0
+        assert _compare_version_strings("+b", "_b") == 0
+        assert _compare_version_strings("_b", "+b") == 0
 
-        assert compare_version_strings("+b", "++b") == 0
-        assert compare_version_strings("+b", "+b+") == 0
+        assert _compare_version_strings("+b", "++b") == 0
+        assert _compare_version_strings("+b", "+b+") == 0
 
-        assert compare_version_strings("+.", "+_") == 0
-        assert compare_version_strings("_+", "+.") == 0
-        assert compare_version_strings("+", ".") == 0
-        assert compare_version_strings(",", "+") == 0
+        assert _compare_version_strings("+.", "+_") == 0
+        assert _compare_version_strings("_+", "+.") == 0
+        assert _compare_version_strings("+", ".") == 0
+        assert _compare_version_strings(",", "+") == 0
 
-        assert compare_version_strings("++", "_") == 0
-        assert compare_version_strings("+", "..") == 0
+        assert _compare_version_strings("++", "_") == 0
+        assert _compare_version_strings("+", "..") == 0
 
-        assert compare_version_strings("4_0", "4_0") == 0
-        assert compare_version_strings("4_0", "4.0") == 0
-        assert compare_version_strings("4.0", "4_0") == 0
+        assert _compare_version_strings("4_0", "4_0") == 0
+        assert _compare_version_strings("4_0", "4.0") == 0
+        assert _compare_version_strings("4.0", "4_0") == 0
 
-        assert compare_version_strings("4.999", "5.0") < 0
-        assert compare_version_strings("4.999.9", "5.0") < 0
-        assert compare_version_strings("5.0", "4.999_9") > 0
+        assert _compare_version_strings("4.999", "5.0") < 0
+        assert _compare_version_strings("4.999.9", "5.0") < 0
+        assert _compare_version_strings("5.0", "4.999_9") > 0
 
         # except when it comes to breaking up sequences of alphanumeric characters
         # that do impact the comparison
-        assert compare_version_strings("4.999", "4.999.9") < 0
-        assert compare_version_strings("4.999", "4.99.9") > 0
+        assert _compare_version_strings("4.999", "4.999.9") < 0
+        assert _compare_version_strings("4.999", "4.99.9") > 0
 
     def test_non_ascii_character_equivalence(self):
         """Test handling of non-ascii characters."""
         # the existence of sequences of non-ascii characters should not impact the
         # version comparison at all
-        assert compare_version_strings("1.1.Á.1", "1.1.1") == 0
-        assert compare_version_strings("1.1.Á", "1.1.Á") == 0
-        assert compare_version_strings("1.1.Á", "1.1.Ê") == 0
-        assert compare_version_strings("1.1.ÁÁ", "1.1.Á") == 0
-        assert compare_version_strings("1.1.Á", "1.1.ÊÊ") == 0
+        assert _compare_version_strings("1.1.Á.1", "1.1.1") == 0
+        assert _compare_version_strings("1.1.Á", "1.1.Á") == 0
+        assert _compare_version_strings("1.1.Á", "1.1.Ê") == 0
+        assert _compare_version_strings("1.1.ÁÁ", "1.1.Á") == 0
+        assert _compare_version_strings("1.1.Á", "1.1.ÊÊ") == 0
 
         # except when it comes to breaking up sequences of ascii characters that do
         # impact the comparison
-        assert compare_version_strings("1.1Á1", "1.11") < 0
+        assert _compare_version_strings("1.1Á1", "1.11") < 0
