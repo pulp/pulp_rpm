95bc7bc48acf98b70fa1b0817ab70831c44cfd88
Daniel Alley <dalley@redhat.com>
2025-10-15 00:34:13 -0400

    temp

--- pulp_rpm/app/migrations/0013_RAW_rpm_evr_extension.py
+++ pulp_rpm/app/migrations/0013_RAW_rpm_evr_extension.py
@@ -13,15 +13,15 @@ create type pulp_evr_array_item as (
 
 create type pulp_evr_t as (
   epoch INT,
-  version pulp_evr_array_item[],
-  release pulp_evr_array_item[]
+  version TEXT,
+  release TEXT
 );
 
 CREATE OR REPLACE FUNCTION pulp_evr_trigger() RETURNS trigger AS $$
   BEGIN
     NEW.evr = (select ROW(coalesce(NEW.epoch::numeric,0),
-                          pulp_rpmver_array(coalesce(NEW.version,'pulp_isempty'))::pulp_evr_array_item[],
-                          pulp_rpmver_array(coalesce(NEW.release,'pulp_isempty'))::pulp_evr_array_item[])::pulp_evr_t);
+                          coalesce(NEW.version,''),
+                          coalesce(NEW.release,''))::pulp_evr_t);
     RETURN NEW;
   END;
 $$ language 'plpgsql';
@@ -57,84 +57,300 @@ create or replace function pulp_isdigit(ch CHAR)
   END ;
 $$ language 'plpgsql';
 
-create or replace FUNCTION pulp_rpmver_array (string1 IN VARCHAR)
-  RETURNS pulp_evr_array_item[] as $$
+-- Compare two version strings according to RPM version comparison rules
+create or replace FUNCTION pulp_rpmvercmp(a TEXT, b TEXT)
+  RETURNS INT as $$
   declare
-    str1 VARCHAR := string1;
-    digits VARCHAR(10) := '0123456789';
-    lc_alpha VARCHAR(27) := 'abcdefghijklmnopqrstuvwxyz';
-    uc_alpha VARCHAR(27) := 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    alpha VARCHAR(54) := lc_alpha || uc_alpha;
-    one VARCHAR;
+    one TEXT;
+    two TEXT;
+    a_part TEXT;
+    b_part TEXT;
+    a_segment TEXT;
+    b_segment TEXT;
     isnum BOOLEAN;
-    ver_array pulp_evr_array_item[] := ARRAY[]::pulp_evr_array_item[];
+    result INT;
   BEGIN
-    if str1 is NULL
-    then
-      RAISE EXCEPTION 'VALUE_ERROR.';
+    -- Fast path for identical strings
+    if a = b then
+      return 0;
     end if;
 
-    one := str1;
-    <<segment_loop>>
-    while one <> ''
+    one := a;
+    two := b;
+
+    -- Loop through each version segment
+    while length(one) > 0 or length(two) > 0
     loop
-      declare
-        segm1 VARCHAR;
-        segm1_n NUMERIC := 0;
-      begin
-        -- Handle tilde separator: it sorts before everything else
-        -- Tilde is represented as a special marker in the array
-        if one <> '' and substr(one, 1, 1) = '~'
-        then
-          ver_array := array_append(ver_array, (-2, '~')::pulp_evr_array_item);
-          one := substr(one, 2);
-          continue;
-        end if;
+      -- Strip leading non-alphanumeric characters (except ~ and ^)
+      while length(one) > 0 and not pulp_isalphanum(one) and substr(one, 1, 1) <> '~' and substr(one, 1, 1) <> '^'
+      loop
+        one := substr(one, 2);
+      end loop;
 
-        -- Handle caret separator: it sorts after base version
-        -- Caret is represented as a special marker in the array
-        if one <> '' and substr(one, 1, 1) = '^'
-        then
-          ver_array := array_append(ver_array, (-1, '^')::pulp_evr_array_item);
-          one := substr(one, 2);
-          continue;
-        end if;
+      while length(two) > 0 and not pulp_isalphanum(two) and substr(two, 1, 1) <> '~' and substr(two, 1, 1) <> '^'
+      loop
+        two := substr(two, 2);
+      end loop;
 
-        -- Throw out all non-alphanum characters (except ~ and ^)
-        while one <> '' and not pulp_isalphanum(one) and substr(one, 1, 1) <> '~' and substr(one, 1, 1) <> '^'
+      -- Handle tilde separator - it sorts before everything else
+      if length(one) > 0 and substr(one, 1, 1) = '~' then
+        if length(two) = 0 or substr(two, 1, 1) <> '~' then
+          return -1;  -- one < two
+        end if;
+        one := substr(one, 2);
+        two := substr(two, 2);
+        continue;
+      elsif length(two) > 0 and substr(two, 1, 1) = '~' then
+        return 1;  -- one > two
+      end if;
+
+      -- Handle caret separator - complex logic
+      if length(one) > 0 and substr(one, 1, 1) = '^' then
+        if length(two) = 0 then
+          return 1;  -- one > two (caret vs end: caret wins)
+        elsif substr(two, 1, 1) <> '^' then
+          return -1;  -- one < two (caret vs continuation: continuation wins)
+        end if;
+        one := substr(one, 2);
+        two := substr(two, 2);
+        continue;
+      elsif length(two) > 0 and substr(two, 1, 1) = '^' then
+        if length(one) = 0 then
+          return -1;  -- one < two (end vs caret: caret wins)
+        else
+          return 1;  -- one > two (continuation vs caret: continuation wins)
+        end if;
+      end if;
+
+      -- If we ran to the end of either, we are finished
+      if length(one) = 0 and length(two) = 0 then
+        return 0;
+      elsif length(one) = 0 then
+        return -1;
+      elsif length(two) = 0 then
+        return 1;
+      end if;
+
+      -- Determine if this segment is numeric or alpha
+      if pulp_isdigit(substr(one, 1, 1)) then
+        isnum := true;
+        -- Extract numeric segment from both
+        a_segment := '';
+        while length(one) > 0 and pulp_isdigit(substr(one, 1, 1))
         loop
+          a_segment := a_segment || substr(one, 1, 1);
           one := substr(one, 2);
         end loop;
-        str1 := one;
-        if str1 <> '' and pulp_isdigit(str1)
-        then
-          str1 := ltrim(str1, digits);
-          isnum := true;
-        else
-          str1 := ltrim(str1, alpha);
-          isnum := false;
-        end if;
-        if str1 <> ''
-        then segm1 := substr(one, 1, length(one) - length(str1));
-        else segm1 := one;
+
+        b_segment := '';
+        while length(two) > 0 and pulp_isdigit(substr(two, 1, 1))
+        loop
+          b_segment := b_segment || substr(two, 1, 1);
+          two := substr(two, 2);
+        end loop;
+
+        -- If second segment is not numeric, numeric wins
+        if length(b_segment) = 0 then
+          return 1;
         end if;
 
-        if segm1 = '' then return ver_array; end if; /* arbitrary */
-        if isnum
-        then
-          segm1 := ltrim(segm1, '0');
-          if segm1 <> '' then segm1_n := segm1::numeric; end if;
-          segm1 := NULL;
-        else
-        end if;
-        ver_array := array_append(ver_array, (segm1_n, segm1)::pulp_evr_array_item);
-        one := str1;
-      end;
-    end loop segment_loop;
+        -- Strip leading zeros
+        a_segment := ltrim(a_segment, '0');
+        b_segment := ltrim(b_segment, '0');
+        if length(a_segment) = 0 then a_segment := '0'; end if;
+        if length(b_segment) = 0 then b_segment := '0'; end if;
 
-    return ver_array;
-  END ;
-$$ language 'plpgsql';
+        -- Compare by length first (longer number = bigger)
+        if length(a_segment) < length(b_segment) then
+          return -1;
+        elsif length(a_segment) > length(b_segment) then
+          return 1;
+        end if;
+
+        -- Same length, compare as strings
+        if a_segment < b_segment then
+          return -1;
+        elsif a_segment > b_segment then
+          return 1;
+        end if;
+      else
+        -- Extract alpha segment from both
+        a_segment := '';
+        while length(one) > 0 and pulp_isalphanum(one) and not pulp_isdigit(substr(one, 1, 1))
+        loop
+          a_segment := a_segment || substr(one, 1, 1);
+          one := substr(one, 2);
+        end loop;
+
+        b_segment := '';
+        while length(two) > 0 and pulp_isalphanum(two) and not pulp_isdigit(substr(two, 1, 1))
+        loop
+          b_segment := b_segment || substr(two, 1, 1);
+          two := substr(two, 2);
+        end loop;
+
+        -- If first segment is empty, arbitrary
+        if length(a_segment) = 0 then
+          return -1;
+        end if;
+
+        -- If second is numeric and first is alpha, numeric wins
+        if length(b_segment) = 0 and length(two) > 0 and pulp_isdigit(substr(two, 1, 1)) then
+          return -1;
+        end if;
+
+        -- Compare alphabetically
+        if a_segment < b_segment then
+          return -1;
+        elsif a_segment > b_segment then
+          return 1;
+        end if;
+      end if;
+    end loop;
+
+    return 0;
+  END;
+$$ language 'plpgsql' IMMUTABLE;
+
+-- Compare two EVR composite types
+create or replace FUNCTION pulp_evr_cmp(a pulp_evr_t, b pulp_evr_t)
+  RETURNS INT as $$
+  declare
+    result INT;
+  BEGIN
+    -- Compare epoch first
+    if a.epoch < b.epoch then
+      return -1;
+    elsif a.epoch > b.epoch then
+      return 1;
+    end if;
+
+    -- Compare version
+    result := pulp_rpmvercmp(a.version, b.version);
+    if result <> 0 then
+      return result;
+    end if;
+
+    -- Compare release
+    return pulp_rpmvercmp(a.release, b.release);
+  END;
+$$ language 'plpgsql' IMMUTABLE;
+
+-- Define comparison operators for pulp_evr_t
+CREATE OR REPLACE FUNCTION pulp_evr_eq(a pulp_evr_t, b pulp_evr_t)
+  RETURNS BOOLEAN as $$
+  BEGIN
+    return pulp_evr_cmp(a, b) = 0;
+  END;
+$$ language 'plpgsql' IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION pulp_evr_ne(a pulp_evr_t, b pulp_evr_t)
+  RETURNS BOOLEAN as $$
+  BEGIN
+    return pulp_evr_cmp(a, b) <> 0;
+  END;
+$$ language 'plpgsql' IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION pulp_evr_lt(a pulp_evr_t, b pulp_evr_t)
+  RETURNS BOOLEAN as $$
+  BEGIN
+    return pulp_evr_cmp(a, b) < 0;
+  END;
+$$ language 'plpgsql' IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION pulp_evr_le(a pulp_evr_t, b pulp_evr_t)
+  RETURNS BOOLEAN as $$
+  BEGIN
+    return pulp_evr_cmp(a, b) <= 0;
+  END;
+$$ language 'plpgsql' IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION pulp_evr_gt(a pulp_evr_t, b pulp_evr_t)
+  RETURNS BOOLEAN as $$
+  BEGIN
+    return pulp_evr_cmp(a, b) > 0;
+  END;
+$$ language 'plpgsql' IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION pulp_evr_ge(a pulp_evr_t, b pulp_evr_t)
+  RETURNS BOOLEAN as $$
+  BEGIN
+    return pulp_evr_cmp(a, b) >= 0;
+  END;
+$$ language 'plpgsql' IMMUTABLE;
+
+-- Create operators
+CREATE OPERATOR = (
+  LEFTARG = pulp_evr_t,
+  RIGHTARG = pulp_evr_t,
+  FUNCTION = pulp_evr_eq,
+  COMMUTATOR = =,
+  NEGATOR = <>,
+  RESTRICT = eqsel,
+  JOIN = eqjoinsel,
+  HASHES,
+  MERGES
+);
+
+CREATE OPERATOR <> (
+  LEFTARG = pulp_evr_t,
+  RIGHTARG = pulp_evr_t,
+  FUNCTION = pulp_evr_ne,
+  COMMUTATOR = <>,
+  NEGATOR = =,
+  RESTRICT = neqsel,
+  JOIN = neqjoinsel
+);
+
+CREATE OPERATOR < (
+  LEFTARG = pulp_evr_t,
+  RIGHTARG = pulp_evr_t,
+  FUNCTION = pulp_evr_lt,
+  COMMUTATOR = >,
+  NEGATOR = >=,
+  RESTRICT = scalarltsel,
+  JOIN = scalarltjoinsel
+);
+
+CREATE OPERATOR <= (
+  LEFTARG = pulp_evr_t,
+  RIGHTARG = pulp_evr_t,
+  FUNCTION = pulp_evr_le,
+  COMMUTATOR = >=,
+  NEGATOR = >,
+  RESTRICT = scalarlesel,
+  JOIN = scalarlejoinsel
+);
+
+CREATE OPERATOR > (
+  LEFTARG = pulp_evr_t,
+  RIGHTARG = pulp_evr_t,
+  FUNCTION = pulp_evr_gt,
+  COMMUTATOR = <,
+  NEGATOR = <=,
+  RESTRICT = scalargtsel,
+  JOIN = scalargtjoinsel
+);
+
+CREATE OPERATOR >= (
+  LEFTARG = pulp_evr_t,
+  RIGHTARG = pulp_evr_t,
+  FUNCTION = pulp_evr_ge,
+  COMMUTATOR = <=,
+  NEGATOR = <,
+  RESTRICT = scalargesel,
+  JOIN = scalargejoinsel
+);
+
+-- Create operator class for btree indexing
+CREATE OPERATOR CLASS pulp_evr_ops
+  DEFAULT FOR TYPE pulp_evr_t USING btree AS
+  OPERATOR 1 <,
+  OPERATOR 2 <=,
+  OPERATOR 3 =,
+  OPERATOR 4 >=,
+  OPERATOR 5 >,
+  FUNCTION 1 pulp_evr_cmp(pulp_evr_t, pulp_evr_t);
 """
 
 
@@ -163,8 +379,8 @@ CREATE TRIGGER pulp_evr_update_trigger
 migration_sql = """
 update rpm_package set evr = (
   select ROW(coalesce(epoch::numeric,0),
-             pulp_rpmver_array(version)::pulp_evr_array_item[],
-             pulp_rpmver_array(release)::pulp_evr_array_item[])::pulp_evr_t
+             coalesce(version,''),
+             coalesce(release,''))::pulp_evr_t
   );
 """
 
