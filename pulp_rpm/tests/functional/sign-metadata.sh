7f705c9de090b6f0016272e333c97ca6d7041a97
Pedro Brochado <pedropsb95@gmail.com>
2026-01-15 10:25:11 -0300

    Update shell script to comply with shellcheck
    (cherry picked from commit 1282d25420960281c01025cd35b9862a8b450865)

--- pulp_rpm/tests/functional/sign-metadata.sh
+++ pulp_rpm/tests/functional/sign-metadata.sh
@@ -7,12 +7,12 @@ GPG_KEY_ID="pulp-fixture-signing-key"
 
 # Create a detached signature
 gpg --quiet --batch --homedir ~/.gnupg/ --detach-sign --local-user "${GPG_KEY_ID}" \
-   --armor --output ${SIGNATURE_PATH} ${FILE_PATH}
+   --armor --output "${SIGNATURE_PATH}" "${FILE_PATH}"
 
 # Check the exit status
 STATUS=$?
 if [[ ${STATUS} -eq 0 ]]; then
-   echo {\"file\": \"${FILE_PATH}\", \"signature\": \"${SIGNATURE_PATH}\"}
+   echo "{\"file\": \"${FILE_PATH}\", \"signature\": \"${SIGNATURE_PATH}\"}"
 else
    exit ${STATUS}
 fi
