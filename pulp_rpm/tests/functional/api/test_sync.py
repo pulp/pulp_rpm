d78b27ed579bac8e17acc5c0e93fd32cff230661
Pedro Brochado <pedropsb95@gmail.com>
2026-01-22 16:23:06 -0300

    Improve yaml.SafeLoader implementation against desserialization attacks
    Re-implement the bugfix for <https://github.com/pulp/pulp_rpm/issues/3285>
    in a way that SafeLoader is more transparently modified.
    SafeLoader is known to be safe against those attacks (CWE-502).
    
    The previous patch to SafeLoader was too intrusive in the code and
    hard to audit (if it was still safe after the patch). This is a simpler
    patch and easier to audit. The behavior differs, but still addresses the bug:
    Instead of preventing certain key's values to be casted, the new
    implementation prevents any casting from int and float unquoted
    values (regardless of the key).
    
    (cherry picked from commit 3c912cf25def012cfd70780f30581672f2ad28e6)

--- pulp_rpm/tests/functional/api/test_sync.py
+++ pulp_rpm/tests/functional/api/test_sync.py
@@ -1053,19 +1053,19 @@ def test_modular_metadata(
                 "pulp_labels",
             },
         )
-        assert list(diff) == [], list(diff)
+        assert list(diff) == [], (m1["name"], list(diff))
 
     for m1, m2 in zip(module_defaults, RPM_MODULEMD_DEFAULTS_DATA):
         diff = dictdiffer.diff(
             m1, m2, ignore={"pulp_created", "pulp_last_updated", "pulp_href", "prn", "pulp_labels"}
         )
-        assert list(diff) == [], list(diff)
+        assert list(diff) == [], (m1["name"], list(diff))
 
     for m1, m2 in zip(module_obsoletes, RPM_MODULEMD_OBSOLETES_DATA):
         diff = dictdiffer.diff(
             m1, m2, ignore={"pulp_created", "pulp_last_updated", "pulp_href", "prn", "pulp_labels"}
         )
-        assert list(diff) == [], list(diff)
+        assert list(diff) == [], (m1["name"], list(diff))
 
     # assert all package from modular repo is marked as modular
     for pkg in get_content(repository)["present"][RPM_PACKAGE_CONTENT_NAME]:
