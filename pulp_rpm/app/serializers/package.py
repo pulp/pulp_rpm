eb1b92f9487d41f3f3a7981bbd667a8bf9e7a79d
Daniel Alley <dalley@redhat.com>
2026-03-20 12:43:59 -0400

    Use createrepo_c flags to ensure we don't parse file digests
    Parsing file digests changes the API, due to an accident. We've already
    handled that, but we can avoid it happening in the first place and save
    some memory while we're at it.

--- pulp_rpm/app/serializers/package.py
+++ pulp_rpm/app/serializers/package.py
@@ -21,6 +21,7 @@ from pulpcore.plugin.files import PulpTemporaryUploadedFile
 from tempfile import NamedTemporaryFile
 from pulpcore.plugin.util import get_domain_pk
 
+from pulp_rpm.app.constants import CR_HEADER_FLAGS
 from pulp_rpm.app.models import Package
 from pulp_rpm.app.shared_utils import format_nvra, read_crpackage_from_artifact
 
@@ -429,7 +430,9 @@ class PackageUploadSerializer(PackageSerializer):
         try:
             if uploaded_file:
                 cr_object = cr.package_from_rpm(
-                    uploaded_file.file.name, changelog_limit=settings.KEEP_CHANGELOG_LIMIT
+                    uploaded_file.file.name,
+                    changelog_limit=settings.KEEP_CHANGELOG_LIMIT,
+                    header_reading_flags=CR_HEADER_FLAGS,
                 )
                 new_pkg = Package.createrepo_to_dict(cr_object)
             elif upload:
@@ -446,7 +449,9 @@ class PackageUploadSerializer(PackageSerializer):
 
                 # Now we have a file, read metadata from it
                 cr_object = cr.package_from_rpm(
-                    temp_file.name, changelog_limit=settings.KEEP_CHANGELOG_LIMIT
+                    temp_file.name,
+                    changelog_limit=settings.KEEP_CHANGELOG_LIMIT,
+                    header_reading_flags=CR_HEADER_FLAGS,
                 )
                 new_pkg = Package.createrepo_to_dict(cr_object)
 
