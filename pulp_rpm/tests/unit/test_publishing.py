de5c9176d5b1c9f19d9a3146d470ebed0c437342
Daniel Alley <dalley@redhat.com>
2026-04-17 14:49:20 -0400

    Remove the rest of the nested_by_both layout support
    Finish removing the leftovers kept for ZDU reasons
    
    re #4358

--- pulp_rpm/tests/unit/test_publishing.py
+++ pulp_rpm/tests/unit/test_publishing.py
@@ -17,33 +17,16 @@ class TestPublishing(TestCase):
 
         # Test that collisions on nevra or path are checking epoch correctly.
         cm = _CollisionManager()
-        cm.add(mid_epoch, "nevra", "path", None)
-        cm.add(low_epoch, "nevra", "path2", None)
+        cm.add(mid_epoch, "nevra", "path")
+        cm.add(low_epoch, "nevra", "path2")
         self.assertEqual([mid_epoch.cid], cm.retained_cids())
-        cm.add(high_epoch, "nevra2", "path", None)
+        cm.add(high_epoch, "nevra2", "path")
         self.assertEqual([high_epoch.cid], cm.retained_cids())
 
         # Test that collisions on nevra or path are checking build_time correctly.
         cm = _CollisionManager()
-        cm.add(mid_build_time, "nevra", "path", None)
-        cm.add(low_build_time, "nevra", "path2", None)
+        cm.add(mid_build_time, "nevra", "path")
+        cm.add(low_build_time, "nevra", "path2")
         self.assertEqual([mid_build_time.cid], cm.retained_cids())
-        cm.add(high_build_time, "nevra2", "path", None)
+        cm.add(high_build_time, "nevra2", "path")
         self.assertEqual([high_build_time.cid], cm.retained_cids())
-
-        # Test that collisions on 2nd_path are being handled correctly.
-        cm = _CollisionManager()
-        cm.add(mid_build_time, "nevra", "path", "second_path")
-        cm.add(low_build_time, "nevra2", "path2", "second_path")
-        self.assertEqual({mid_build_time.cid, low_build_time.cid}, set(cm.retained_cids()))
-        self.assertIn(mid_build_time.cid, cm.cid_to_second_path)
-        self.assertEqual(cm.cid_to_second_path[mid_build_time.cid], "second_path")
-        self.assertNotIn(low_build_time.cid, cm.cid_to_second_path)
-
-        cm.add(high_build_time, "nevra3", "path3", "second_path")
-        self.assertEqual(
-            {mid_build_time.cid, low_build_time.cid, high_build_time.cid}, set(cm.retained_cids())
-        )
-        self.assertIn(high_build_time.cid, cm.cid_to_second_path)
-        self.assertEqual(cm.cid_to_second_path[high_build_time.cid], "second_path")
-        self.assertNotIn(mid_build_time.cid, cm.cid_to_second_path)
